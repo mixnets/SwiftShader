3092e07176e31ffbf4d1ff9d8c99fbe57c8cbea5
Nicolas Capens <capn@google.com>
2014-11-03 17:40:15 -0500

    wip-radiance
    Change-Id: Ic9b58dd38073b32a8507842d5da8af3d8b260f43

---
+++ src/Radiance/libRAD/Object.hpp
@@ -0,0 +1,48 @@
+#ifndef rad_Object_hpp
+#define rad_Object_hpp
+
+#include "Common/debug.h"
+#include "Common/Thread.hpp"
+
+namespace es2
+{
+	class Object
+	{
+	public:
+		Object()
+		{
+			referenceCount = 1;
+		}
+
+		void reference()
+		{
+			sw::atomicIncrement(&referenceCount);
+		}
+
+		void release()
+		{
+			ASSERT(referenceCount > 0);
+
+			if(referenceCount > 0)
+			{
+				sw::atomicDecrement(&referenceCount);
+			}
+
+			if(referenceCount == 0)
+			{
+				delete this;
+			}
+		}
+
+	protected:
+		virtual ~Object()
+		{
+			ASSERT(referenceCount == 0);
+		}
+
+	private:
+		volatile int referenceCount;
+	};
+}
+
+#endif   // rad_Object_hpp
