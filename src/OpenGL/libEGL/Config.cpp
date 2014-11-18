d42641f6d22263e6bb73f27cca69bef287eba1b7
Ping-Hao Wu <pinghao@google.com>
2014-11-18 14:52:06 -0800

    WIP - Build on Android
    Change-Id: I7c9e22ba22fe23dc8d77a038eae970c62d1eafd6

--- src/OpenGL/libEGL/Config.cpp
+++ src/OpenGL/libEGL/Config.cpp
@@ -319,8 +319,8 @@ bool ConfigSet::getConfigs(EGLConfig *configs, const EGLint *attribList, EGLint 
             case EGL_MAX_PBUFFER_WIDTH:          match = config->mMaxPBufferWidth >= attribute[1];                 break;
             case EGL_MAX_PBUFFER_HEIGHT:         match = config->mMaxPBufferHeight >= attribute[1];                break;
             case EGL_MAX_PBUFFER_PIXELS:         match = config->mMaxPBufferPixels >= attribute[1];                break;
-			case EGL_RECORDABLE_ANDROID:         match = false; /* UNIMPLEMENTED(); */                             break;
-			case EGL_FRAMEBUFFER_TARGET_ANDROID: match = false; /* UNIMPLEMENTED(); */                             break;
+            case EGL_RECORDABLE_ANDROID:         match = true;                                                     break;
+            case EGL_FRAMEBUFFER_TARGET_ANDROID: match = true;                                                     break;
 			default:
 				UNIMPLEMENTED();
 				match = false;
