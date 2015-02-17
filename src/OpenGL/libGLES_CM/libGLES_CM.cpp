05536e4362d670a8406f82e9122fbd183828bc15
Nicolas Capens <capn@google.com>
2015-02-17 10:51:33 -0500

    silence
    Change-Id: I8305f5ec49230dad2dad29604fdabb98dae358dd

--- src/OpenGL/libGLES_CM/libGLES_CM.cpp
+++ src/OpenGL/libGLES_CM/libGLES_CM.cpp
@@ -3161,7 +3161,7 @@ void GL_APIENTRY glScissor(GLint x, GLint y, GLsizei width, GLsizei height)
 
 void GL_APIENTRY glShadeModel(GLenum mode)
 {
-	UNIMPLEMENTED();
+	//UNIMPLEMENTED();
 }
 
 void GL_APIENTRY glStencilFunc(GLenum func, GLint ref, GLuint mask)
@@ -3369,7 +3369,7 @@ void GL_APIENTRY glTexEnvi(GLenum target, GLenum pname, GLint param)
 
 void GL_APIENTRY glTexEnvx(GLenum target, GLenum pname, GLfixed param)
 {
-	UNIMPLEMENTED();
+	//UNIMPLEMENTED();
 }
 
 void GL_APIENTRY glTexEnviv(GLenum target, GLenum pname, const GLint *params)
@@ -3627,7 +3627,7 @@ void GL_APIENTRY glTexParameteri(GLenum target, GLenum pname, GLint param)
 			}
 			break;
 		case GL_TEXTURE_CROP_RECT_OES:
-			UNIMPLEMENTED();
+			//UNIMPLEMENTED();
 			break;
 		default:
 			return error(GL_INVALID_ENUM);
@@ -3642,7 +3642,7 @@ void GL_APIENTRY glTexParameteriv(GLenum target, GLenum pname, const GLint* para
 
 void GL_APIENTRY glTexParameterx(GLenum target, GLenum pname, GLfixed param)
 {
-	UNIMPLEMENTED();
+	//UNIMPLEMENTED();
 }
 
 void GL_APIENTRY glTexParameterxv(GLenum target, GLenum pname, const GLfixed *params)
@@ -3802,7 +3802,7 @@ void GL_APIENTRY glDrawTexsOES(GLshort x, GLshort y, GLshort z, GLshort width, G
 
 void GL_APIENTRY glDrawTexiOES(GLint x, GLint y, GLint z, GLint width, GLint height)
 {
-	UNIMPLEMENTED();
+	//UNIMPLEMENTED();
 }
 
 void GL_APIENTRY glDrawTexxOES(GLfixed x, GLfixed y, GLfixed z, GLfixed width, GLfixed height)
