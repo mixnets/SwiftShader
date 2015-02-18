e51b11199a91a3cc842f6f45523a9364408044ea
Nicolas Capens <capn@google.com>
2015-02-18 17:53:39 -0500

    Implement basic GLSL ES 3.0 texture sampling.
    Bug 19331817
    
    Change-Id: Ieb4581c62525112e0b76e94bcbbb8f9608c782ea

--- src/OpenGL/compiler/OutputASM.cpp
+++ src/OpenGL/compiler/OutputASM.cpp
@@ -767,8 +767,11 @@ namespace glsl
 				else
 				{
 					TString name = TFunction::unmangleName(node->getName());
+					TIntermSequence &arguments = node->getSequence();
+					TBasicType samplerType = arguments[0]->getAsTyped()->getType().getBasicType();
 
-					if(name == "texture2D" || name == "textureCube" || name == "texture3D")
+					if(name == "texture2D" || name == "textureCube" || name == "texture3D" ||
+					   (name == "texture" && (samplerType == EbtSampler2D || samplerType == EbtSampler3D)))
 					{
 						if(argumentCount == 2)
 						{	
