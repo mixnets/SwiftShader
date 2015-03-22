f2a07346f831984625810c9e2de40ff215ce5f4a
Ping-Hao Wu <pinghao@google.com>
2015-03-21 22:45:06 -0700

    ANDROID_image_native_buffer implementation
    Change-Id: I9c2dd37067318bb8ce8be4d9c442a18ec2be2fd0

--- src/OpenGL/libEGL/libEGL.cpp
+++ src/OpenGL/libEGL/libEGL.cpp
@@ -19,6 +19,9 @@
 #include "Image.hpp"
 #include "common/debug.h"
 #include "Common/Version.h"
+#if defined(__ANDROID__)
+    #include <system/window.h>
+#endif
 
 #include <string.h>
 
@@ -869,6 +872,15 @@ EGLImageKHR EGLAPIENTRY eglCreateImageKHR(EGLDisplay dpy, EGLContext ctx, EGLenu
 	{
 		return error(EGL_BAD_PARAMETER, EGL_NO_IMAGE_KHR);
 	}
+
+	#if defined(__ANDROID__)
+		if(target == EGL_NATIVE_BUFFER_ANDROID)
+		{
+			// When target is EGL_NATIVE_BUFFER_ANDROID, ctx is always EGL_NO_CONTEXT.
+			// Get the current context so that we can validate and create shared image
+			context = static_cast<egl::Context*>(egl::getCurrentContext());
+		}
+	#endif
 
 	EGLenum validationResult = context->validateSharedImage(target, name, textureLevel);
 
