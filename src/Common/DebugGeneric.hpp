7abc341dd4ccd129fb7bfbf3d6e7aa959e759a87
Greg Hartman <ghartman@google.com>
2015-04-09 18:48:53 -0700

    Set up Android-specific debugging
    Change-Id: I293568f0d0ee42dd64ee869e3c3ba28810e4883a

---
+++ src/Common/DebugGeneric.hpp
@@ -0,0 +1,41 @@
+// SwiftShader Software Renderer
+//
+// Copyright(c) 2005-2012 TransGaming Inc.
+//
+// All rights reserved. No part of this software may be copied, distributed, transmitted,
+// transcribed, stored in a retrieval system, translated into any human or computer
+// language by any means, or disclosed to third parties without the explicit written
+// agreement of TransGaming Inc. Without such an agreement, no rights or licenses, express
+// or implied, including but not limited to any patent rights, are granted to you.
+//
+
+#ifndef DebugGeneric_hpp
+#define DebugGeneric_hpp
+
+#include <assert.h>
+#include <stdio.h>
+
+#undef min
+#undef max
+
+void trace(const char *format, ...);
+
+#ifndef NDEBUG
+	#define TRACE(format, ...) trace("[0x%0.8X]%s(" format ")\n", this, __FUNCTION__, ##__VA_ARGS__)
+#else
+	#define TRACE(...) ((void)0)
+#endif
+
+#ifndef NDEBUG
+	#define UNIMPLEMENTED() {trace("\t! Unimplemented: %s(%d)\n", __FUNCTION__, __LINE__); ASSERT(false);}
+#else
+	#define UNIMPLEMENTED() ((void)0)
+#endif
+
+#ifndef NDEBUG
+	#define ASSERT(expression) {if(!(expression)) trace("\t! Assert failed in %s(%d): " #expression "\n", __FUNCTION__, __LINE__); assert(expression);}
+#else
+	#define ASSERT assert
+#endif
+
+#endif   // DebugGeneric_hpp
