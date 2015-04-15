7df99d3ac8bf3d79fbc1f28c50e9eb3ecc30e0f7
Nicolas Capens <capn@google.com>
2015-04-15 16:50:21 -0400

    Access library dependencies through a single object.
    Bug 18752589
    
    Change-Id: I93155cead50b30faa876023f9ed00f60b7c9e34e

---
+++ src/OpenGL/libGLESv2/libGLESv2.hpp
@@ -0,0 +1,78 @@
+#ifndef libGLESv2_hpp
+#define libGLESv2_hpp
+
+#define GL_API
+#include <GLES/gl.h>
+#define GL_GLEXT_PROTOTYPES
+#include <GLES/glext.h>
+#define EGLAPI
+#include <EGL/egl.h>
+
+#include "Common/SharedLibrary.hpp"
+
+namespace sw
+{
+class FrameBuffer;
+enum Format : unsigned char;
+}
+
+namespace egl
+{
+class Context;
+class Image;
+class Config;
+}
+
+class LibGLESv2exports
+{
+public:
+	LibGLESv2exports();
+
+	egl::Context *(*es2CreateContext)(const egl::Config *config, const egl::Context *shareContext, int clientVersion);
+	__eglMustCastToProperFunctionPointerType (*es2GetProcAddress)(const char *procname);
+	egl::Image *(*createBackBuffer)(int width, int height, const egl::Config *config);
+	egl::Image *(*createDepthStencil)(unsigned int width, unsigned int height, sw::Format format, int multiSampleDepth, bool discard);
+	sw::FrameBuffer *(*createFrameBuffer)(EGLNativeDisplayType display, EGLNativeWindowType window, int width, int height);
+};
+
+class LibGLESv2
+{
+public:
+	LibGLESv2()
+	{
+		libGLESv2 = nullptr;
+	}
+
+	~LibGLESv2()
+	{
+		freeLibrary(libGLESv2);
+	}
+
+	LibGLESv2exports *operator->()
+	{
+		static LibGLESv2exports *(*libGLESv2exports)() = nullptr;
+
+		if(!libGLESv2)
+		{
+			#if defined(_WIN32)
+			const char *libGLESv2_lib[] = {"libGLESv2.dll", "libGLES_V2_translator.dll"};
+			#elif defined(__ANDROID__)
+			const char *libGLESv2_lib[] = {"/vendor/lib/egl/libGLESv2_swiftshader.so"};
+			#elif defined(__LP64__)
+			const char *libGLESv2_lib[] = {"lib64GLES_V2_translator.so", "libGLESv2.so.2", "libGLESv2.so"};
+			#else
+			const char *libGLESv2_lib[] = {"libGLES_V2_translator.so", "libGLESv2.so.2", "libGLESv2.so"};
+			#endif
+
+			libGLESv2 = loadLibrary(libGLESv2_lib);
+			libGLESv2exports = (LibGLESv2exports *(*)())getProcAddress(libGLESv2, "libGLESv2exports");
+		}
+
+		return libGLESv2exports();
+}
+
+private:
+	void *libGLESv2;
+};
+
+#endif   // libGLESv2_hpp
