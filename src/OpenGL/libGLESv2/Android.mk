d99c1b162e33343aaadc35955587a9c794236bf3
Greg Hartman <ghartman@google.com>
2015-04-30 15:44:11 -0700

    Makefile updates to productionize the release
    Change-Id: I9b043641f2c978d50cc4ae6492dff74ad5f006d6

--- src/OpenGL/libGLESv2/Android.mk
+++ src/OpenGL/libGLESv2/Android.mk
@@ -1,12 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
 
-LOCAL_CLANG := true
-
-LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/lib/egl
-LOCAL_MODULE := libGLESv2_swiftshader
-
-LOCAL_SRC_FILES += \
+COMMON_SRC_FILES := \
 	Buffer.cpp \
 	Context.cpp \
 	Device.cpp \
@@ -26,24 +20,34 @@ LOCAL_SRC_FILES += \
 	VertexArray.cpp \
 	VertexDataManager.cpp \
 
-LOCAL_CFLAGS += -DLOG_TAG=\"libGLESv2_swiftshader\"
+COMMON_CFLAGS := \
+	-DLOG_TAG=\"libGLESv2_swiftshader\" \
+	-fno-operator-names \
+	-msse2 \
+	-D__STDC_CONSTANT_MACROS \
+	-D__STDC_LIMIT_MACROS \
+	-std=c++11
 
-# Android's make system also uses NDEBUG, so we need to set/unset it forcefully
-# Uncomment for ON:
-LOCAL_CFLAGS += -UNDEBUG -g -O0
-# Uncomment for OFF:
-#LOCAL_CFLAGS += -fomit-frame-pointer -ffunction-sections -fdata-sections -DANGLE_DISABLE_TRACE
+COMMON_SHARED_LIBRARIES := \
+	libdl \
+	liblog \
+	libcutils \
+	libhardware \
+	libui \
+	libutils \
+	$(GCE_STLPORT_LIBS)
 
-LOCAL_CFLAGS += -fno-operator-names -msse2 -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS
-LOCAL_CFLAGS += -std=c++11
+COMMON_STATIC_LIBRARIES := \
+	swiftshader_compiler \
+	swiftshader_top \
+	libLLVM_swiftshader
 
-LOCAL_SHARED_LIBRARIES += libdl liblog libcutils libhardware libui libutils \
-    $(GCE_STLPORT_LIBS)
+COMMON_LDFLAGS := \
+	-Wl,--gc-sections \
+	-Wl,--version-script=$(LOCAL_PATH)/exports.map \
+	-Wl,--hash-style=sysv
 
-LOCAL_STATIC_LIBRARIES += swiftshader_compiler swiftshader_top libLLVM_swiftshader
-LOCAL_LDFLAGS += -Wl,--gc-sections -Wl,--version-script=$(LOCAL_PATH)/exports.map -Wl,--hash-style=sysv
-
-LOCAL_C_INCLUDES += \
+COMMON_C_INCLUDES := \
 	$(LOCAL_PATH)/../include \
 	$(LOCAL_PATH)/../ \
 	$(LOCAL_PATH)/../../ \
@@ -56,6 +60,40 @@ LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../../Shader/ \
 	$(LOCAL_PATH)/../../Main/
 
-include external/stlport/libstlport.mk
+include $(CLEAR_VARS)
 
+LOCAL_MODULE_PATH := vendor/transgaming/swiftshader/$(TARGET_ARCH)/debug
+LOCAL_MODULE := libGLESv2_swiftshader_vendor_debug
+LOCAL_INSTALLED_MODULE_STEM := libGLESv2_swiftshader.so
+LOCAL_CFLAGS += $(COMMON_CFLAGS) -UNDEBUG -g -O0
+
+LOCAL_CLANG := true
+LOCAL_SRC_FILES += $(COMMON_SRC_FILES)
+
+LOCAL_SHARED_LIBRARIES += $(COMMON_SHARED_LIBRARIES)
+LOCAL_STATIC_LIBRARIES += $(COMMON_STATIC_LIBRARIES)
+LOCAL_LDFLAGS += $(COMMON_LDFLAGS)
+LOCAL_C_INCLUDES += $(COMMON_C_INCLUDES)
+include external/stlport/libstlport.mk
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_PATH := vendor/transgaming/swiftshader/$(TARGET_ARCH)/release
+LOCAL_MODULE := libGLESv2_swiftshader_vendor_release
+LOCAL_INSTALLED_MODULE_STEM := libGLESv2_swiftshader.so
+LOCAL_CFLAGS += \
+	$(COMMON_CFLAGS) \
+	-fomit-frame-pointer \
+	-ffunction-sections \
+	-fdata-sections \
+	-DANGLE_DISABLE_TRACE
+
+LOCAL_CLANG := true
+LOCAL_SRC_FILES += $(COMMON_SRC_FILES)
+LOCAL_SHARED_LIBRARIES += $(COMMON_SHARED_LIBRARIES)
+LOCAL_STATIC_LIBRARIES += $(COMMON_STATIC_LIBRARIES)
+LOCAL_LDFLAGS += $(COMMON_LDFLAGS)
+LOCAL_C_INCLUDES += $(COMMON_C_INCLUDES)
+include external/stlport/libstlport.mk
 include $(BUILD_SHARED_LIBRARY)
--- src/gralloc/Android.mk
+++ src/gralloc/Android.mk
@@ -1,47 +1,50 @@
-# Copyright (C) 2008 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 LOCAL_PATH := $(call my-dir)
 
-# HAL module implemenation stored in
-# hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_INSTALLED_MODULE_STEM := gralloc.gce_x86.so
-
-LOCAL_SHARED_LIBRARIES := \
+COMMON_SHARED_LIBRARIES := \
     liblog \
     libutils \
     libcutils
 
-LOCAL_STATIC_LIBRARIES := \
+COMMON_STATIC_LIBRARIES := \
     libgceframebufferconfig \
     libgcemetadata \
     libyuv_static
 
-LOCAL_SRC_FILES := 	\
+COMMON_SRC_FILES := 	\
 	gralloc.cpp 	\
 	framebuffer.cpp \
 	mapper.cpp
 
-LOCAL_MODULE := gralloc_swiftshader.gce_x86
-LOCAL_CFLAGS:= -DLOG_TAG=\"gralloc_gce_x86\" -Wno-missing-field-initializers
+COMMON_CFLAGS:= -DLOG_TAG=\"gralloc_gce_x86\" -Wno-missing-field-initializers
 
-LOCAL_C_INCLUDES := \
+COMMON_C_INCLUDES := \
     device/google/gce/include \
     external/libyuv/files/include
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_PATH := vendor/transgaming/swiftshader/$(TARGET_ARCH)/debug
+LOCAL_MODULE := gralloc_swiftshader.gce_x86_vendor_debug
+LOCAL_INSTALLED_MODULE_STEM := gralloc.gce_x86.so
+LOCAL_CFLAGS += $(COMMON_CFLAGS) -UNDEBUG -g -O0
+
+LOCAL_SHARED_LIBRARIES += $(COMMON_SHARED_LIBRARIES)
+LOCAL_STATIC_LIBRARIES += $(COMMON_STATIC_LIBRARIES)
+LOCAL_SRC_FILES += $(COMMON_SRC_FILES)
+LOCAL_C_INCLUDES += $(COMMON_C_INCLUDES)
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_PATH := vendor/transgaming/swiftshader/$(TARGET_ARCH)/release
+LOCAL_MODULE := gralloc_swiftshader.gce_x86_vendor_release
+LOCAL_INSTALLED_MODULE_STEM := gralloc.gce_x86.so
+LOCAL_CFLAGS += $(COMMON_CFLAGS)
+
+LOCAL_SHARED_LIBRARIES += $(COMMON_SHARED_LIBRARIES)
+LOCAL_STATIC_LIBRARIES += $(COMMON_STATIC_LIBRARIES)
+LOCAL_SRC_FILES += $(COMMON_SRC_FILES)
+LOCAL_C_INCLUDES += $(COMMON_C_INCLUDES)
 
 include $(BUILD_SHARED_LIBRARY)
