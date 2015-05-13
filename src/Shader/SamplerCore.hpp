30628cb0a2e6bddb2529863da44f80c72327f116
Nicolas Capens <capn@google.com>
2015-05-13 17:30:57 -0400

    wip2-565-tex
    Change-Id: I91c702e620f466df07dd11ac59ceda99f726ec3f

--- src/Shader/SamplerCore.hpp
+++ src/Shader/SamplerCore.hpp
@@ -54,6 +54,8 @@ namespace sw
 		void convertSigned15(Float4 &cf, Short4 &ci);
 		void convertUnsigned16(Float4 &cf, Short4 &ci);
 		void sRGBtoLinear16_8_12(Short4 &c);
+		void sRGBtoLinear16_6_12(Short4 &c);
+		void sRGBtoLinear16_5_12(Short4 &c);
 
 		bool hasFloatTexture() const;
 		bool hasUnsignedTextureComponent(int component) const;
