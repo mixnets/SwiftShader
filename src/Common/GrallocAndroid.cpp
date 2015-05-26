34527d3428dfa5cb1ff92eb6498317cdf87e202f
Nicolas Capens <capn@google.com>
2015-05-25 23:27:55 -0400

    Disable R5G6B5 conversion in gralloc.
    Bug 20891368
    
    Change-Id: Ie293159d2092985c496202eb25733e69f9a3ff6f

--- src/Common/GrallocAndroid.cpp
+++ src/Common/GrallocAndroid.cpp
@@ -10,19 +10,9 @@ GrallocModule* GrallocModule::getInstance()
 
 GrallocModule::GrallocModule()
 {
-    const hw_module_t* module;
-    hw_get_module("converting_gralloc", &module);
-    if (module)
-    {
-        m_supportsConversion = true;
-        ALOGI("Loaded converting gralloc");
-    }
-    else
-    {
-        m_supportsConversion = false;
-        ALOGE("Falling back to standard gralloc with reduced format support");
-        hw_get_module(GRALLOC_HARDWARE_MODULE_ID, &module);
-    }
+    const hw_module_t *module = 0;
+    hw_get_module(GRALLOC_HARDWARE_MODULE_ID, &module);
+
     if (!module)
     {
         ALOGE("Failed to load standard gralloc");
