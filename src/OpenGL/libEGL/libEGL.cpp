cf708ac34118499ee1483ca45b6524acedd9cce1
Greg Hartman <ghartman@google.com>
2015-06-18 21:06:54 -0700

    Implementation of the fence sync.
    Bug 21662268
    
    Change-Id: Ieeb8930bd2ff2c457e6aab37a0857ccdfb473380

--- src/OpenGL/libEGL/libEGL.cpp
+++ src/OpenGL/libEGL/libEGL.cpp
@@ -21,6 +21,10 @@
 #include "Common/Version.h"
 
 #if defined(__ANDROID__)
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <cutils/log.h>
 #include <system/window.h>
 #endif
 
@@ -179,7 +183,8 @@ const char *QueryString(EGLDisplay dpy, EGLint name)
 		               "EGL_KHR_gl_renderbuffer_image "
 		               "EGL_KHR_image_base "
 		               "EGL_ANDROID_framebuffer_target "
-		               "EGL_ANDROID_recordable");
+		               "EGL_ANDROID_recordable "
+                               "EGL_KHR_fence_sync");
 	case EGL_VENDOR:
 		return success("TransGaming Inc.");
 	case EGL_VERSION:
@@ -960,6 +965,128 @@ EGLSurface CreatePlatformPixmapSurfaceEXT(EGLDisplay dpy, EGLConfig config, void
 	return CreatePixmapSurface(dpy, config, (EGLNativePixmapType)native_pixmap, attrib_list);
 }
 
+class FenceSync
+{
+public:
+	explicit FenceSync(Context* context)
+	{
+		signaled_ = EGL_UNSIGNALED_KHR;
+		context->addRef();
+		context_ = context;
+	}
+
+	~FenceSync()
+	{
+		context_->release();
+		context_ = nullptr;
+	}
+
+	Context* getContext() const { return context_; }
+	void signal() { signaled_ = EGL_SIGNALED_KHR; }
+	bool isSignaled() const { return signaled_ == EGL_SIGNALED_KHR; }
+
+private:
+	EGLint signaled_;
+	Context* context_;
+};
+
+EGLSyncKHR CreateSyncKHR(EGLDisplay dpy, EGLenum type, const EGLint *attrib_list)
+{
+	TRACE("(EGLDisplay dpy = %p, EGLunum type = %x, EGLint *attrib_list=%p)", dpy, type, attrib_list);
+	egl::Display *display = static_cast<egl::Display*>(dpy);
+	if (!validateDisplay(display))
+	{
+		return error(EGL_BAD_DISPLAY, EGL_NO_SYNC_KHR);
+	}
+
+	if (type != EGL_SYNC_FENCE_KHR)
+	{
+		UNREACHABLE(type);
+		return error(EGL_BAD_ATTRIBUTE, EGL_NO_SYNC_KHR);
+	}
+
+	if (attrib_list != NULL && attrib_list[0] != EGL_NONE)
+	{
+		UNREACHABLE(attrib_list[0]);
+		return error(EGL_BAD_ATTRIBUTE, EGL_NO_SYNC_KHR);
+	}
+
+	egl::Context *context = static_cast<egl::Context*>(egl::getCurrentContext());
+	if (!validateContext(display, context))
+	{
+		return error(EGL_BAD_MATCH, EGL_NO_SYNC_KHR);
+	}
+	return new FenceSync(context);
+}
+
+EGLBoolean DestroySyncKHR(EGLDisplay dpy, EGLSyncKHR sync)
+{
+	TRACE("(EGLDisplay dpy = %p, EGLSyncKHR sync = %p)", dpy, sync);
+	egl::Display *display = static_cast<egl::Display*>(dpy);
+	if (!validateDisplay(display))
+	{
+		return error(EGL_BAD_DISPLAY, EGL_FALSE);
+	}
+	FenceSync *eglSync = static_cast<FenceSync*>(sync);
+
+	delete(eglSync);
+
+	return EGL_TRUE;
+}
+
+EGLint ClientWaitSyncKHR(EGLDisplay dpy, EGLSyncKHR sync, EGLint flags,
+		EGLTimeKHR timeout)
+{
+	TRACE("(EGLDisplay dpy = %p, EGLSyncKHR sync = %p, EGLint flags = %x, EGLTimeKHR value = %llx)", dpy, sync, flags, timeout);
+	egl::Display *display = static_cast<egl::Display*>(dpy);
+	if (!validateDisplay(display))
+	{
+		return error(EGL_BAD_DISPLAY, EGL_FALSE);
+	}
+	FenceSync *eglSync = static_cast<FenceSync*>(sync);
+
+	(void)flags;
+	(void)timeout;
+
+	if (!eglSync->isSignaled())
+	{
+		eglSync->getContext()->finish();
+		eglSync->signal();
+	}
+
+	return EGL_CONDITION_SATISFIED_KHR;
+}
+
+EGLBoolean GetSyncAttribKHR(EGLDisplay dpy, EGLSyncKHR sync,
+		EGLint attribute, EGLint *value)
+{
+	TRACE("(EGLDisplay dpy = %p, EGLSyncKHR sync = %p, EGLint attribute = %x, EGLint *value = %p)", dpy, sync, attribute, value);
+	egl::Display *display = static_cast<egl::Display*>(dpy);
+	if (!validateDisplay(display))
+	{
+		return error(EGL_BAD_DISPLAY, EGL_FALSE);
+	}
+	FenceSync *eglSync = static_cast<FenceSync*>(sync);
+
+	switch (attribute) {
+	case EGL_SYNC_TYPE_KHR:
+		*value = EGL_SYNC_FENCE_KHR;
+		return EGL_TRUE;
+	case EGL_SYNC_STATUS_KHR:
+		*value = eglSync->isSignaled() ? EGL_SIGNALED_KHR : EGL_UNSIGNALED_KHR;
+		return EGL_TRUE;
+	case EGL_SYNC_CONDITION_KHR:
+		*value = EGL_SYNC_PRIOR_COMMANDS_COMPLETE_KHR;
+		return EGL_TRUE;
+	default:
+#ifdef __ANDROID__
+		ALOGE("%s: badness requested unknown attribute %x",
+			  __FUNCTION__, attribute);
+#endif
+		return error(EGL_BAD_ATTRIBUTE, EGL_FALSE);
+	}
+}
+
 __eglMustCastToProperFunctionPointerType GetProcAddress(const char *procname)
 {
 	TRACE("(const char *procname = \"%s\")", procname);
@@ -979,6 +1106,10 @@ __eglMustCastToProperFunctionPointerType GetProcAddress(const char *procname)
 		EXTENSION(eglGetPlatformDisplayEXT),
 		EXTENSION(eglCreatePlatformWindowSurfaceEXT),
 		EXTENSION(eglCreatePlatformPixmapSurfaceEXT),
+		EXTENSION(eglCreateSyncKHR),
+		EXTENSION(eglDestroySyncKHR),
+		EXTENSION(eglClientWaitSyncKHR),
+		EXTENSION(eglGetSyncAttribKHR),
 
 		#undef EXTENSION
 	};
