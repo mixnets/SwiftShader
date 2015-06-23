b15453a3bad428984e1a002b328f531fe6accd1d
Nicolas Capens <capn@google.com>
2015-06-23 15:16:58 -0400

    raster-float
    Change-Id: Ifff8e6085866fd72a96471c4539e675e615b695c

--- src/Shader/SetupRoutine.cpp
+++ src/Shader/SetupRoutine.cpp
@@ -581,40 +581,18 @@ namespace sw
 				Pointer<Byte> rightEdge = primitive + q * sizeof(Primitive) + OFFSET(Primitive,outline->right);
 				Pointer<Byte> edge = IfThenElse(swap, rightEdge, leftEdge);
 
-				// Deltas
 				Int DX12 = X2 - X1;
 				Int DY12 = Y2 - Y1;
+				Float slope = Float(DX12) / Float(DY12);
 
-				Int FDX12 = DX12 << 4;
-				Int FDY12 = DY12 << 4;
-
-				Int X = DX12 * ((y1 << 4) - Y1) + X1 * DY12;
-				Int x = X / FDY12;     // Edge
-				Int d = X % FDY12;     // Error-term
-				Int ceil = -d >> 31;   // Ceiling division: remainder <= 0
-				x -= ceil;
-				d -= ceil & FDY12;
-				
-				Int Q = FDX12 / FDY12;   // Edge-step
-				Int R = FDX12 % FDY12;   // Error-step
-				Int floor = R >> 31;     // Flooring division: remainder >= 0
-				Q += floor;
-				R += floor & FDY12;
-
-				Int D = FDY12;   // Error-overflow
 				Int y = y1;
 
 				Do
 				{
-					*Pointer<Short>(edge + y * sizeof(Primitive::Span)) = Short(Clamp(x, xMin, xMax));
+					Float dy = Float((y << 4) - Y1);
+					Float x = (1.0f / 16.0f) * (Float(X1) + (dy * slope));
 
-					x += Q;
-					d += R;
-
-					Int overflow = -d >> 31;
-			
-					d -= D & overflow;
-					x -= overflow;
+					*Pointer<Short>(edge + y * sizeof(Primitive::Span)) = Short(Clamp(Int(Ceil(x)), xMin, xMax));
 
 					y++;
 				}
