3ad1a49310d4af42d9c212881b2b71e56634fa34
Alexis Hetu <sugoi@google.com>
2015-07-22 17:03:26 -0400

    TextureSize implementation
    Initial TextureSize parsing and implementation
    
    Change-Id: I8b9b1808366b1013a5001e2dfa15a26d8471ab6a

--- src/Shader/VertexProgram.cpp
+++ src/Shader/VertexProgram.cpp
@@ -283,6 +283,7 @@ namespace sw
 			case Shader::OPCODE_NE:         notEqual(d, s0, s1);            break;
 			case Shader::OPCODE_TEXLDL:		TEXLDL(r, d, s0, src1);			break;
 			case Shader::OPCODE_TEX:		TEX(r, d, s0, src1);			break;
+			case Shader::OPCODE_TEXSIZE:	TEXSIZE(r, d, s0.x, src1);		break;
 			case Shader::OPCODE_END:										break;
 			default:
 				ASSERT(false);
@@ -1479,6 +1480,19 @@ namespace sw
 		dst.y = tmp[(src1.swizzle >> 2) & 0x3];
 		dst.z = tmp[(src1.swizzle >> 4) & 0x3];
 		dst.w = tmp[(src1.swizzle >> 6) & 0x3];
+	}
+
+	void VertexProgram::TEXSIZE(Registers &r, Vector4f &dst, Float4 &lod, const Src &src1)
+	{
+		Pointer<Byte> textureMipmap = r.data + OFFSET(DrawData, mipmap[16]) + src1.index * sizeof(Texture) + OFFSET(Texture, mipmap);
+		Float lodBias[4] = { lod.x, lod.y, lod.z, lod.w };
+		for(int i = 0; i < 4; ++i)
+		{
+			Pointer<Byte> mipmap = textureMipmap + (As<Int>(lodBias[i]) + Int(1)) * sizeof(Mipmap);
+			dst.x = Insert(dst.x, As<Float>(Int(*Pointer<Short>(mipmap + OFFSET(Mipmap, width)))), i);
+			dst.y = Insert(dst.y, As<Float>(Int(*Pointer<Short>(mipmap + OFFSET(Mipmap, height)))), i);
+			dst.z = Insert(dst.z, As<Float>(Int(*Pointer<Short>(mipmap + OFFSET(Mipmap, depth)))), i);
+		}
 	}
 
 	void VertexProgram::sampleTexture(Registers &r, Vector4f &c, const Src &s, Float4 &u, Float4 &v, Float4 &w, Float4 &q)
