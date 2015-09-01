285ff09b972f5f88c2439a51f4f224f32a725e2c
Alexis Hetu <sugoi@google.com>
2015-09-01 10:58:37 -0400

    Handling ETC2, EAC and ASTC related enums
    Note that no decoding functionality is added here.
    This cl merely acknowledges textures of the ETC2, EAC
    and ASTC format by handling their related enums in
    SwiftShader, and the byte data is then allowed to be
    passed down all the way to the Surface object, where
    the decoding code will be added. Also note that this
    cl does not add the extensions strings required for
    ASTC support, so ASTC is still unsupported after this cl.
    
    Change-Id: I1d8aed0fb64b0d4c72846e87410750d1e485c46b

--- src/OpenGL/libGLESv2/utilities.cpp
+++ src/OpenGL/libGLESv2/utilities.cpp
@@ -332,7 +332,35 @@ namespace es2
                format == GL_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2 ||
                format == GL_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2 ||
                format == GL_COMPRESSED_RGBA8_ETC2_EAC ||
-               format == GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC;
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC ||
+               format == GL_COMPRESSED_RGBA_ASTC_4x4_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_5x4_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_5x5_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_6x5_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_6x6_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_8x5_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_8x6_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_8x8_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_10x5_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_10x6_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_10x8_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_10x10_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_12x10_KHR ||
+               format == GL_COMPRESSED_RGBA_ASTC_12x12_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_4x4_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_5x4_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_5x5_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_6x5_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_6x6_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_8x5_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_8x6_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_8x8_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_10x5_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_10x6_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_10x8_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_10x10_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_12x10_KHR ||
+               format == GL_COMPRESSED_SRGB8_ALPHA8_ASTC_12x12_KHR;
 	}
 
 	bool IsDepthTexture(GLenum format)
