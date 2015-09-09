393f2300af15f64360d8b8b30d593df51648c1a1
Alexis Hetu <sugoi@google.com>
2015-09-09 14:25:40 -0400

    ASTC decoder
    This cl adds a partial checkout of the astc-encoder github repo
    and uses that code to perform ASTC decoding. Currently, there's
    no need for ASTC encoding, so that part of the repo was not
    included. The sRGB conversion is not done in the decoder, but
    rather done in place, after the decoding is done, directly inside Surface::decodeASTC(). All related dEQP tests pass.
    
    Change-Id: I5f29ec2aa57b04871e839e27a858d5df463f26bd

---
+++ src/Renderer/ASTC_Decoder.cpp
@@ -0,0 +1,82 @@
+// Copyright 2016 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ASTC_Decoder.hpp"
+#include "astc_codec_internals.h"
+
+void write_imageblock32(unsigned char* img, const imageblock * pb,	// picture-block to initialize with image data. We assume that orig_data is valid.
+	// output dimensions
+	int xsize, int ysize, int zsize, int destPitchB, int destSliceB,
+	// block dimensions
+	int xdim, int ydim, int zdim,
+	// position to write the block to
+	int xpos, int ypos, int zpos)
+{
+	const float *fptr = pb->orig_data;
+	const uint8_t *nptr = pb->nan_texel;
+
+	union pixelData
+	{
+		unsigned int i;
+		float f;
+	};
+
+	for(int z = 0; z < zdim; z++)
+	{
+		for(int y = 0; y < ydim; y++)
+		{
+			for(int x = 0; x < xdim; x++)
+			{
+				int xi = xpos + x;
+				int yi = ypos + y;
+				int zi = zpos + z;
+
+				if(xi >= 0 && yi >= 0 && zi >= 0 && xi < xsize && yi < ysize && zi < zsize)
+				{
+					pixelData* pix = (pixelData*)(&img[zi * destSliceB + yi * destPitchB + xi * 16]);
+
+					if(*nptr)
+					{
+						pix[0].i = pix[1].i = pix[2].i = pix[3].i = 0xFFFFFFFF;
+					}
+					else
+					{
+						pix[0].f = fptr[0];
+						pix[1].f = fptr[1];
+						pix[2].f = fptr[2];
+						pix[3].f = fptr[3];
+					}
+				}
+				fptr += 4;
+				nptr++;
+			}
+		}
+	}
+}
+
+void ASTC_Decoder::DecodeBlock(const unsigned char *source, unsigned char* dest,
+                               int destWidth, int destHeight, int destDepth,
+                               int destPitchB, int destSliceB,
+                               int xBlockSize, int yBlockSize, int zBlockSize,
+                               int x, int y, int z)
+{
+	build_quantization_mode_table();
+
+	imageblock pb;
+	symbolic_compressed_block scb;
+	physical_compressed_block pcb = *(physical_compressed_block *)source;
+	physical_to_symbolic(xBlockSize, yBlockSize, zBlockSize, pcb, &scb);
+	decompress_symbolic_block(DECODE_HDR, xBlockSize, yBlockSize, zBlockSize, x * xBlockSize, y * yBlockSize, z * zBlockSize, &scb, &pb);
+	write_imageblock32(dest, &pb, destWidth, destHeight, destDepth, destPitchB, destSliceB, xBlockSize, yBlockSize, zBlockSize, x * xBlockSize, y * yBlockSize, z * zBlockSize);
+}
