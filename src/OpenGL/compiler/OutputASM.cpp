04488d721f5c0aae735a232f3836be7fcb72674b
Alexis Hetu <sugoi@google.com>
2015-09-11 15:06:48 -0400

    Fixed non square matrix multiplication
    A few cases of matrix multiplication weren't working properly
    due to using the wrong matrix dimension. For example, if we
    have the following matrix operation (using matColRow):
    mat32 = mat22 * mat32
    This results in 3 multiplication of 2 element vectors for each
    row of the result and the current code (before this cl) would
    only perform 2 multiplications and produce the wrong result.
    
    This cl should fix all uses of the * operator and of the
    matrixCompMult function when using non square matrices.
    
    Change-Id: Id0dbfd9d65c20102220049c34435c37e3db7f9da

--- src/OpenGL/compiler/OutputASM.cpp
+++ src/OpenGL/compiler/OutputASM.cpp
@@ -533,11 +533,12 @@ namespace glsl
 		case EOpVectorTimesMatrix:
 			if(visit == PostVisit)
 			{
-				int size = leftType.getNominalSize();
+				sw::Shader::Opcode dpOpcode = sw::Shader::OPCODE_DP(leftType.getNominalSize());
 
+				int size = rightType.getNominalSize();
 				for(int i = 0; i < size; i++)
 				{
-					Instruction *dot = emit(sw::Shader::OPCODE_DP(size), result, left, right);
+					Instruction *dot = emit(dpOpcode, result, left, right);
 					dot->dst.mask = 1 << i;
 					argument(dot->src[1], right, i);
 				}
@@ -549,7 +550,8 @@ namespace glsl
 				Instruction *mul = emit(sw::Shader::OPCODE_MUL, result, left, right);
 				mul->src[1].swizzle = 0x00;
 
-				for(int i = 1; i < leftType.getNominalSize(); i++)
+				int size = rightType.getNominalSize();
+				for(int i = 1; i < size; i++)
 				{
 					Instruction *mad = emit(sw::Shader::OPCODE_MAD, result, left, right, result);
 					argument(mad->src[0], left, i);
@@ -562,7 +564,8 @@ namespace glsl
 			{
 				int dim = leftType.getNominalSize();
 
-				for(int i = 0; i < dim; i++)
+				int size = rightType.getNominalSize();
+				for(int i = 0; i < size; i++)
 				{
 					Instruction *mul = emit(sw::Shader::OPCODE_MUL, result, left, right);
 					mul->dst.index += i;
@@ -1211,7 +1214,7 @@ namespace glsl
 			{
 				ASSERT(dim2(arg[0]) == dim2(arg[1]));
 
-				for(int i = 0; i < dim2(arg[0]); i++)
+				for(int i = 0; i < dim(arg[0]); i++)
 				{
 					Instruction *mul = emit(sw::Shader::OPCODE_MUL, result, arg[0], arg[1]);
 					mul->dst.index += i;
