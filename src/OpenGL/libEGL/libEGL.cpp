102fba0c7fc49523781ef7f4e53df66c1678d2d2
Nicolas Capens <capn@google.com>
2015-09-24 16:25:06 -0400

    Implementation of EGL_KHR_swap_buffers_with_damage.
    This is a trivial implementation which does not actually optimize
    compositing using the damage rects. That can be implemented later.
    
    Change-Id: Ibf511ef166a33b1d2f1826bfc5618765062fe2a2

--- src/OpenGL/libEGL/libEGL.cpp
+++ src/OpenGL/libEGL/libEGL.cpp
@@ -179,6 +179,7 @@ const char *QueryString(EGLDisplay dpy, EGLint name)
 		               "EGL_KHR_gl_renderbuffer_image "
 		               "EGL_KHR_fence_sync "
 		               "EGL_KHR_image_base "
+		               "EGL_KHR_swap_buffers_with_damage "
 		               "EGL_ANDROID_framebuffer_target "
 		               "EGL_ANDROID_recordable");
 	case EGL_VENDOR:
@@ -942,6 +943,11 @@ EGLBoolean DestroyImageKHR(EGLDisplay dpy, EGLImageKHR image)
 	glImage->destroyShared();
 
 	return success(EGL_TRUE);
+}
+
+EGLBoolean SwapBuffersWithDamageKHR(EGLDisplay dpy, EGLSurface surface, EGLint *rects, EGLint n_rects)
+{
+	return eglSwapBuffers(dpy, surface);   // FIXME: Pass damage rects to compositor for optimization
 }
 
 EGLDisplay GetPlatformDisplayEXT(EGLenum platform, void *native_display, const EGLint *attrib_list)
