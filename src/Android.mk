f90b04c9118799d624a1bbd30d381ce7accf856b
Greg Hartman <ghartman@google.com>
2015-11-12 16:38:42 -0800

    Fix go/ab builds for jb-mr1
    This forces clang to avoid use init_array for all initialization rather than
    splitting the initialization between init_array and ctors.
    
    Local builds do this anyway, but for some reason the builds on go/ab do not.
    Setting this flag makes the builds consistent (and unbroken on JB-MR1).
    
    I already checked this in on cloud-android-current-release because I wanted
    to test it before submitting.
    
    Bug 25597090
    
    Change-Id: I5a7195ba53531835fb8333b698ed39d21ff847a6
    (cherry picked from commit 95c7182f118cc686df27c3f49db7d3c7908e0e59)

--- src/Android.mk
+++ src/Android.mk
@@ -82,11 +82,11 @@ COMMON_SRC_FILES += \
 	OpenGL/common/Object.cpp \
 	OpenGL/common/MatrixStack.cpp \
 
-COMMON_CFLAGS := -DLOG_TAG=\"swiftshader\" -Wno-unused-parameter -Wno-implicit-exception-spec-mismatch -Wno-overloaded-virtual -fno-operator-names -msse2 -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -std=c++11
-
 ifneq ($(filter gce_x86 gce calypso, $(TARGET_DEVICE)),)
 COMMON_CFLAGS += -DDISPLAY_LOGO=0
 endif
+
+COMMON_CFLAGS := -DLOG_TAG=\"swiftshader\" -Wno-unused-parameter -Wno-implicit-exception-spec-mismatch -Wno-overloaded-virtual -fno-operator-names -msse2 -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -std=c++11 -Xclang -fuse-init-array
 
 include $(CLEAR_VARS)
 LOCAL_CLANG := true
