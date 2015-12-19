cc7304606d3325f8d30ac425a3d7c5012785201f
Nicolas Capens <capn@google.com>
2015-12-19 01:48:43 -0500

    WIP-retina
    Change-Id: I712b089c36e9c696c44a8d99a3d21819798a6142

--- src/Main/FrameBufferOSX.hpp
+++ src/Main/FrameBufferOSX.hpp
@@ -9,27 +9,27 @@
 
 namespace sw
 {
-    class FrameBufferOSX : public FrameBuffer
-    {
-    public:
-        FrameBufferOSX(CALayer* layer, int width, int height);
-        ~FrameBufferOSX() override;
+	class FrameBufferOSX : public FrameBuffer
+	{
+	public:
+		FrameBufferOSX(CALayer *layer, int width, int height);
+		~FrameBufferOSX() override;
 
-        void flip(void *source, Format sourceFormat, size_t sourceStride) override;
-        void blit(void *source, const Rect *sourceRect, const Rect *destRect, Format sourceFormat, size_t sourceStride) override;
+		void flip(void *source, Format sourceFormat, size_t sourceStride) override;
+		void blit(void *source, const Rect *sourceRect, const Rect *destRect, Format sourceFormat, size_t sourceStride) override;
 
-        void *lock() override;
-        void unlock() override;
+		void *lock() override;
+		void unlock() override;
 
-    private:
-        int width;
-        int height;
-        CALayer* layer;
-        uint8_t* buffer;
-        CGDataProviderRef provider;
-        CGColorSpaceRef colorspace;
-        CGImageRef currentImage;
-    };
+	private:
+		int width;
+		int height;
+		CALayer *layer;
+		uint8_t *buffer;
+		CGDataProviderRef provider;
+		CGColorSpaceRef colorspace;
+		CGImageRef currentImage;
+	};
 }
 
 #endif   // sw_FrameBufferOSX
--- src/OpenGL/libGLESv2/libGLESv2.hpp
+++ src/OpenGL/libGLESv2/libGLESv2.hpp
@@ -262,7 +262,11 @@ private:
 		if(!libGLESv2)
 		{
 			#if defined(_WIN32)
-				const char *libGLESv2_lib[] = {"libGLESv2.dll", "libGLES_V2_translator.dll"};
+				#if defined(__LP64__)
+					const char *libGLESv2_lib[] = {"libGLESv2.dll", "lib64GLES_V2_translator.dll"};
+				#else
+					const char *libGLESv2_lib[] = {"libGLESv2.dll", "libGLES_V2_translator.dll"};
+				#endif
 			#elif defined(__ANDROID__)
 				#if defined(__LP64__)
 					const char *libGLESv2_lib[] = {"/vendor/lib64/egl/libGLESv2_swiftshader.so"};
@@ -276,7 +280,11 @@ private:
 					const char *libGLESv2_lib[] = {"libGLES_V2_translator.so", "libGLESv2.so.2", "libGLESv2.so"};
 				#endif
 			#elif defined(__APPLE__)
-				const char *libGLESv2_lib[] = {"libGLES_V2_translator.dylib", "libGLESv2.dylib"};
+				#if defined(__LP64__)
+					const char *libGLESv2_lib[] = {"lib64GLES_V2_translator.dylib", "libGLESv2.dylib"};
+				#else
+					const char *libGLESv2_lib[] = {"libGLES_V2_translator.dylib", "libGLESv2.dylib"};
+				#endif
 			#else
 				#error "libGLESv2::loadExports unimplemented for this platform"
 			#endif
