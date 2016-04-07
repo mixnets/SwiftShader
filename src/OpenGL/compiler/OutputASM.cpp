d9daba2f1a1c806f9f2c643ce443e835c780689b
Nicolas Capens <capn@google.com>
2016-04-07 01:03:14 -0400

    Constants are constant expressions, but not vice versa.
    Constant expressions can be left unfolded and thus not be constants.
    Make sure constants are always recognized as constant expressions.
    
    Change-Id: Iabcf2761ee3a9bdaca0cd09682a9cf56f0b908c6

--- src/OpenGL/compiler/OutputASM.cpp
+++ src/OpenGL/compiler/OutputASM.cpp
@@ -2072,7 +2072,7 @@ namespace glsl
 			parameter.type = registerType(arg);
 			parameter.bufferIndex = argumentInfo.bufferIndex;
 
-			if(arg->getQualifier() == EvqConstExpr)
+			if(arg->getAsConstantUnion() && arg->getAsConstantUnion()->getUnionArrayPointer())
 			{
 				int component = componentCount(type, argumentInfo.clampedIndex);
 				ConstantUnion *constants = arg->getAsConstantUnion()->getUnionArrayPointer();
@@ -2399,6 +2399,11 @@ namespace glsl
 			outputQualifier = qualifier;
 		}
 
+		if(qualifier == EvqConstExpr && (!operand->getAsConstantUnion() || !operand->getAsConstantUnion()->getUnionArrayPointer()))
+		{
+			return sw::Shader::PARAMETER_TEMP;
+		}
+
 		switch(qualifier)
 		{
 		case EvqTemporary:           return sw::Shader::PARAMETER_TEMP;
@@ -2450,7 +2455,7 @@ namespace glsl
 		{
 		case EvqTemporary:           return temporaryRegister(operand);
 		case EvqGlobal:              return temporaryRegister(operand);
-		case EvqConstExpr:           UNREACHABLE(EvqConstExpr);
+		case EvqConstExpr:           return temporaryRegister(operand);   // Unevaluated constant expression
 		case EvqAttribute:           return attributeRegister(operand);
 		case EvqVaryingIn:           return varyingRegister(operand);
 		case EvqVaryingOut:          return varyingRegister(operand);
