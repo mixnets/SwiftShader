8525bc715d4b19d0e072f2e6a18ec7c91fc1af9f
Nicolas Capens <capn@google.com>
2016-04-22 14:58:52 -0400

    Fix predicating instructions before return in loops.
    Only the instructions following a return statement were being marked as
    affected by it so predication would be applied. But in a loop the instructions
    above the return statement are also affected by it.
    
    Change-Id: If2490a6e0b4e9cf8b6e28b33cbbbcec8b4ebfdaa

--- src/Shader/Shader.cpp
+++ src/Shader/Shader.cpp
@@ -61,7 +61,7 @@ namespace sw
 		}
 	}
 
-	Shader::Opcode Shader::OPCODE_NRM(int i)	
+	Shader::Opcode Shader::OPCODE_NRM(int i)
 	{
 		switch(i)
 		{
@@ -116,7 +116,7 @@ namespace sw
 		predicate = false;
 		predicateNot = false;
 		predicateSwizzle = 0xE4;
-		
+
 		coissue = false;
 		samplerType = SAMPLER_UNKNOWN;
 		usage = USAGE_POSITION;
@@ -159,7 +159,7 @@ namespace sw
 					token++;
 					size--;
 				}
-				
+
 				token++;
 				size--;
 			}
@@ -170,7 +170,7 @@ namespace sw
 
 				predicateNot = (Modifier)((*token & 0x0F000000) >> 24) == MODIFIER_NOT;
 				predicateSwizzle = (unsigned char)((*token & 0x00FF0000) >> 16);
-				
+
 				token++;
 				size--;
 			}
@@ -198,11 +198,11 @@ namespace sw
 	std::string Shader::Instruction::string(ShaderType shaderType, unsigned short version) const
 	{
 		std::string instructionString;
-		
+
 		if(opcode != OPCODE_DCL)
 		{
 			instructionString += coissue ? "+ " : "";
-			
+
 			if(predicate)
 			{
 				instructionString += predicateNot ? "(!p0" : "(p0";
@@ -216,7 +216,7 @@ namespace sw
 			{
 				instructionString += " " + dst.string(shaderType, version) +
 				                           dst.relativeString() +
-				                           dst.maskString(); 
+				                           dst.maskString();
 			}
 
 			for(int i = 0; i < 4; i++)
@@ -226,8 +226,8 @@ namespace sw
 					instructionString += (dst.type != PARAMETER_VOID || i > 0) ? ", " : " ";
 					instructionString += src[i].preModifierString() +
 										 src[i].string(shaderType, version) +
-										 src[i].relativeString() + 
-										 src[i].postModifierString() + 
+										 src[i].relativeString() +
+										 src[i].postModifierString() +
 										 src[i].swizzleString();
 				}
 			}
@@ -348,10 +348,10 @@ namespace sw
 		{
 		case 0:		return "";
 		case 1:		return "_x2";
-		case 2:		return "_x4"; 
+		case 2:		return "_x4";
 		case 3:		return "_x8";
 		case -1:	return "_d2";
-		case -2:	return "_d4"; 
+		case -2:	return "_d4";
 		case -3:	return "_d8";
 		default:
 			return "";
@@ -627,7 +627,7 @@ namespace sw
 		src[i].rel.type = PARAMETER_VOID;
 		src[i].rel.swizzle = 0x00;
 		src[i].rel.scale = 1;
-		
+
 		switch(opcode)
 		{
 		case OPCODE_DEF:
@@ -1013,7 +1013,7 @@ namespace sw
 		else if(type != PARAMETER_RASTOUT && !(type == PARAMETER_ADDR && shaderType == SHADER_VERTEX) && type != PARAMETER_LOOP && type != PARAMETER_PREDICATE && type != PARAMETER_MISCTYPE)
 		{
 			buffer << index;
-			
+
 			return typeString(shaderType, version) + buffer.str();
 		}
 		else
@@ -1074,7 +1074,7 @@ namespace sw
 	{
 		return opcode == OPCODE_IF || opcode == OPCODE_IFC;
 	}
-	
+
 	bool Shader::Instruction::isCall() const
 	{
 		return opcode == OPCODE_CALL || opcode == OPCODE_CALLNZ;
@@ -1155,7 +1155,7 @@ namespace sw
 			token += 1 + tokenCount;
 		}
 	}
-	
+
 	int Shader::size(unsigned long opcode) const
 	{
 		return size(opcode, version);
@@ -1628,7 +1628,7 @@ namespace sw
 				{
 					containsLeave = true;
 				}
-				
+
 				if(instruction[i]->isBreak())
 				{
 					containsBreak = true;
@@ -1652,6 +1652,7 @@ namespace sw
 		int breakDepth = 0;
 		int continueDepth = 0;
 		bool leaveReturn = false;
+		int functionBegin = 0;
 
 		for(unsigned int i = 0; i < instruction.size(); i++)
 		{
@@ -1729,10 +1730,28 @@ namespace sw
 			if(instruction[i]->opcode == OPCODE_LEAVE)
 			{
 				leaveReturn = true;
+
+				// Mark loop body instructions prior to the return statement 
+				for(int l = functionBegin; l < i; l++)
+				{
+					if(instruction[l]->isLoop())
+					{
+						for(int r = l + 1; r < i; r++)
+						{
+							instruction[r]->analysisLeave = true;
+						}
+
+						break;
+					}
+				}
 			}
 			else if(instruction[i]->opcode == OPCODE_RET)   // End of the function
 			{
 				leaveReturn = false;
+			}
+			else if(instruction[i]->opcode == OPCODE_LABEL)
+			{
+				functionBegin = i;
 			}
 
 			if(leaveReturn)
