2707d8ec1a096e0f0e3fe9a39af8bcd9812f8244
Nicolas Capens <capn@google.com>
2016-09-28 10:36:57 -0400

    Don't return values from terminator instructions.
    All LLVM instructions are LLVM values, but in the case of terminator
    instructions their value is meaningless, and we don't use them.
    
    Bug swiftshader:10
    
    Change-Id: I9eb71e1dcea2614cb810021dfbf3c8908a7e2510

--- src/Reactor/Nucleus.hpp
+++ src/Reactor/Nucleus.hpp
@@ -64,10 +64,10 @@ namespace sw
 		static Value *getArgument(unsigned int index);
 
 		// Terminators
-		static Value *createRetVoid();
-		static Value *createRet(Value *V);
-		static Value *createBr(BasicBlock *dest);
-		static Value *createCondBr(Value *cond, BasicBlock *ifTrue, BasicBlock *ifFalse);
+		static void createRetVoid();
+		static void createRet(Value *V);
+		static void createBr(BasicBlock *dest);
+		static void createCondBr(Value *cond, BasicBlock *ifTrue, BasicBlock *ifFalse);
 
 		// Binary operators
 		static Value *createAdd(Value *lhs, Value *rhs);
