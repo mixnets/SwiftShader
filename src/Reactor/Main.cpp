cfb380289711307833ef0e010c5378f8629a5d59
Nicolas Capens <capn@google.com>
2016-09-30 14:57:16 -0400

    WIP-vector-emulation
    Change-Id: I065c52711719b12fe55cf0190e735240c008629e

--- src/Reactor/Main.cpp
+++ src/Reactor/Main.cpp
@@ -33,7 +33,7 @@ int reference(int *p, int y)
 	return sum;
 }
 
-TEST(SubzeroReactorSample, SubzeroReactor)
+TEST(SubzeroReactorTest, Sample)
 {
 	Routine *routine = nullptr;
 
@@ -67,6 +67,66 @@ TEST(SubzeroReactorSample, SubzeroReactor)
 			int one[2] = {1, 0};
 			int result = callable(&one[1], 2);
 			EXPECT_EQ(result, reference(&one[1], 2));
+		}
+	}
+
+	delete routine;
+}
+
+TEST(SubzeroReactorTest, SubVectorLoadStore)
+{
+	Routine *routine = nullptr;
+
+	{
+		Function<Int(Pointer<Byte>, Pointer<Byte>)> function;
+		{
+			Pointer<Byte> in = function.Arg<0>();
+			Pointer<Byte> out = function.Arg<1>();
+
+			*Pointer<Int4>(out + 16 * 0)   = *Pointer<Int4>(in + 16 * 0);
+			*Pointer<Short4>(out + 16 * 1) = *Pointer<Short4>(in + 16 * 1);
+			*Pointer<Byte8>(out + 16 * 2)  = *Pointer<Byte8>(in + 16 * 2);
+			*Pointer<Byte4>(out + 16 * 3)  = *Pointer<Byte4>(in + 16 * 3);
+			*Pointer<Int>(out + 16 * 4)    = *Pointer<Int>(in + 16 * 4);   // TODO: Short2
+   
+			Return(0);
+		}
+
+		routine = function(L"one");
+
+		if(routine)
+		{
+			int8_t in[16 * 5] = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16,
+			                     17, 18, 19, 20, 21, 22, 23, 24, -1, -1, -1, -1, -1, -1, -1, -1,
+			                     25, 26, 27, 28, 29, 30, 31, 32, -1, -1, -1, -1, -1, -1, -1, -1,
+			                     33, 34, 35, 36, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			                     37, 38, 39, 40, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+
+			int8_t out[16 * 5] = {0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+			                      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+			                      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+			                      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+			                      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
+		
+			int (*callable)(void*, void*) = (int(*)(void*,void*))routine->getEntry();
+			callable(in, out);
+
+			for(int row = 0; row < 5; row++)
+			{
+				for(int col = 0; col < 16; col++)
+				{
+					int i = row * 16 + col;
+
+					if(in[i] == -1)
+					{
+						EXPECT_EQ(out[i], 0) << "Row " << row << " column " << col <<  " not left untouched.";
+					}
+					else
+					{
+						EXPECT_EQ(out[i], in[i]) << "Row " << row << " column " << col << " not equal to input.";
+					}
+				}
+			}
 		}
 	}
 
