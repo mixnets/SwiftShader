90c0ba357ec0c4b6ed9149df8e6d663b7d098aa3
Gabryelle Chamberlin <gabryellec@google.com>
2017-06-07 15:12:37 -0400

    All functions to run DEQP tests implemented
    Change-Id: Id9ed8dc2f73702ed72f7127c5d91c8a469257c0c

---
+++ src/Vulkan/libVulkan/utils.cpp
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <map>
+#include "utils.h"
+#include "libVulkan.hpp"
+#include "Context.h"
+
+namespace vkutils
+{
+  void *findEntry(const char *name)
+	{
+		return vulkan::func_ptrs.find(std::string(name))->second;
+	}
+
+	void * Allocate(const VkAllocationCallbacks * pAlloc, size_t size, size_t align, VkSystemAllocationScope scope)
+	{
+		return pAlloc->pfnAllocation(pAlloc->pUserData, size, align, scope);
+	}
+
+	void * Alloc(const VkAllocationCallbacks * pParent, const VkAllocationCallbacks * pAlloc, size_t size, size_t align, VkSystemAllocationScope scope)
+	{
+		if (pAlloc)
+		{
+			return Allocate(pAlloc, size, align, scope);
+		}
+		else
+		{
+			return Allocate(pParent, size, align, scope);
+		}
+	}
+
+	void Free(VkAllocationCallbacks * pAlloc, void * pData)
+	{
+		if (pData == NULL)
+		{
+			return;
+		}
+
+		pAlloc->pfnFree(pAlloc->pUserData, pData);
+	}
+
+}
+
