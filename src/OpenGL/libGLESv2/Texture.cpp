f6d7304b443a12600becfe03db74db4a3601ab5c
Yahan Zhou <yahan@google.com>
2017-07-31 16:12:08 -0700

    wip: glGetTexImage basic support for GLES2
    This patch adds basic support for glGetTexImage in GLES2. It only
    supports 2D/Cube textures and a limited set of pixel types.
    
    Change-Id: I9417e4bbcbded467d4b0a087c80dc81787739713

--- src/OpenGL/libGLESv2/Texture.cpp
+++ src/OpenGL/libGLESv2/Texture.cpp
@@ -411,6 +411,15 @@ egl::Image *Texture::createSharedImage(GLenum target, unsigned int level)
 	return image;
 }
 
+void Texture::getImage(egl::Context *context, GLenum format, GLenum type, const egl::Image::PackInfo& packInfo, void *pixels, egl::Image *image)
+{
+	if(pixels && image)
+	{
+		GLsizei depth = (getTarget() == GL_TEXTURE_3D_OES || getTarget() == GL_TEXTURE_2D_ARRAY) ? image->getDepth() : 1;
+		image->saveImageData(context, 0, 0, 0, image->getWidth(), image->getHeight(), depth, format, type, packInfo, pixels);
+	}
+}
+
 void Texture::setImage(egl::Context *context, GLenum format, GLenum type, const egl::Image::UnpackInfo& unpackInfo, const void *pixels, egl::Image *image)
 {
 	if(pixels && image)
@@ -627,6 +636,16 @@ void Texture2D::setImage(egl::Context *context, GLint level, GLsizei width, GLsi
 	}
 
 	Texture::setImage(context, format, type, unpackInfo, pixels, image[level]);
+}
+
+void Texture2D::getImage(egl::Context *context, GLint level, GLsizei width, GLsizei height, GLenum format, GLenum type, const egl::Image::PackInfo& packInfo, void *pixels)
+{
+	if(!image[level])
+	{
+		return;
+	}
+
+	Texture::getImage(context, format, type, packInfo, pixels, image[level]);
 }
 
 void Texture2D::bindTexImage(gl::Surface *surface)
@@ -1333,6 +1352,18 @@ void TextureCubeMap::setImage(egl::Context *context, GLenum target, GLint level,
 	}
 
 	Texture::setImage(context, format, type, unpackInfo, pixels, image[face][level]);
+}
+
+void TextureCubeMap::getImage(egl::Context *context, GLenum target, GLint level, GLsizei width, GLsizei height, GLenum format, GLenum type, const egl::Image::PackInfo& packInfo, void *pixels)
+{
+	int face = CubeFaceIndex(target);
+
+	if(!image[face][level])
+	{
+		return;
+	}
+
+	Texture::getImage(context, format, type, packInfo, pixels, image[face][level]);
 }
 
 void TextureCubeMap::copyImage(GLenum target, GLint level, GLenum format, GLint x, GLint y, GLsizei width, GLsizei height, Framebuffer *source)
