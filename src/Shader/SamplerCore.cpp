4ba3a8b72afae0196046e175cab23563a811f7e7
Nicolas Capens <capn@google.com>
2017-11-21 11:49:26 -0500

    WIP-pass-nearest
    Change-Id: Ie9aabeac1108ebb7230bcd9bb1e75f231d6782a2

--- src/Shader/SamplerCore.cpp
+++ src/Shader/SamplerCore.cpp
@@ -316,7 +316,7 @@ namespace sw
 		{
 			// FIXME: YUV and sRGB are not supported by the floating point path
 			bool forceFloatFiltering = state.highPrecisionFiltering && !state.sRGB && !hasYuvFormat() && (state.textureFilter != FILTER_POINT);
-			if(hasFloatTexture() || hasUnnormalizedIntegerTexture() || forceFloatFiltering)   // FIXME: Mostly identical to integer sampling
+			if(hasFloatTexture() || hasUnnormalizedIntegerTexture() || forceFloatFiltering || state.textureType == TEXTURE_CUBE)   // FIXME: Mostly identical to integer sampling
 			{
 				Float4 uuuu = u;
 				Float4 vvvv = v;
@@ -653,7 +653,7 @@ namespace sw
 
 		Short4 borderMask;
 
-		if(state.addressingModeU == ADDRESSING_BORDER)
+	/*	if(state.addressingModeU == ADDRESSING_BORDER)
 		{
 			Short4 u0;
 
@@ -705,7 +705,7 @@ namespace sw
 			c.y = (borderMask & c.y) | (~borderMask & (*Pointer<Short4>(texture + OFFSET(Texture,borderColor4[1])) >> (hasUnsignedTextureComponent(1) ? 0 : 1)));
 			c.z = (borderMask & c.z) | (~borderMask & (*Pointer<Short4>(texture + OFFSET(Texture,borderColor4[2])) >> (hasUnsignedTextureComponent(2) ? 0 : 1)));
 			c.w = (borderMask & c.w) | (~borderMask & (*Pointer<Short4>(texture + OFFSET(Texture,borderColor4[3])) >> (hasUnsignedTextureComponent(3) ? 0 : 1)));
-		}
+		}*/
 
 		return c;
 	}
@@ -1125,7 +1125,7 @@ namespace sw
 
 		Int4 borderMask;
 
-		if(state.addressingModeU == ADDRESSING_BORDER)
+	/*	if(state.addressingModeU == ADDRESSING_BORDER)
 		{
 			Int4 u0;
 
@@ -1177,7 +1177,7 @@ namespace sw
 			c.y = As<Float4>((borderMask & As<Int4>(c.y)) | (~borderMask & *Pointer<Int4>(texture + OFFSET(Texture,borderColorF[1]))));
 			c.z = As<Float4>((borderMask & As<Int4>(c.z)) | (~borderMask & *Pointer<Int4>(texture + OFFSET(Texture,borderColorF[2]))));
 			c.w = As<Float4>((borderMask & As<Int4>(c.w)) | (~borderMask & *Pointer<Int4>(texture + OFFSET(Texture,borderColorF[3]))));
-		}
+		}*/
 
 		return c;
 	}
@@ -1712,7 +1712,7 @@ namespace sw
 		return As<Short4>(UShort4(tmp));
 	}
 
-	void SamplerCore::computeIndices(UInt index[4], Short4 uuuu, Short4 vvvv, Short4 wwww, Vector4f &offset, const Pointer<Byte> &mipmap, SamplerFunction function)
+	void SamplerCore::computeIndices(Int index[4], Short4 uuuu, Short4 vvvv, Short4 wwww, Vector4f &offset, const Pointer<Byte> &mipmap, SamplerFunction function)
 	{
 		bool texelFetch = (function == Fetch);
 		bool hasOffset = (function.option == Offset);
@@ -1781,27 +1781,27 @@ namespace sw
 
 			for(int i = 0; i < 4; i++)
 			{
-				index[i] = Min(Max(index[i], min), max);
+			/**///	index[i] = Min(Max(index[i], min), max);
 			}
 		}
 	}
 
-	void SamplerCore::computeIndices(UInt index[4], Int4& uuuu, Int4& vvvv, Int4& wwww, const Pointer<Byte> &mipmap, SamplerFunction function)
+	void SamplerCore::computeIndices(Int index[4], Int4& uuuu, Int4& vvvv, Int4& wwww, const Pointer<Byte> &mipmap, SamplerFunction function)
 	{
-		UInt4 indices = uuuu + vvvv;
+		Int4 indices = uuuu + vvvv;
 
 		if(hasThirdCoordinate())
 		{
-			indices += As<UInt4>(wwww);
+			indices += wwww;
 		}
 
 		for(int i = 0; i < 4; i++)
 		{
-			index[i] = Extract(As<Int4>(indices), i);
+			index[i] = Extract(indices, i);
 		}
 	}
 
-	Vector4s SamplerCore::sampleTexel(UInt index[4], Pointer<Byte> buffer[4])
+	Vector4s SamplerCore::sampleTexel(Int index[4], Pointer<Byte> buffer[4])
 	{
 		Vector4s c;
 
@@ -2039,7 +2039,7 @@ namespace sw
 	{
 		Vector4s c;
 
-		UInt index[4];
+		Int index[4];
 		computeIndices(index, uuuu, vvvv, wwww, offset, mipmap, function);
 
 		if(hasYuvFormat())
@@ -2148,7 +2148,7 @@ namespace sw
 	{
 		Vector4f c;
 
-		UInt index[4];
+		Int index[4];
 		computeIndices(index, uuuu, vvvv, wwww, mipmap, function);
 
 		if(hasFloatTexture() || has32bitIntegerTextureComponents())
@@ -2392,6 +2392,7 @@ namespace sw
 			switch(addressingMode)
 			{
 			case ADDRESSING_CLAMP:
+			case ADDRESSING_BORDER:
 				{
 					Float4 one = As<Float4>(Int4(oneBits));
 					coord *= Min(Max(uvw, Float4(0.0f)), one);
