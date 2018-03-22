a8bb3c5b9e6e4fe65cb3ec4c50b228025eda0e27
Nicolas Capens <capn@google.com>
2018-03-22 13:22:20 -0400

    WIP-copyteximage-format
    The OpenGL ES 3.0 spec section 3.8.5 states that:
    
    If internalformat is unsized, the internal format of the new texel array is determined
    by the following rules, applied in order. If an effective internal format exists
    that has
    1. the same component sizes as,
    2. component sizes greater than or equal to, or
    3. component sizes smaller than or equal to
    those of the source buffer’s effective internal format (for all matching components
    in internalformat), that format is chosen for the new image array, and this is also
    the new texel array’s effective internal format.
    
    Change-Id: Id243b963779108e205c275499ddfb6e041a873d6

--- src/OpenGL/libGLESv2/libGLESv2.cpp
+++ src/OpenGL/libGLESv2/libGLESv2.cpp
@@ -977,9 +977,21 @@ void CopyTexImage2D(GLenum target, GLint level, GLenum internalformat, GLint x, 
 			{
 				internalformat = colorbufferFormat;
 			}
-			else if(GetRedSize(colorbufferFormat) == 8)
+			else if(GetColorComponentType(colorbufferFormat) == GL_UNSIGNED_NORMALIZED && GetRedSize(colorbufferFormat) <= 8)
 			{
 				internalformat = gl::GetSizedInternalFormat(internalformat, GL_UNSIGNED_BYTE);
+			}
+			else if(GetColorComponentType(colorbufferFormat) == GL_INT)
+			{
+				internalformat = gl::GetSizedInternalFormat(internalformat, GL_INT);
+			}
+			else if(GetColorComponentType(colorbufferFormat) == GL_UNSIGNED_INT)
+			{
+				internalformat = gl::GetSizedInternalFormat(internalformat, GL_UNSIGNED_INT);
+			}
+			else if(GetColorComponentType(colorbufferFormat) == GL_FLOAT && GetRedSize(colorbufferFormat) == 16)   // GL_EXT_color_buffer_half_float
+			{
+				internalformat = gl::GetSizedInternalFormat(internalformat, GL_HALF_FLOAT_OES);
 			}
 			else
 			{
