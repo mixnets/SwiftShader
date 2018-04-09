46c9f9cd801cbf14ea261b1429944cf75fc3762c
Nicolas Capens <capn@google.com>
2018-04-09 16:27:20 -0400

    Move ConvertReadFormatType to utilities.
    Bug swiftshader:104
    
    Change-Id: Ifcdffd3f960d7bb4982495381465da15ef671c3f

--- src/OpenGL/libGLESv2/utilities.h
+++ src/OpenGL/libGLESv2/utilities.h
@@ -57,6 +57,7 @@ namespace es2
 	bool IsTextureTarget(GLenum target);
 	GLenum ValidateTextureFormatType(GLenum format, GLenum type, GLint internalformat, GLenum target, GLint clientVersion);
 	size_t GetTypeSize(GLenum type);
+	sw::Format ConvertReadFormatType(GLenum format, GLenum type);
 
 	bool IsColorRenderable(GLint internalformat, GLint clientVersion);
 	bool IsDepthRenderable(GLint internalformat, GLint clientVersion);
