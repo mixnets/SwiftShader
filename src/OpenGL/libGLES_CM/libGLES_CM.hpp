cd36d9b0da554ec2828b5aefc63f31d844611510
Nicolas Capens <capn@google.com>
2018-06-21 01:42:30 -0400

    Newly load sibling library from the same directory.
    Previously we were only attempting to obtain a handle to an already
    loaded sibling library residing in the same directory. If it wasn't
    already pre-loaded, we only loaded the library by name without any
    directory prefix. This could lead to not finding the correct library.
    
    Also, it's fine if the directory prefix is empty, so there's no need for
    duplicate loops.
    
    Bug b/110407478
    
    Change-Id: Ia55b426e58b9f4039064bba9253dae80e4fdf5cf

--- src/OpenGL/libGLES_CM/libGLES_CM.hpp
+++ src/OpenGL/libGLES_CM/libGLES_CM.hpp
@@ -261,9 +261,9 @@ private:
 				#endif
 			#elif defined(__ANDROID__)
 				#if defined(__LP64__)
-					const char *libGLES_CM_lib[] = {"/vendor/lib64/egl/libGLESv1_CM_swiftshader.so", "/system/lib64/egl/libGLESv1_CM_swiftshader.so"};
+					const char *libGLES_CM_lib[] = {"libGLESv1_CM_swiftshader.so", "libGLESv1_CM_swiftshader.so"};
 				#else
-					const char *libGLES_CM_lib[] = {"/vendor/lib/egl/libGLESv1_CM_swiftshader.so", "/system/lib/egl/libGLESv1_CM_swiftshader.so"};
+					const char *libGLES_CM_lib[] = {"libGLESv1_CM_swiftshader.so", "libGLESv1_CM_swiftshader.so"};
 				#endif
 			#elif defined(__linux__)
 				#if defined(__LP64__)
