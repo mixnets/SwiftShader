e5c16cceeee1ed3d723c2a5d1adbc6d78d3eabe6
Alexis Hetu <sugoi@google.com>
2018-08-31 16:45:54 -0400

    Initial empty Vulkan API
    Initial version of the VulkanAPI, empty and unimplemented.
    Successfully builds vk_swiftshader.dll for all platforms/configuration in Visual Studio.
    
    To test using dEQP:
    - Edit environment variables
    	- Define VK_ICD_FILENAMES to <SwiftShader's source directory>\src\Vulkan\vk_swiftshader_icd.json
    - If the location of vk_swiftshader.dll you're using is different than the one specified in
      src\Vulkan\vk_swiftshader_icd.json, modify it to point to the vk_swiftshader.dll file you want to use
    
    Change-Id: I36b5fc665cf5addc5ccf77c9e940a7e3c60436d6

---
+++ src/Vulkan/VkQueue.cpp
@@ -0,0 +1,45 @@
+// Copyright 2018 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "VkQueue.hpp"
+#include "VkCommandBuffer.hpp"
+#include "VkFence.hpp"
+
+namespace vk
+{
+
+void Queue::submit(uint32_t submitCount, const VkSubmitInfo* pSubmits, VkFence fence)
+{
+	for(uint32_t i = 0; i < submitCount; i++)
+	{
+		const VkSubmitInfo& submitInfo = pSubmits[i];
+		if(submitInfo.waitSemaphoreCount || submitInfo.signalSemaphoreCount)
+		{
+			UNIMPLEMENTED();
+		}
+
+		for(uint32_t j = 0; j < submitInfo.commandBufferCount; j++)
+		{
+			Cast(submitInfo.pCommandBuffers[j])->submit();
+		}
+	}
+
+	// FIXME: signal the fence only once the work is completed
+	if(fence != VK_NULL_HANDLE)
+	{
+		Cast(fence)->signal();
+	}
+}
+
+} // namespace vk
