fd604bc3e8aadf77b47b8c6fc10435f9b47f0c55
Alexis Hetu <sugoi@google.com>
2018-08-31 16:45:54 -0400

    Initial empty Vulkan API
    Initial version of the VulkanAPI, empty and unimplemented.
    Successfully builds vk_swiftshader.dll for all platforms/configuration in Visual Studio.
    
    To test using dEQP:
    - Edit environment variables
    	- Define VK_ICD_FILENAMES to <SwiftShader's source directory>\src\Vulkan\vk_swiftshader_icd.json
    - If the location of vk_swiftshader.dll you're using is different than the one specified in
      src\Vulkan\vk_swiftshader_icd.json, modify it to point to the vk_swiftshader.dll file you want to use
    
    Change-Id: I36b5fc665cf5addc5ccf77c9e940a7e3c60436d6

---
+++ src/Vulkan/VkPhysicalDevice.hpp
@@ -0,0 +1,53 @@
+// Copyright 2018 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VK_PHYSICAL_DEVICE_HPP_
+#define VK_PHYSICAL_DEVICE_HPP_
+
+#include "VkObject.hpp"
+
+namespace vk
+{
+
+VkClass(PhysicalDevice)
+{
+public:
+	PhysicalDevice() : name("SwiftShader Device") {}
+
+	~PhysicalDevice() = delete;
+
+	const VkPhysicalDeviceFeatures& getFeatures() const;
+	bool hasFeatures(const VkPhysicalDeviceFeatures& requestedFeatures) const;
+	const VkPhysicalDeviceProperties& getProperties() const;
+	void getFormatProperties(VkFormat format, VkFormatProperties* pFormatProperties) const;
+	void getImageFormatProperties(VkFormat format, VkImageType type, VkImageTiling tiling,
+	                              VkImageUsageFlags usage, VkImageCreateFlags flags,
+	                              VkImageFormatProperties* pImageFormatProperties) const;
+	uint32_t getQueueFamilyPropertyCount() const;
+	void getQueueFamilyProperties(uint32_t pQueueFamilyPropertyCount,
+	                              VkQueueFamilyProperties* pQueueFamilyProperties) const;
+	const VkPhysicalDeviceMemoryProperties& getMemoryProperties() const;
+
+	static inline VkSystemAllocationScope GetAllocationScope() { return VK_SYSTEM_ALLOCATION_SCOPE_DEVICE; }
+
+private:
+	const VkPhysicalDeviceLimits& getLimits() const;
+	VkSampleCountFlags getSampleCounts() const;
+
+	const char *const name = nullptr;
+};
+
+} // namespace vk
+
+#endif // VK_PHYSICAL_DEVICE_HPP_
