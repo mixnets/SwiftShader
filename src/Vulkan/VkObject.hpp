e29439c7b3a22947b079b4b4d5e723bd3df613dc
Alexis Hetu <sugoi@google.com>
2018-09-26 11:25:46 -0400

    Vulkan dispatchable objects
    Vulkan has a few dispatchable objects: Instance, Device, Physical Device,
    Command Buffer and Queue. These objects, when loaded through an ICD, are
    constrained to have a bit of memory allocated at the beginning of these
    objects to contain loader data.
    
    In order to do that without making it impossible for these objects to have
    a base class, since the loader data must be the first element the class points
    to (so no vtable), a base class, VkDispatchableObject was created to handle
    pointing directly to the loader data when casting to the associated VK handle
    and similarly back to a pointer to the internal object by subtracting the offset.
    
    The internal object itself can then have a base class, which is a templated
    class called VkObject. All objects other than the dispatchable objects will
    be derived directly from VkObject.
    
    Also, since all these object are allocated through a custom placement new
    operator, they have to be deallocated through an associated destroy() function,
    so all of their destructors have been deleted, in order to prevent these
    objects from being released any other way.
    
    Bug b/116336664
    
    Change-Id: Iac749f6adcba0eaf7557f0df876ac0474081d9cc

---
+++ src/Vulkan/VkObject.hpp
@@ -0,0 +1,87 @@
+// Copyright 2018 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VK_OBJECT_HPP_
+#define VK_OBJECT_HPP_
+
+#include "VkDebug.hpp"
+#include "VkMemory.h"
+#include <vulkan/vulkan.h>
+#include <vulkan/vk_icd.h>
+
+namespace vk
+{
+// For use in the placement new to make it verbose that we're allocating an object using device memory
+static constexpr VkAllocationCallbacks* DEVICE_MEMORY = nullptr;
+
+template<typename T, typename VkT>
+class VkObjectBase
+{
+public:
+	using VkType = VkT;
+
+	void destroy(const VkAllocationCallbacks* pAllocator) {} // Method defined by objects to delete their content, if necessary
+
+	void* operator new(size_t count, const VkAllocationCallbacks* pAllocator)
+	{
+		return vk::allocate(count, alignof(T), pAllocator, T::GetAllocationScope());
+	}
+
+	void operator delete(void* ptr, const VkAllocationCallbacks* pAllocator)
+	{
+		// Should never happen
+		ASSERT(false);
+	}
+
+	static constexpr VkSystemAllocationScope GetAllocationScope() { return VK_SYSTEM_ALLOCATION_SCOPE_OBJECT; }
+
+protected:
+	// All derived classes should have deleted destructors
+	~VkObjectBase() {}
+};
+
+template<typename T, typename VkT>
+class VkObject : public VkObjectBase<T, VkT>
+{
+public:
+	operator VkT()
+	{
+		return reinterpret_cast<VkT>(this);
+	}
+};
+
+template<typename T, typename VkT>
+class VkDispatchableObject : public VkObjectBase<T, VkT>
+{
+	VK_LOADER_DATA loaderData = { ICD_LOADER_MAGIC };
+
+	static ptrdiff_t loaderDataOffset()
+	{
+		return (ptrdiff_t)&reinterpret_cast<const volatile char&>((((T*)0)->loaderData));
+	}
+public:
+	static inline T* Cast(VkT vkObject)
+	{
+		return vkObject ? reinterpret_cast<T*>(reinterpret_cast<char*>(vkObject) - loaderDataOffset()) : nullptr;
+	}
+
+	operator VkT()
+	{
+		return reinterpret_cast<VkT>(&loaderData);
+	}
+};
+
+} // namespace vk
+
+#endif // VK_OBJECT_HPP_
