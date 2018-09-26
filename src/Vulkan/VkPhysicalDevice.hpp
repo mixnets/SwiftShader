a73ed2b977288f0277a80b04fc735ae80c6598f5
Alexis Hetu <sugoi@google.com>
2018-09-26 11:25:46 -0400

    WIP - Vulkan objects
    This cl adds an internal SwiftShader class for all Vulkan objects
    that can be created or queried through the API.
    
    All objects should obey the proper memory management rules and
    avoid crashes on OOM errors.
    
    Change-Id: I36b5fc665cf5addc5ccf77c9e940a7e3c60436d6

--- src/Vulkan/VkPhysicalDevice.hpp
+++ src/Vulkan/VkPhysicalDevice.hpp
@@ -55,6 +55,9 @@ public:
 	void getQueueFamilyProperties(uint32_t pQueueFamilyPropertyCount,
 	                              VkQueueFamilyProperties* pQueueFamilyProperties) const;
 	const VkPhysicalDeviceMemoryProperties& getMemoryProperties() const;
+	void getSparseImageFormatProperties(VkFormat format, VkImageType type,
+	    VkSampleCountFlagBits samples, VkImageUsageFlags usage, VkImageTiling tiling,
+	    uint32_t* pPropertyCount, VkSparseImageFormatProperties* pProperties) const;
 	void getExternalBufferProperties(const VkPhysicalDeviceExternalBufferInfo* pExternalBufferInfo,
 	                                 VkExternalBufferProperties* pExternalBufferProperties) const;
 	void getExternalFenceProperties(const VkPhysicalDeviceExternalFenceInfo* pExternalFenceInfo,
