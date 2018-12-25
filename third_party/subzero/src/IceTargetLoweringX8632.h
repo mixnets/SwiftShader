acffbe8e2566af01288fc9bc5907913df2817ac4
Raul Tambre <raul@tambre.ee>
2018-12-25 12:11:56 +0200

    Fix warnings from -Wundefined-var-template
    Bug: chromium:604888
    Change-Id: Iec78d0203aee3c3c831b989a948765734738b222

--- third_party/subzero/src/IceTargetLoweringX8632.h
+++ third_party/subzero/src/IceTargetLoweringX8632.h
@@ -77,6 +77,26 @@ private:
   }
 };
 
+//---------------------------------------------
+// Traits that have to be visible in the header
+//---------------------------------------------
+template <>
+std::array<SmallBitVector, RCX86_NUM>
+    TargetX86Base<X8632::Traits>::TypeToRegisterSet = {{}};
+
+template <>
+std::array<SmallBitVector, RCX86_NUM>
+    TargetX86Base<X8632::Traits>::TypeToRegisterSetUnfiltered = {{}};
+
+template <>
+std::array<SmallBitVector,
+           TargetX86Base<X8632::Traits>::Traits::RegisterSet::Reg_NUM>
+    TargetX86Base<X8632::Traits>::RegisterAliases = {{}};
+
+template <>
+FixupKind TargetX86Base<X8632::Traits>::AbsFixup =
+    TargetX86Base<X8632::Traits>::Traits::FK_Abs;
+
 } // end of namespace X8632
 } // end of namespace Ice
 
