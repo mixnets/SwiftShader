e1b8a9366bef10f2357e1638c48c3d0ff284c36f
Ben Clayton <bclayton@google.com>
2019-02-01 08:18:19 +0000

    Start a custom .clang-format rule
    Also add clang-format.sh to perform this operation.
    
    Both of these sit in the ./src directory.
    
    Bug: b/144825072
    Change-Id: I5228ac59f7781ad1307c305949af7e7f9e0c31d1

---
+++ src/clang-format.sh
@@ -0,0 +1,8 @@
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd ${SRC_DIR}
+for dir in "Device" "Pipeline" "Reactor" "System" "Vulkan" "WSI"
+do
+    find ${SRC_DIR}/$dir  -iname "*.hpp" -o -iname "*.cpp" -o -iname "*.inl" | xargs clang-format -i
+done
+popd
+
