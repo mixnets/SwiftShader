9189ec1364bf51f5ce954483d3720087cc4cd3d4
Ben Clayton <headlessclayton@gmail.com>
2019-02-26 15:02:18 +0000

    Plumb PipelineLayouts down to SpirvRoutine
    This initializes arrays to hold the descriptor sets in the routine. Nothing uses this yet.
    
    Bug: b/126330097
    Change-Id: If052d0b93e62e4f32e88ed02f9bc21f4203587f5

--- src/Pipeline/SpirvShader.cpp
+++ src/Pipeline/SpirvShader.cpp
@@ -16,6 +16,7 @@
 #include "SpirvShader.hpp"
 #include "System/Math.hpp"
 #include "Vulkan/VkDebug.hpp"
+#include "Vulkan/VkPipelineLayout.hpp"
 #include "Device/Config.hpp"
 
 namespace sw
@@ -1362,4 +1363,10 @@ namespace sw
 			}
 		}
 	}
+
+	SpirvRoutine::SpirvRoutine(vk::PipelineLayout const *pipelineLayout) :
+		pipelineLayout(pipelineLayout)
+	{
+	}
+
 }
