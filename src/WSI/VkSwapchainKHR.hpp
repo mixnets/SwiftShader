09b77ae5de6edd25d98fc69a26aaf8d122c84d9c
Hernan Liatis <hliatis@google.com>
2019-02-26 11:19:27 -0800

    Implement vkGetSwapchainImagesKHR
    dEQP tests passing:
    dEQP-VK.wsi.xlib.swapchain.get_images
    
    Bug: b/124265819
    Change-Id: I4e2dc8dd48b93bbddba44654c72afb8f43bc3561

--- src/WSI/VkSwapchainKHR.hpp
+++ src/WSI/VkSwapchainKHR.hpp
@@ -16,6 +16,7 @@
 #define SWIFTSHADER_VKSWAPCHAINKHR_HPP
 
 #include "VkObject.hpp"
+#include "VkImage.hpp"
 
 #include <vector>
 
@@ -49,10 +50,12 @@ public:
 
 	VkResult createImages(VkDevice device);
 
+	uint32_t getImageCount() const;
+	VkResult getImages(uint32_t *pSwapchainImageCount, VkImage *pSwapchainImages) const;
+
 private:
 	VkSwapchainCreateInfoKHR createInfo;
 	std::vector<PresentImage> images;
-	uint32_t imageCount;
 
 	void resetImages();
 };
