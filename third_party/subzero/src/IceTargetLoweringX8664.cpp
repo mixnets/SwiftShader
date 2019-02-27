12b3e7d359b6443dfadf24102ce5b58ca3dc667f
Stephen White <senorblanco@chromium.org>
2019-02-27 10:58:23 -0500

    Fix push & pop of XMM registers.
    Microsoft's x86-64 calling convention ABI requires registers XMM6-15 to
    be preserved by the callee:
    https://docs.microsoft.com/en-us/cpp/build/x64-calling-convention?view=vs-2017#calling-convention-defaults
    
    Implement a _pop_reg() analog to _push_reg(), so we can specialize
    the XMM treatment. Pass the RegNum all the way down to the specialized
    class, because we don't know which registers should be 128-bit at the call site.
    
    Bug chromium:931926
    Bug swiftshader:22
    
    Change-Id: I57637c852f0f3bb9a374d61a16a7aaa434ac908d

--- third_party/subzero/src/IceTargetLoweringX8664.cpp
+++ third_party/subzero/src/IceTargetLoweringX8664.cpp
@@ -292,13 +292,34 @@ void TargetX8664::_unlink_bp() {
   }
 }
 
-void TargetX8664::_push_reg(Variable *Reg) {
+void TargetX8664::_push_reg(RegNumT RegNum) {
   Variable *rbp =
       getPhysicalRegister(Traits::RegisterSet::Reg_rbp, Traits::WordType);
-  if (Reg != rbp || !NeedSandboxing) {
-    _push(Reg);
+
+  if ((int) RegNum >= (int) Traits::RegisterSet::Reg_xmm0 && (int) RegNum <= (int) Traits::RegisterSet::Reg_xmm15) {
+    Variable *rsp =
+        getPhysicalRegister(Traits::RegisterSet::Reg_rsp, Traits::WordType);
+    auto* address = Traits::X86OperandMem::create(Func, IceType_v4f32, rsp, nullptr);
+    _sub_sp(Ctx->getConstantInt32(16));
+    _storep(getPhysicalRegister(RegNum, IceType_v4f32), address);
+  } else if (RegNum != Traits::RegisterSet::Reg_rbp || !NeedSandboxing) {
+    _push(getPhysicalRegister(RegNum, Traits::WordType));
   } else {
     _push_rbp();
+  }
+}
+
+void TargetX8664::_pop_reg(RegNumT RegNum) {
+  if ((int) RegNum >= (int) Traits::RegisterSet::Reg_xmm0 && (int) RegNum <= (int) Traits::RegisterSet::Reg_xmm15) {
+    Variable *reg =
+        getPhysicalRegister(RegNum, IceType_v4f32);
+    Variable *rsp =
+        getPhysicalRegister(Traits::RegisterSet::Reg_rsp, Traits::WordType);
+    auto* address = Traits::X86OperandMem::create(Func, reg->getType(), rsp, nullptr);
+    _movp(reg, address);
+    _add_sp(Ctx->getConstantInt32(16));
+  } else {
+    _pop(getPhysicalRegister(RegNum, Traits::WordType));
   }
 }
 
