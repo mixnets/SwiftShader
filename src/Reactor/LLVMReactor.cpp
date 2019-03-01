8c9a15eccf0bfac95a23986980726497a09b5cc8
Chris Forbes <chrisforbes@google.com>
2019-03-01 09:08:47 -0800

    Add support for OpUMulExtended, OpSMulExtended
    - Make the existing LLVMReactor lowering support for MulHigh on non-x86
      available on x86 as well, as we don't have good intrinsics-based implementation
      of 4x 32bit mul highs. At some point in the future we can rework this
      to use some shuffles and a pair of pmuludq. A direct NEON
      implementation is also possible for ARM -- sqdmulh does the right
      thing.
    - Plumb through Int4 and UInt4 variants of MulHigh
    - Remove UInt4's uint,uint,uint,uint constructor declaration, which had
      no implementation.
    - Add more test vectors to ReactorUnitTests::MulHigh, to cover Int4 and
      UInt4 cases
    - Implement SPIRV OpUMulExtended, OpSMulExtended in terms of MulHigh
    
    Bug: b/126873455
    Change-Id: I25ba0a69691e7a6f7a5542ec4a90a44ba8f68331

--- src/Reactor/LLVMReactor.cpp
+++ src/Reactor/LLVMReactor.cpp
@@ -352,30 +352,6 @@ namespace
 		return ::builder->CreateAdd(lhs, rhs);
 	}
 
-	llvm::Value *lowerMulHigh(llvm::Value *x, llvm::Value *y, bool sext)
-	{
-		llvm::VectorType *ty = llvm::cast<llvm::VectorType>(x->getType());
-		llvm::VectorType *extTy = llvm::VectorType::getExtendedElementVectorType(ty);
-
-		llvm::Value *extX, *extY;
-		if (sext)
-		{
-			extX = ::builder->CreateSExt(x, extTy);
-			extY = ::builder->CreateSExt(y, extTy);
-		}
-		else
-		{
-			extX = ::builder->CreateZExt(x, extTy);
-			extY = ::builder->CreateZExt(y, extTy);
-		}
-
-		llvm::Value *mult = ::builder->CreateMul(extX, extY);
-
-		llvm::IntegerType *intTy = llvm::cast<llvm::IntegerType>(ty->getElementType());
-		llvm::Value *mulh = ::builder->CreateAShr(mult, intTy->getIntegerBitWidth());
-		return ::builder->CreateTrunc(mulh, ty);
-	}
-
 	llvm::Value *lowerPack(llvm::Value *x, llvm::Value *y, bool isSigned)
 	{
 		llvm::VectorType *srcTy = llvm::cast<llvm::VectorType>(x->getType());
@@ -446,6 +422,30 @@ namespace
 		return ret;
 	}
 #endif  // !defined(__i386__) && !defined(__x86_64__)
+
+	llvm::Value *lowerMulHigh(llvm::Value *x, llvm::Value *y, bool sext)
+	{
+		llvm::VectorType *ty = llvm::cast<llvm::VectorType>(x->getType());
+		llvm::VectorType *extTy = llvm::VectorType::getExtendedElementVectorType(ty);
+
+		llvm::Value *extX, *extY;
+		if (sext)
+		{
+			extX = ::builder->CreateSExt(x, extTy);
+			extY = ::builder->CreateSExt(y, extTy);
+		}
+		else
+		{
+			extX = ::builder->CreateZExt(x, extTy);
+			extY = ::builder->CreateZExt(y, extTy);
+		}
+
+		llvm::Value *mult = ::builder->CreateMul(extX, extY);
+
+		llvm::IntegerType *intTy = llvm::cast<llvm::IntegerType>(ty->getElementType());
+		llvm::Value *mulh = ::builder->CreateAShr(mult, intTy->getIntegerBitWidth());
+		return ::builder->CreateTrunc(mulh, ty);
+	}
 #endif  // REACTOR_LLVM_VERSION >= 7
 }
 
@@ -5713,6 +5713,16 @@ namespace rr
 #else
 		return As<Int4>(V(lowerRoundInt(V(cast.value), T(Int4::getType()))));
 #endif
+	}
+
+	RValue<Int4> MulHigh(RValue<Int4> x, RValue<Int4> y)
+	{
+		return As<Int4>(V(lowerMulHigh(V(x.value), V(y.value), true)));
+	}
+
+	RValue<UInt4> MulHigh(RValue<UInt4> x, RValue<UInt4> y)
+	{
+		return As<UInt4>(V(lowerMulHigh(V(x.value), V(y.value), false)));
 	}
 
 	RValue<Short8> PackSigned(RValue<Int4> x, RValue<Int4> y)
