6e524da92ba4ecc2041a4006b84d90550837c54a
Ben Clayton <bclayton@google.com>
2019-03-02 00:12:44 +0000

    Kokoro: Check that the Visual Studio projects are updated
    Change-Id: I837c1deab4466badb35915f90b6fc7506bc97ee9

---
+++ kokoro/gcp_ubuntu/check_vcxproj_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+
+# Check that all Visual Studio project files are up to date.
+GENERATE_GO="build/Visual Studio 15 2017 Win64/generate.go"
+go run "${GENERATE_GO}"
+if ! git diff --exit-code HEAD; then
+    @echo "Visual Studio project files not updated."
+    echo "Please run: "
+    echo "  go run \"${GENERATE_GO}\""
+    echo "to update the Visual Studio project files"
+fi
