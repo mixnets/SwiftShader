mixnets/SwiftShader

commit bfaf4e825b78b554b89065d57327d072c3a02518

Regres: Run latest PS testlists on parent.
This is better than running potentially two different test lists for both.

Requires caching the parent test results with the testlist's hash as a cache key.

Change-Id: Ic153e65395fc1c3f02a66bbb1df60731ce28901b

tests/regres/testlist/testlist.go
@@ -1,0 +1,120 @@
+// Copyright 2019 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package testlist provides utilities for handling test lists.
+package testlist
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/gob"
+	"encoding/hex"
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"../cause"
+)
+
+// API is an enumerator of graphics APIs.
+type API string
+
+// Graphics APIs.
+const (
+	EGL    = API("egl")
+	GLES2  = API("gles2")
+	GLES3  = API("gles3")
+	Vulkan = API("vulkan")
+)
+
+// Group is a list of tests to be run for a single API.
+type Group struct {
+	Name  string
+	File  string
+	API   API
+	Tests []string
+}
+
+// Lists is the full list of tests to be run.
+type Lists []Group
+
+// Hash returns a SHA1 hash of the set of tests.
+func (l Lists) Hash() string {
+	h := sha1.New()
+	if err := gob.NewEncoder(h).Encode(l); err != nil {
+		panic(cause.Wrap(err, "Could not encode testlist to produce hash"))
+	}
+	var hash [20]byte
+	copy(hash[:], h.Sum(nil))
+	return hex.EncodeToString(hash[:])
+}
+
+// Load loads the test list json file and returns the full set of tests.
+func Load(root, jsonPath string) (Lists, error) {
+	root, err := filepath.Abs(root)
+	if err != nil {
+		return nil, cause.Wrap(err, "Couldn't get absolute path of '%s'", root)
+	}
+
+	jsonPath, err = filepath.Abs(jsonPath)
+	if err != nil {
+		return nil, cause.Wrap(err, "Couldn't get absolute path of '%s'", jsonPath)
+	}
+
+	i, err := ioutil.ReadFile(jsonPath)
+	if err != nil {
+		return nil, cause.Wrap(err, "Couldn't read test list from '%s'", jsonPath)
+	}
+
+	var jsonGroups []struct {
+		Name     string
+		API      string
+		TestFile string `json:"tests"`
+	}
+	if err := json.NewDecoder(bytes.NewReader(i)).Decode(&jsonGroups); err != nil {
+		return nil, cause.Wrap(err, "Couldn't parse '%s'", jsonPath)
+	}
+
+	dir := filepath.Dir(jsonPath)
+
+	out := make(Lists, len(jsonGroups))
+	for i, jsonGroup := range jsonGroups {
+		path := filepath.Join(dir, jsonGroup.TestFile)
+		tests, err := ioutil.ReadFile(path)
+		if err != nil {
+			return nil, cause.Wrap(err, "Couldn't read '%s'", tests)
+		}
+		relPath, err := filepath.Rel(root, path)
+		if err != nil {
+			return nil, cause.Wrap(err, "Couldn't get relative path for '%s'", path)
+		}
+		group := Group{
+			Name: jsonGroup.Name,
+			File: relPath,
+			API:  API(jsonGroup.API),
+		}
+		for _, line := range strings.Split(string(tests), "\n") {
+			line = strings.TrimSpace(line)
+			if line != "" && !strings.HasPrefix(line, "#") {
+				group.Tests = append(group.Tests, line)
+			}
+		}
+		sort.Strings(group.Tests)
+		out[i] = group
+	}
+
+	return out, nil
+}

commit ad09434c85674828e5a0c27314501ee87aa7d6a7

Regres: Don't attempt to build failing changes forever.
If a test failed to build, an error was logged to stdout, but the error was not posted to the change. This meant the change would be picked up by regres again, and the process would repeat ad-infinitum.

Posting of the build error used to work, but was broken by bfaf4e825b.

tests/regres/main.go
@@ -247,6 +247,7 @@ func (r *regres) run() error {
 		if err != nil {
 			log.Println(cause.Wrap(err, "Failed to test changelist '%s'", change.latest))
 			time.Sleep(time.Minute)
+			change.pending = false
 			continue
 		}
 
@@ -335,14 +336,8 @@ func (r *regres) testLatest(change *changeInfo) (*CommitTestResults, testlist.Li
 		return results, testlists, nil // Use cached results
 	}
 
-	if err := test.build(); err != nil {
-		return nil, nil, cause.Wrap(err, "Failed to build '%s'", change.latest)
-	}
-
-	results, err := test.run(testlists)
-	if err != nil {
-		return nil, nil, cause.Wrap(err, "Failed to test '%s'", change.latest)
-	}
+	// Build the change and test it.
+	results := test.buildAndRun(testlists)
 
 	// Cache the results for future tests
 	if err := results.save(cachePath); err != nil {
@@ -368,16 +363,8 @@ func (r *regres) testParent(change *changeInfo, testlists testlist.Lists) (*Comm
 		return nil, cause.Wrap(err, "Failed to checkout '%s'", change.parent)
 	}
 
-	// Build the parent change.
-	if err := test.build(); err != nil {
-		return nil, cause.Wrap(err, "Failed to build '%s'", change.parent)
-	}
-
-	// Run the tests on the parent change.
-	results, err := test.run(testlists)
-	if err != nil {
-		return nil, cause.Wrap(err, "Failed to test '%s'", change.parent)
-	}
+	// Build the parent change and test it.
+	results := test.buildAndRun(testlists)
 
 	// Store the results of the parent change to the cache.
 	if err := results.save(cachePath); err != nil {
@@ -623,6 +610,27 @@ func (t *test) checkout() error {
 	return nil
 }
 
+// buildAndRun calls t.build() followed by t.run(). Errors are logged and
+// reported in the returned CommitTestResults.Error field.
+func (t *test) buildAndRun(testLists testlist.Lists) *CommitTestResults {
+	// Build the parent change.
+	if err := t.build(); err != nil {
+		msg := fmt.Sprintf("Failed to build '%s'", t.commit)
+		log.Println(cause.Wrap(err, msg))
+		return &CommitTestResults{Error: msg}
+	}
+
+	// Run the tests on the parent change.
+	results, err := t.run(testLists)
+	if err != nil {
+		msg := fmt.Sprintf("Failed to test change '%s'", t.commit)
+		log.Println(cause.Wrap(err, msg))
+		return &CommitTestResults{Error: msg}
+	}
+
+	return results
+}
+
 // build builds the SwiftShader source into t.buildDir.
 func (t *test) build() error {
 	log.Printf("Building '%s'\n", t.commit)
@@ -703,7 +711,6 @@ func (t *test) run(testLists testlist.Lists) (*CommitTestResults, error) {
 
 	out := CommitTestResults{
 		Version: dataVersion,
-		Built:   true,
 		Tests:   map[string]TestResult{},
 	}
 
@@ -821,7 +828,7 @@ func (s Status) Passing() bool {
 // results.
 type CommitTestResults struct {
 	Version  int
-	Built    bool
+	Error    string
 	Tests    map[string]TestResult
 	Duration time.Duration
 }
@@ -865,13 +872,11 @@ func (r *CommitTestResults) save(path string) error {
 // CommitTestResults. This string is used as the report message posted to the
 // gerrit code review.
 func compare(old, new *CommitTestResults) string {
-	switch {
-	case !old.Built && !new.Built:
-		return "Build continues to be broken."
-	case old.Built && !new.Built:
-		return "Build broken."
-	case !old.Built && !new.Built:
-		return "Build now fixed. Cannot compare against broken parent."
+	if old.Error != "" {
+		return old.Error
+	}
+	if new.Error != "" {
+		return new.Error
 	}
 
 	oldStatusCounts, newStatusCounts := map[Status]int{}, map[Status]int{}