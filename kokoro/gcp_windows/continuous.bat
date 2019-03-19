8be49bfdb258c32a1b691930dcd22cf6616fb565
Nicolas Capens <capn@google.com>
2019-03-19 00:16:20 -0400

    WIP-kokoro-windows-unittests
    Change-Id: Ic1269fdae3088b08a85322cbc7e33a3bdd8292cb

--- kokoro/gcp_windows/continuous.bat
+++ kokoro/gcp_windows/continuous.bat
@@ -7,3 +7,8 @@ cd git\SwiftShader
 git submodule update --init
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild" SwiftShader.sln
+
+SET PATH=%PATH%;T:\src\git\SwiftShader\out\Debug_x64
+
+REM Run the GLES unit tests. TODO(capn): move to different directory (build?).
+bin\GLESUnitTests\x64\Debug\GLESUnitTests.exe
