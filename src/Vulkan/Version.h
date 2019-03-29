a89597db829d74db0868c1d83070f0d90258a565
Nicolas Capens <capn@google.com>
2019-03-29 05:23:19 -0400

    WIP-texture-sampling
    Bug b/129523279
    
    Test: dEQP-VK.texture.filtering.2d.formats.r8g8b8a8_unorm.nearest
    Change-Id: I6652171748384868d1dccea4bcc0292dafda083e

--- src/Vulkan/Version.h
+++ src/Vulkan/Version.h
@@ -13,8 +13,8 @@
 // limitations under the License.
 
 #define MAJOR_VERSION 5
-#define MINOR_VERSION 0
-#define PATCH_VERSION 0
+#define MINOR_VERSION 9
+#define PATCH_VERSION 39
 #define BUILD_REVISION 1
 
 #define STRINGIFY(x) #x
