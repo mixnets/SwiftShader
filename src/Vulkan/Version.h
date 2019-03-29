1c9778edc6c9edb19904b213e4daf21a35f8dd8d
Nicolas Capens <capn@google.com>
2019-03-29 05:23:19 -0400

    WIP-texture-sampling
    Bug b/129523279
    
    Test: dEQP-VK.texture.filtering.2d.formats.r8g8b8a8_unorm.nearest
    Change-Id: I6652171748384868d1dccea4bcc0292dafda083e

--- src/Vulkan/Version.h
+++ src/Vulkan/Version.h
@@ -13,8 +13,8 @@
 // limitations under the License.
 
 #define MAJOR_VERSION 5
-#define MINOR_VERSION 0
-#define PATCH_VERSION 0
+#define MINOR_VERSION 3
+#define PATCH_VERSION 3
 #define BUILD_REVISION 1
 
 #define STRINGIFY(x) #x
--- src/Vulkan/VkDescriptorSetLayout.hpp
+++ src/Vulkan/VkDescriptorSetLayout.hpp
@@ -17,10 +17,21 @@
 
 #include "VkObject.hpp"
 
+#include "Device/Sampler.hpp"
+
 namespace vk
 {
 
 class DescriptorSet;
+
+// TODO(capn): Move to the Device or Pipeline layer.
+struct ImageSamplerDescriptor
+{
+	// TODO(capn): Minimize to the data actually needed.
+	VkDescriptorImageInfo imageInfo;
+
+	sw::Texture texture;
+};
 
 class DescriptorSetLayout : public Object<DescriptorSetLayout, VkDescriptorSetLayout>
 {
