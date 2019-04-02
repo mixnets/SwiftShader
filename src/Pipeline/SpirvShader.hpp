d5e5f4359dddf2e88f4485fa463d214a95a72d4e
Ben Clayton <bclayton@google.com>
2019-04-02 12:28:22 +0100

    Implement dynamic buffer offsets.
    Tests: *dynamic*
    Bug: b/126330097
    Change-Id: I7e4f7e3d921acb72878b7728216415ba66f63ec7

--- src/Pipeline/SpirvShader.hpp
+++ src/Pipeline/SpirvShader.hpp
@@ -659,7 +659,8 @@ namespace sw
 		Value inputs = Value{MAX_INTERFACE_COMPONENTS};
 		Value outputs = Value{MAX_INTERFACE_COMPONENTS};
 
-		std::array<Pointer<Byte>, vk::MAX_BOUND_DESCRIPTOR_SETS> descriptorSets;
+		Pointer<Pointer<Byte>> descriptorSets;
+		Pointer<Int> descriptorDynamicOffsets;
 		Pointer<Byte> pushConstants;
 
 		void createLvalue(SpirvShader::Object::ID id, uint32_t size)
