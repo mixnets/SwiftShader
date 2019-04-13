c341a54de8faeb89363a2409a4ddb5bd8a091685
Ben Clayton <bclayton@google.com>
2019-04-13 10:20:37 -0400

    Kokoro: Build VS solutions produced from CMake
    This now builds both the LLVM and Subzero Reactor backends.
    Fixed issue where failures were not actually failing the build in continuous.bat.
    Changed the test's expected path of vk_swiftshader.dll to more closely match macOS and Linux.
    
    Bug: b/130458775
    Bug: b/123360006
    Change-Id: Id2cfc0a7016c9813597567fd6ab900fe4047b8f8

--- tests/VulkanUnitTests/Driver.cpp
+++ tests/VulkanUnitTests/Driver.cpp
@@ -50,9 +50,9 @@ bool Driver::loadSwiftShader()
 {
 #if OS_WINDOWS
 #    if defined(NDEBUG)
-    return load("../../build/Release_x64/vk_swiftshader.dll");
+    return load("./build/Release/vk_swiftshader.dll");
 #    else
-    return load("../../build/Debug_x64/vk_swiftshader.dll");
+    return load("./build/Debug/vk_swiftshader.dll");
 #    endif
 #elif OS_MAC
     return load("./build/Darwin/libvk_swiftshader.dylib");
