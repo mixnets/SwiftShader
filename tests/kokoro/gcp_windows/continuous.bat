830139b4a47b6e48832804ffa409e0e0f806bee1
Ben Clayton <bclayton@google.com>
2019-04-13 10:20:37 -0400

    Blindly attempt to build VS solutions produced from CMake
    Change-Id: Id2cfc0a7016c9813597567fd6ab900fe4047b8f8

--- tests/kokoro/gcp_windows/continuous.bat
+++ tests/kokoro/gcp_windows/continuous.bat
@@ -1,6 +1,6 @@
 @echo on
 
-SET PATH=%PATH%;C:\python27
+SET PATH=%PATH%;C:\python27;C:\Program Files\cmake\bin
 
 cd git\SwiftShader
 
@@ -9,11 +9,21 @@ git submodule update --init
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild"
 SET CONFIG=Debug
 
-REM TODO: Switch between reactor backends with the REACTOR_BACKEND env var.
+cd build
+
+cmake .. -G "Visual Studio 15 2017 Win64" -Thost=x64 "-DREACTOR_BACKEND=%REACTOR_BACKEND%"
+
 %MSBUILD% /p:Configuration=%CONFIG% SwiftShader.sln
 
 SET PATH=%PATH%;T:\src\git\SwiftShader\out\Debug_x64
 SET SWIFTSHADER_DISABLE_DEBUGGER_WAIT_DIALOG=1
 
+
+REM SHOW ME THEc FILES
+dir
+dir Debug
+dir Debug\GLESUnitTests
+REM SHOW ME THEc FILES
+
 REM Run the GLES unit tests. TODO(capn): move to different directory (build?).
-bin\GLESUnitTests\x64\Debug\GLESUnitTests.exe
+Debug\GLESUnitTests.exe
