816ae965779ddbec9b5e87e86c50c27d09479350
Chris Forbes <chrisforbes@google.com>
2019-04-22 10:54:23 -0700

    Partial support for input attachments
    TODO: split this up
    
    Bug: b/nnn
    Test: dEQP-VK.renderpass*.input.*
    Change-Id: I15412e66516ea907d4b2ffd913a9eb1a3f8a9bb9

--- src/Vulkan/VkPipeline.cpp
+++ src/Vulkan/VkPipeline.cpp
@@ -452,7 +452,7 @@ void GraphicsPipeline::compileShaders(const VkAllocationCallbacks* pAllocator, c
 
 		// FIXME (b/119409619): use an allocator here so we can control all memory allocations
 		// TODO: also pass in any pipeline state which will affect shader compilation
-		auto spirvShader = new sw::SpirvShader{code};
+		auto spirvShader = new sw::SpirvShader{code, pCreateInfo->renderPass, pCreateInfo->subpass};
 
 		switch (pStage->stage)
 		{
@@ -544,7 +544,7 @@ void ComputePipeline::compileShaders(const VkAllocationCallbacks* pAllocator, co
 	ASSERT(shader == nullptr);
 
 	// FIXME(b/119409619): use allocator.
-	shader = new sw::SpirvShader(code);
+	shader = new sw::SpirvShader(code, VK_NULL_HANDLE, 0);
 	vk::DescriptorSet::Bindings descriptorSets;  // FIXME(b/129523279): Delay code generation until invoke time.
 	sw::ComputeProgram program(shader, layout, descriptorSets);
 
