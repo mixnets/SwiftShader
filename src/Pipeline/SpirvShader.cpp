cdf13758ecb0a7a85ac5654a60324d8a5c3d9ea7
Chris Forbes <chrisforbes@google.com>
2019-04-22 18:12:13 -0700

    Adjust input attachment index when indexing into array of input descriptors
    Slightly hacky...
    
    Bug: b/nnn
    Test: dEQP-VK.renderpass*
    Change-Id: I22a443edeee705b24ccc3a838fbab5f333f5dd87

--- src/Pipeline/SpirvShader.cpp
+++ src/Pipeline/SpirvShader.cpp
@@ -618,7 +618,7 @@ namespace sw
 					if (insn.opcode() == spv::OpAccessChain || insn.opcode() == spv::OpInBoundsAccessChain)
 					{
 						Decorations dd{};
-						ApplyDecorationsForAccessChain(&dd, pointerId, insn.wordCount() - 4, insn.wordPointer(4));
+						ApplyDecorationsForAccessChain(&dd, &descriptorDecorations[resultId], pointerId, insn.wordCount() - 4, insn.wordPointer(4));
 						// Note: offset is the one thing that does *not* propagate, as the access chain accounts for it.
 						dd.HasOffset = false;
 						decorations[resultId].Apply(dd);
@@ -1248,7 +1248,7 @@ namespace sw
 		}
 	}
 
-	void SpirvShader::ApplyDecorationsForAccessChain(Decorations *d, Object::ID baseId, uint32_t numIndexes, uint32_t const *indexIds) const
+	void SpirvShader::ApplyDecorationsForAccessChain(Decorations *d, DescriptorDecorations *dd, Object::ID baseId, uint32_t numIndexes, uint32_t const *indexIds) const
 	{
 		ApplyDecorationsForId(d, baseId);
 		auto &baseObject = getObject(baseId);
@@ -1270,6 +1270,12 @@ namespace sw
 			}
 			case spv::OpTypeArray:
 			case spv::OpTypeRuntimeArray:
+				if (dd->InputAttachmentIndex >= 0)
+				{
+					dd->InputAttachmentIndex += GetConstantInt(indexIds[i]);
+				}
+				typeId = type.element;
+				break;
 			case spv::OpTypeVector:
 				typeId = type.element;
 				break;
