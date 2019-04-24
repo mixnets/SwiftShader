dec96d403ef85d00a850c578f9eadd8bb30161d0
Nicolas Capens <capn@google.com>
2019-04-24 02:03:22 -0400

    WIP-explicit-lod
    Change-Id: I265b81d953fe5a0496d029704a0f5eeff4229823

--- src/Pipeline/SpirvShader.hpp
+++ src/Pipeline/SpirvShader.hpp
@@ -52,6 +52,7 @@ namespace sw
 	enum FilterType ENUM_UNDERLYING_TYPE_UNSIGNED_INT;
 	enum MipmapType ENUM_UNDERLYING_TYPE_UNSIGNED_INT;
 	enum AddressingMode ENUM_UNDERLYING_TYPE_UNSIGNED_INT;
+	enum SamplerMethod;
 
 	// SIMD contains types that represent multiple scalars packed into a single
 	// vector data type. Types in the SIMD namespace provide a semantic hint
@@ -155,6 +156,9 @@ namespace sw
 	public:
 		using InsnStore = std::vector<uint32_t>;
 		InsnStore insns;
+
+		using ImageSampler = void(void* image, void* uvsIn, void* texelOut);
+		using GetImageSampler = ImageSampler*(const vk::ImageView *imageView, const vk::Sampler *sampler);
 
 		/* Pseudo-iterator over SPIRV instructions, designed to support range-based-for. */
 		class InsnIterator
@@ -732,6 +736,8 @@ namespace sw
 		EmitResult EmitKill(InsnIterator insn, EmitState *state) const;
 		EmitResult EmitPhi(InsnIterator insn, EmitState *state) const;
 		EmitResult EmitImageSampleImplicitLod(InsnIterator insn, EmitState *state) const;
+		EmitResult EmitImageSampleExplicitLod(InsnIterator insn, EmitState *state) const;
+		EmitResult EmitImageSample(GetImageSampler getImageSampler, InsnIterator insn, EmitState *state) const;
 		EmitResult EmitImageQuerySize(InsnIterator insn, EmitState *state) const;
 		EmitResult EmitImageRead(InsnIterator insn, EmitState *state) const;
 		EmitResult EmitImageWrite(InsnIterator insn, EmitState *state) const;
@@ -758,10 +764,11 @@ namespace sw
 		// Returns the pair <significand, exponent>
 		std::pair<SIMD::Float, SIMD::Int> Frexp(RValue<SIMD::Float> val) const;
 
-		using ImageSampler = void(void* image, void* uvsIn, void* texelOut);
-
-		static ImageSampler *getImageSampler(const vk::ImageView *imageView, const vk::Sampler *sampler);
+		static ImageSampler *getImageSamplerImplicitLod(const vk::ImageView *imageView, const vk::Sampler *sampler);
+		static ImageSampler *getImageSamplerExplicitLod(const vk::ImageView *imageView, const vk::Sampler *sampler);
+		static ImageSampler *getImageSampler(SamplerMethod samplerMethod, const vk::ImageView *imageView, const vk::Sampler *sampler);
 		static void emitSamplerFunction(
+			SamplerMethod samplerMethod,
 			const vk::ImageView *imageView, const vk::Sampler *sampler,
 			Pointer<Byte> image, Pointer<SIMD::Float> in, Pointer<Byte> out);
 
