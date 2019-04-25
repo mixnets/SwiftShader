7ddf7d85d479581aa111473f91fb9c55caa83b76
Chris Forbes <chrisforbes@google.com>
2019-04-25 10:00:06 -0700

    Plumb constants through to the sampler, and make sRGB work
    Change-Id: Ib4974a5f0dea325e198905cb94bae138ccac4c6e
    Test: dEQP-VK.texture.swizzle.component_mapping.*
    Bug: b/129523279

--- src/Pipeline/SpirvShader.hpp
+++ src/Pipeline/SpirvShader.hpp
@@ -257,7 +257,7 @@ namespace sw
 		using InsnStore = std::vector<uint32_t>;
 		InsnStore insns;
 
-		using ImageSampler = void(void* image, void* uvsIn, void* texelOut);
+		using ImageSampler = void(void* image, void* uvsIn, void* texelOut, void* constants);
 		using GetImageSampler = ImageSampler*(const vk::ImageView *imageView, const vk::Sampler *sampler);
 
 		/* Pseudo-iterator over SPIRV instructions, designed to support range-based-for. */
@@ -865,7 +865,7 @@ namespace sw
 		static void emitSamplerFunction(
 			SamplerMethod samplerMethod,
 			const vk::ImageView *imageView, const vk::Sampler *sampler,
-			Pointer<Byte> image, Pointer<SIMD::Float> in, Pointer<Byte> out);
+			Pointer<Byte> image, Pointer<SIMD::Float> in, Pointer<Byte> out, Pointer<Byte> constants);
 
 		// TODO(b/129523279): Eliminate conversion and use vk::Sampler members directly.
 		static sw::TextureType convertTextureType(VkImageViewType imageViewType);
@@ -895,6 +895,7 @@ namespace sw
 		Pointer<Pointer<Byte>> descriptorSets;
 		Pointer<Int> descriptorDynamicOffsets;
 		Pointer<Byte> pushConstants;
+		Pointer<Byte> constants;
 		Int killMask = Int{0};
 
 		void createVariable(SpirvShader::Object::ID id, uint32_t size)
