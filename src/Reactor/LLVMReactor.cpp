8c07b3461d8ecfa36de8892a018ec79f1c852d70
Ben Clayton <bclayton@google.com>
2019-05-02 10:56:19 +0100

    LLVMReactor: Always run the IR verifier.
    Stuff is broken and has gone unnoticed.
    
    Change-Id: I2624a38f78a05114caf69681291733343fbe1c90

--- src/Reactor/LLVMReactor.cpp
+++ src/Reactor/LLVMReactor.cpp
@@ -52,6 +52,7 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Mangler.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
 #include "llvm/Support/Error.h"
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Target/TargetOptions.h"
@@ -969,6 +970,15 @@ namespace rr
 			debugInfo->Finalize();
 		}
 #endif // ENABLE_RR_DEBUG_INFO
+
+		// TODO: Disable for release builds once heavy development is over.
+		bool verifyIR = true;
+		if(verifyIR)
+		{
+			llvm::legacy::PassManager pm;
+			pm.add(llvm::createVerifierPass());
+			pm.run(*::module);
+		}
 
 		if(false)
 		{
