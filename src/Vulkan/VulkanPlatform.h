c3094aacdf2fec5322d2c01b49b13ef7e0aa8063
Alexis Hetu <sugoi@google.com>
2019-05-09 17:46:07 -0400

    32 bit safe code
    In order to easily add 32 bit support, this cl adds a
    header which essentially uses a union to make sure that
    even when we have 32 bit pointers, the Vulkan handles are
    64 bit.
    
    This change should be noop.
    
    Bug b/129979580
    
    Change-Id: I54254929186584ec4544a1da5a7def7cf56e392e

---
+++ src/Vulkan/VulkanPlatform.h
@@ -0,0 +1,114 @@
+// Copyright 2019 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VULKAN_PLATFORM
+#define VULKAN_PLATFORM
+
+#include <cstdint>
+
+template<typename T> class alignas(sizeof(uint64_t)) VkWrapper
+{
+public:
+	using HandleType = T;
+
+	VkWrapper()
+	{
+		u.dummy = 0;
+	}
+
+	VkWrapper(HandleType handle)
+	{
+		u.dummy = 0;
+		u.handle = handle;
+	}
+
+	void operator=(HandleType handle)
+	{
+		u.handle = handle;
+	}
+
+	HandleType get() const
+	{
+		return u.handle;
+	}
+
+	operator HandleType() const
+	{
+		return u.handle;
+	}
+
+private:
+	union PointerHandleUnion
+	{
+		HandleType handle;
+		uint64_t dummy; // VkWrapper's size must always be 64 bits even when void* is 32 bits
+	};
+	PointerHandleUnion u;
+};
+
+// VkDescriptorSet objects are really just memory in the VkDescriptorPool
+// object, so define different/more convenient operators for this object.
+struct VkDescriptorSet_T;
+template<> class alignas(sizeof(uint64_t)) VkWrapper<VkDescriptorSet_T*>
+{
+public:
+	using HandleType = uint8_t*;
+
+	VkWrapper(uint8_t* handle)
+	{
+		u.dummy = 0;
+		u.handle = handle;
+	}
+
+	HandleType get() const
+	{
+		return u.handle;
+	}
+
+	operator HandleType() const
+	{
+		return u.handle;
+	}
+
+	HandleType operator+(ptrdiff_t rhs) const
+	{
+		return u.handle + rhs;
+	}
+
+	HandleType operator+=(ptrdiff_t rhs)
+	{
+		return (u.handle += rhs);
+	}
+
+	ptrdiff_t operator-(const HandleType rhs) const
+	{
+		return u.handle - rhs;
+	}
+
+private:
+	union PointerHandleUnion
+	{
+		HandleType handle;
+		uint64_t dummy; // VkWrapper's size must always be 64 bits even when void* is 32 bits
+	};
+	PointerHandleUnion u;
+};
+
+#define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) \
+	typedef struct object##_T *object##Ptr; \
+	typedef VkWrapper<object##Ptr> object;
+
+#include <vulkan/vulkan.h>
+
+#endif // VULKAN_PLATFORM
