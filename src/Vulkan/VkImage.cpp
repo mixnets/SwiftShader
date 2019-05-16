70315b291480fc61a13a9464595835153e1da296
Alexis Hetu <sugoi@google.com>
2019-05-16 14:20:16 -0400

    Allow ImageView to cast compressed texture into other formats
    If an image is created with flags including the
    VK_IMAGE_CREATE_BLOCK_TEXEL_VIEW_COMPATIBLE_BIT
    bit, then the compressed image may be used with
    an image view which has an uncompressed format
    that is size-compatible with the image's
    compressed format.
    
    Tests: dEQP-VK.image.texel_view_compatible.*.texture_read.e*
    
    Bug b/119620767
    
    Change-Id: Ia1af959780af12e9014f7852aaf991d313f75bf1

--- src/Vulkan/VkImage.cpp
+++ src/Vulkan/VkImage.cpp
@@ -706,6 +706,15 @@ VkDeviceSize Image::getStorageSize(VkImageAspectFlags aspectMask) const
 	return arrayLayers * getLayerSize(static_cast<VkImageAspectFlagBits>(aspectMask));
 }
 
+const Image* Image::getSampledImage(const vk::Format& imageViewFormat) const
+{
+	// If the ImageView's format is compressed, then we do need to decompress the image so that
+	// it may be sampled properly by texture sampling functions, which don't support compressed
+	// textures. If the ImageView's format is NOT compressed, then we reinterpret cast the
+	// compressed image into the ImageView's format, so we must return the compressed image as is.
+	return (decompressedImage && imageViewFormat.isCompressed()) ? decompressedImage : this;
+}
+
 void Image::blit(VkImage dstImage, const VkImageBlit& region, VkFilter filter)
 {
 	device->getBlitter()->blit(this, Cast(dstImage), region, filter);
