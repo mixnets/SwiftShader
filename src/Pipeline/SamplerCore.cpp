f7509f7fdb958b0e9aa13534b70a901c6439dd4d
Nicolas Capens <capn@google.com>
2019-05-16 09:58:26 -0400

    Refactor obtaining format aspect info
    Aspect info is part of the format, so shouldn't be queried from an Image
    utility function.
    
    Bug: b/132437008
    Change-Id: I87d632231a810cece3d1ea579e9df63b6b80a714

--- src/Pipeline/SamplerCore.cpp
+++ src/Pipeline/SamplerCore.cpp
@@ -128,7 +128,7 @@ namespace sw
 			lod = Min(lod, *Pointer<Float>(sampler + OFFSET(vk::Sampler, maxLod)));
 		}
 
-		bool force32BitFiltering = state.highPrecisionFiltering && !hasYuvFormat() && (state.textureFilter != FILTER_POINT);
+		bool force32BitFiltering = state.highPrecisionFiltering && !isYcbcrFormat() && (state.textureFilter != FILTER_POINT);
 		bool seamlessCube = (state.addressingModeU == ADDRESSING_SEAMLESS);
 		bool use32BitFiltering = hasFloatTexture() || hasUnnormalizedIntegerTexture() || force32BitFiltering ||
 		                         seamlessCube || state.unnormalizedCoordinates || state.compareEnable || borderModeActive();
@@ -1514,7 +1514,7 @@ namespace sw
 		UInt index[4];
 		computeIndices(index, uuuu, vvvv, wwww, offset, mipmap, function);
 
-		if(hasYuvFormat())
+		if(isYcbcrFormat())
 		{
 			// Generic YPbPr to RGB transformation
 			// R = Y                               +           2 * (1 - Kr) * Pr
@@ -1757,7 +1757,7 @@ namespace sw
 		}
 		else
 		{
-			ASSERT(!hasYuvFormat());
+			ASSERT(!isYcbcrFormat());
 
 			Vector4s cs = sampleTexel(index, buffer);
 
@@ -1902,7 +1902,7 @@ namespace sw
 		{
 			buffer[0] = *Pointer<Pointer<Byte>>(mipmap + OFFSET(Mipmap,buffer[0]));
 
-			if(hasYuvFormat())
+			if(isYcbcrFormat())
 			{
 				buffer[1] = *Pointer<Pointer<Byte>>(mipmap + OFFSET(Mipmap,buffer[1]));
 				buffer[2] = *Pointer<Pointer<Byte>>(mipmap + OFFSET(Mipmap,buffer[2]));
@@ -2229,9 +2229,9 @@ namespace sw
 		return state.textureFormat.has32bitIntegerTextureComponents();
 	}
 
-	bool SamplerCore::hasYuvFormat() const
+	bool SamplerCore::isYcbcrFormat() const
 	{
-		return state.textureFormat.hasYuvFormat();
+		return state.textureFormat.isYcbcrFormat();
 	}
 
 	bool SamplerCore::isRGBComponent(int component) const
