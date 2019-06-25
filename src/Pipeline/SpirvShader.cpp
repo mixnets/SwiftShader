d5e2667e3203f2e3b93366038cce4af255ac2fd2
Ben Clayton <bclayton@google.com>
2019-06-25 19:07:48 +0100

    Move GenericValue into SpirvShader
    GenericValue is only used by SpirvShader so move it as a private inner class of SpirvShader.
    
    Part of the foundations of much larger refactoring required for function inlining in SpirvShader.
    
    Bug: b/133213304
    Change-Id: I75484c3d036b82f3bc6a735034a7c1b04f05f600

--- src/Pipeline/SpirvShader.cpp
+++ src/Pipeline/SpirvShader.cpp
@@ -6396,6 +6396,11 @@ namespace sw
 		}
 	}
 
+	SpirvShader::GenericValue::GenericValue(SpirvShader const *shader, SpirvRoutine const *routine, SpirvShader::Object::ID objId) :
+			obj(shader->getObject(objId)),
+			intermediate(obj.kind == SpirvShader::Object::Kind::Intermediate ? &routine->getIntermediate(objId) : nullptr),
+			type(obj.type) {}
+
 	SpirvRoutine::SpirvRoutine(vk::PipelineLayout const *pipelineLayout) :
 		pipelineLayout(pipelineLayout)
 	{
