b747841dfbbf6653ca764d545bf221137c6d805d
Ben Clayton <bclayton@google.com>
2019-06-25 21:01:33 +0100

    SpirvShader::EmitState: Add support for sub-scopes.
    Add EmitState::fork() method.
    This is used to create a new context in which to emit inlined functions, and is required to avoid duplicate definitions of SSA values when the same function is inlined multiple times.
    
    Bug: b/133213304
    Change-Id: I4f6c026723b350d69597452c5f9989d1d68058e1

--- src/Pipeline/SpirvShader.hpp
+++ src/Pipeline/SpirvShader.hpp
@@ -924,6 +924,19 @@ namespace sw
 			{
 			}
 
+			// fork() returns a new EmitState initialized with this state's
+			// routine, function, active lane mask and descriptor sets, and the
+			// parent set to this.
+			// fork() is used to create a new context in which to emit inlined
+			// functions. This is required to avoid duplicate definitions of
+			// SSA values when the same function is inlined multiple times.
+			EmitState fork()
+			{
+				EmitState out(routine, function, activeLaneMask(), descriptorSets);
+				out.parent = this;
+				return out;
+			}
+
 			RValue<SIMD::Int> activeLaneMask() const
 			{
 				ASSERT(activeLaneMaskValue != nullptr);
@@ -953,6 +966,7 @@ namespace sw
 			Block::Set visited; // Blocks already built.
 			std::unordered_map<Block::Edge, RValue<SIMD::Int>, Block::Edge::Hash> edgeActiveLaneMasks;
 			std::deque<Block::ID> *pending;
+			EmitState *parent = nullptr;
 
 			const vk::DescriptorSet::Bindings &descriptorSets;
 
@@ -969,6 +983,7 @@ namespace sw
 			{
 				id = resolve(id);
 				auto it = intermediates.find(id);
+				if (it == intermediates.end() && parent != nullptr) { return parent->getIntermediate(id); }
 				ASSERT_MSG(it != intermediates.end(), "Unknown intermediate %d", id.value());
 				return it->second;
 			}
@@ -983,6 +998,7 @@ namespace sw
 			{
 				id = resolve(id);
 				auto it = pointers.find(id);
+				if (it == pointers.end() && parent != nullptr) { return parent->getPointer(id); }
 				ASSERT_MSG(it != pointers.end(), "Unknown pointer %d", id.value());
 				return it->second;
 			}
@@ -1002,6 +1018,7 @@ namespace sw
 			Object::ID resolve(Object::ID id) const
 			{
 				auto it = aliases.find(id);
+				if (it == aliases.end() && parent != nullptr) { return parent->resolve(id); }
 				return (it != aliases.end()) ? resolve(it->second) : id;
 			}
 
