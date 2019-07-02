c92bf401c8d72a4c7e004736ed8fdf8a3ad0570e
Ben Clayton <bclayton@google.com>
2019-07-02 16:15:14 +0100

    SpirvShader: Optimize for targets with fast masked load / stores.
    Fixes the performance loss in https://swiftshader-review.googlesource.com/c/SwiftShader/+/33628.
    
    Bug: b/135609394
    Change-Id: I82bd857d05dcf663f667d2ea8a20075742ea9083

--- src/Pipeline/SpirvShader.cpp
+++ src/Pipeline/SpirvShader.cpp
@@ -400,7 +400,8 @@ namespace sw
 				}
 				else if (ptr.hasStaticSequentialOffsets(sizeof(float)))
 				{
-					if (ptr.isStaticAllInBounds(sizeof(float)))
+					if (!rr::GetCapabilities().HasFastMaskedStore &&
+						ptr.isStaticAllInBounds(sizeof(float)))
 					{
 						// Pointer has no elements OOB, and the store is not atomic.
 						// Perform a RMW.
