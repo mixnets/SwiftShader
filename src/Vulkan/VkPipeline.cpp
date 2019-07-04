e9b415471a7fe27fff8a19d72aae97ef1fe29c83
Ben Clayton <bclayton@google.com>
2019-07-04 15:56:03 +0100

    Performance Experiment: Remove the DeadBranchElim and MergeReturn passes
    Change-Id: Id31990c1dafd242db6bbc4aaf4652c6a05c96e82

--- src/Vulkan/VkPipeline.cpp
+++ src/Vulkan/VkPipeline.cpp
@@ -177,8 +177,6 @@ std::vector<uint32_t> preprocessSpirv(
 	});
 
 
-	opt.RegisterPass(spvtools::CreateDeadBranchElimPass()); // Required for MergeReturnPass
-	opt.RegisterPass(spvtools::CreateMergeReturnPass());
 	opt.RegisterPass(spvtools::CreateInlineExhaustivePass());
 	opt.RegisterPass(spvtools::CreateEliminateDeadFunctionsPass());
 
