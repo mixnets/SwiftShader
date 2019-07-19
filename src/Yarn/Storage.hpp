c9c4bf5cf4bdbd7fe453e5eb3794e997fc71ac0f
Ben Clayton <bclayton@google.com>
2019-07-19 22:50:09 +0100

    WIP / NFR: libyarn scheduler
    Change-Id: I5339aba4603d3abc996b6d297f87014d9f22d374

---
+++ src/Yarn/Storage.hpp
@@ -0,0 +1,226 @@
+// Copyright 2019 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef yarn_storage_hpp
+#define yarn_storage_hpp
+
+#include "Debug.hpp"
+
+namespace yarn {
+namespace storage {
+
+////////////////////////////////////////////////////////////////////////////////
+// Vector<T, BASE_CAPACITY>
+////////////////////////////////////////////////////////////////////////////////
+template <typename T, int BASE_CAPACITY>
+class Vector
+{
+public:
+    inline Vector() = default;
+    inline Vector(const Vector<T, BASE_CAPACITY>& other);
+    inline Vector(Vector<T, BASE_CAPACITY>&& other);
+    inline ~Vector();
+    inline Vector<T, BASE_CAPACITY>& operator = (const Vector<T, BASE_CAPACITY>&);
+    inline Vector<T, BASE_CAPACITY>& operator = (Vector<T, BASE_CAPACITY>&&);
+    inline void push_back(const T& el);
+    inline void emplace_back(T&& el);
+    inline void pop_back();
+    inline T& front();
+    inline T& back();
+    inline T* begin();
+    inline T* end();
+    inline T& operator[] (size_t i);
+    inline const T& operator[] (size_t i) const;
+    inline size_t size() const;
+    inline void resize(size_t n);
+    inline void reserve(size_t n);
+private:
+    inline void free();
+
+    size_t count = 0;
+    size_t capacity = BASE_CAPACITY;
+    T buffer[BASE_CAPACITY];
+    T* elements = buffer;
+};
+
+template <typename T, int BASE_CAPACITY>
+Vector<T, BASE_CAPACITY>::Vector(const Vector<T, BASE_CAPACITY>& other)
+{
+    *this = other;
+}
+
+template <typename T, int BASE_CAPACITY>
+Vector<T, BASE_CAPACITY>::Vector(Vector<T, BASE_CAPACITY>&& other)
+{
+    *this = std::move(other);
+}
+
+template <typename T, int BASE_CAPACITY>
+Vector<T, BASE_CAPACITY>::~Vector()
+{
+    free();
+}
+
+template <typename T, int BASE_CAPACITY>
+Vector<T, BASE_CAPACITY>& Vector<T, BASE_CAPACITY>::operator = (const Vector<T, BASE_CAPACITY>& other)
+{
+    free();
+    reserve(other.capacity);
+    count = other.count;
+    for (size_t i = 0; i < count; i++)
+    {
+        new(&elements[i]) T(other.elements[i]);
+    }
+}
+
+template <typename T, int BASE_CAPACITY>
+Vector<T, BASE_CAPACITY>& Vector<T, BASE_CAPACITY>::operator = (Vector<T, BASE_CAPACITY>&& other)
+{
+    free();
+    capacity = other.capacity;
+    count = other.count;
+    if (other.elements == other.buffer)
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            new(&elements[i]) T(other.elements[i]);
+        }
+    }
+    else
+    {
+        elements = other.elements;
+        other.elements = buffer;
+        other.capacity = BASE_CAPACITY;
+    }
+}
+
+template <typename T, int BASE_CAPACITY>
+void Vector<T, BASE_CAPACITY>::push_back(const T& el)
+{
+    reserve(count + 1);
+    new (&elements[count]) T(el);
+    count++;
+}
+
+template <typename T, int BASE_CAPACITY>
+void Vector<T, BASE_CAPACITY>::emplace_back(T&& el)
+{
+    reserve(count + 1);
+    new (&elements[count]) T(std::move(el));
+    count++;
+}
+
+template <typename T, int BASE_CAPACITY>
+void Vector<T, BASE_CAPACITY>::pop_back()
+{
+    YARN_ASSERT(count > 0, "pop_back() called on empty Vector");
+    count--;
+    elements[count].~T();
+}
+
+template <typename T, int BASE_CAPACITY>
+T& Vector<T, BASE_CAPACITY>::front()
+{
+    YARN_ASSERT(count > 0, "front() called on empty Vector");
+    return elements[0];
+}
+
+template <typename T, int BASE_CAPACITY>
+T& Vector<T, BASE_CAPACITY>::back()
+{
+    YARN_ASSERT(count > 0, "back() called on empty Vector");
+    return elements[count - 1];
+}
+
+template <typename T, int BASE_CAPACITY>
+T* Vector<T, BASE_CAPACITY>::begin()
+{
+    return elements;
+}
+
+template <typename T, int BASE_CAPACITY>
+T* Vector<T, BASE_CAPACITY>::end()
+{
+    return elements + count;
+}
+
+template <typename T, int BASE_CAPACITY>
+T& Vector<T, BASE_CAPACITY>::operator[] (size_t i)
+{
+    YARN_ASSERT(i < count, "index %d exceeds vector size %d", int(i), int(count));
+    return elements[i];
+}
+
+template <typename T, int BASE_CAPACITY>
+const T& Vector<T, BASE_CAPACITY>::operator[] (size_t i) const
+{
+    YARN_ASSERT(i < count, "index %d exceeds vector size %d", int(i), int(count));
+    return elements[i];
+}
+
+template <typename T, int BASE_CAPACITY>
+size_t Vector<T, BASE_CAPACITY>::size() const
+{
+    return count;
+}
+
+template <typename T, int BASE_CAPACITY>
+void Vector<T, BASE_CAPACITY>::resize(size_t n)
+{
+    reserve(n);
+    while (count < n)
+    {
+        new(&elements[count++]) T();
+    }
+    while (n < count)
+    {
+        elements[--count].~T();
+    }
+}
+
+template <typename T, int BASE_CAPACITY>
+void Vector<T, BASE_CAPACITY>::reserve(size_t n)
+{
+    if (n > capacity)
+    {
+        capacity = std::max<size_t>(n * 2, 8);
+        auto grown = reinterpret_cast<T*>(new T[capacity]);
+        for (size_t i = 0; i < count; i++)
+        {
+            new(&grown[i]) T(std::move(elements[i]));
+        }
+        free();
+        elements = grown;
+    }
+}
+
+template <typename T, int BASE_CAPACITY>
+void Vector<T, BASE_CAPACITY>::free()
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        elements[i].~T();
+    }
+
+    if (elements != buffer)
+    {
+        delete []elements;
+        elements = nullptr;
+    }
+}
+
+} // namespace storage
+} // namespace yarn
+
+#endif  // yarn_storage_hpp
---
+++ src/Yarn/TicketQueue.hpp
@@ -0,0 +1,215 @@
+// Copyright 2019 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef yarn_ticket_queue_hpp
+#define yarn_ticket_queue_hpp
+
+#include "Pool.hpp"
+#include "ConditionVariable.hpp"
+#include "Scheduler.hpp"
+
+namespace yarn {
+
+class Ticket
+{
+    struct Shared;
+
+    struct Record
+    {
+        inline ~Record();
+
+        std::shared_ptr<Shared> shared;
+        Record *next = nullptr; // guarded by shared->mutex
+        Record *prev = nullptr; // guarded by shared->mutex
+        inline void unlink(); // guarded by shared->mutex
+
+        inline void done();
+        inline void call();
+
+        std::atomic<bool> isDone = { false };
+
+        ConditionVariable calledCondVar;
+        std::mutex mutex;
+        Task onCall; // guarded by mutex
+        bool called; // guarded by mutex
+    };
+
+    struct Shared
+    {
+        std::mutex mutex;
+        Record tail;
+    };
+
+public:
+
+    class Queue
+    {
+    public:
+        inline Ticket take();
+
+        template <typename F>
+        inline void takeN(size_t n, const F& f);
+
+    private:
+        std::shared_ptr<Shared> shared = std::make_shared<Shared>();
+        UnboundedPool<Record> pool;
+    };
+
+    inline Ticket() = default;
+    inline Ticket(const Ticket& other) = default;
+    inline Ticket(Ticket&& other) = default;
+    inline Ticket& operator = (const Ticket& other) = default;
+
+    inline void wait() const;
+    inline void done() const;
+
+    template<typename Function>
+    inline void onCall(Function&& f) const;
+
+private:
+    inline Ticket(Loan<Record>&& record);
+    Loan<Record> record;
+};
+
+Ticket Ticket::Queue::take()
+{
+    Ticket out;
+    takeN(1, [&](Ticket&& ticket) { out = std::move(ticket); });
+    return out;
+}
+
+template <typename F>
+void Ticket::Queue::takeN(size_t n, const F& f)
+{
+    Loan<Record> first, last;
+    pool.borrowN(n, [&] (Loan<Record>&& record) {
+        Loan<Record> rec = record;
+        rec->shared = shared;
+        if (first.get() == nullptr)
+        {
+            first = rec;
+        }
+        if (last.get() != nullptr)
+        {
+            last->next = rec.get();
+            rec->prev = last.get();
+        }
+        last = rec;
+        f(std::move(Ticket(std::move(rec))));
+    });
+    last->next = &shared->tail;
+    std::unique_lock<std::mutex> lock(shared->mutex);
+    first->prev = shared->tail.prev;
+    shared->tail.prev = last.get();
+    if (first->prev == nullptr)
+    {
+        lock.unlock();
+        first->call();
+    }
+    else
+    {
+        first->prev->next = first.get();
+    }
+}
+
+
+Ticket::Ticket(Loan<Record>&& record) : record(std::move(record)) {}
+
+void Ticket::wait() const
+{
+    std::unique_lock<std::mutex> lock(record->mutex);
+    record->calledCondVar.wait(lock, [this] { return record->called; });
+}
+
+void Ticket::done() const
+{
+    record->done();
+}
+
+template<typename Function>
+void Ticket::onCall(Function&& f) const
+{
+    std::unique_lock<std::mutex> lock(record->mutex);
+    if (record->called)
+    {
+        yarn::schedule(std::move(f));
+        return;
+    }
+    if (record->onCall)
+    {
+        struct Joined
+        {
+            void operator() () const { a(); b(); }
+            Task a, b;
+        };
+        record->onCall = std::move(Joined{ std::move(record->onCall), std::move(f) });
+    }
+    else
+    {
+        record->onCall = std::move(f);
+    }
+}
+
+Ticket::Record::~Record()
+{
+    if (shared != nullptr)
+    {
+        done();
+    }
+}
+
+void Ticket::Record::done()
+{
+    if (isDone.exchange(true)) { return; }
+
+    std::unique_lock<std::mutex> lock(shared->mutex);
+    auto callNext = (prev == nullptr && next != nullptr) ? next : nullptr;
+    unlink();
+    if (callNext != nullptr) // lock needs to be held otherwise callNext might be destructed.
+    {
+        callNext->call();
+    }
+}
+
+void Ticket::Record::call()
+{
+    std::unique_lock<std::mutex> lock(mutex);
+    auto notify = !called;
+    called = true;
+    Task task;
+    std::swap(task, onCall);
+
+    lock.unlock();
+
+    if (notify)
+    {
+        calledCondVar.notify_all();
+    }
+    if (task)
+    {
+        yarn::schedule(std::move(task));
+    }
+}
+
+void Ticket::Record::unlink()
+{
+    if (prev != nullptr) { prev->next = next; }
+    if (next != nullptr) { next->prev = prev; }
+    prev = nullptr;
+    next = nullptr;
+}
+
+} // namespace yarn
+
+#endif  // yarn_ticket_queue_hpp
