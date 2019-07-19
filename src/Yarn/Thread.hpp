738dc1a69b9b0557e28702287d67405b57713215
Ben Clayton <bclayton@google.com>
2019-07-19 22:50:09 +0100

    WIP / NFR: libyarn scheduler
    Change-Id: I5339aba4603d3abc996b6d297f87014d9f22d374

---
+++ src/Yarn/Thread.hpp
@@ -0,0 +1,37 @@
+// Copyright 2019 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef yarn_thread_hpp
+#define yarn_thread_hpp
+
+#include <thread>
+
+namespace yarn {
+
+class Thread
+{
+public:
+    static Thread current();
+
+    static void setName(const char* fmt, ...);
+
+private:
+    Thread(std::thread::native_handle_type handle);
+
+    std::thread::native_handle_type handle;
+};
+
+} // namespace yarn
+
+#endif  // yarn_thread_hpp
