dac433195128a70e01f2887602277f72906b562e
Ben Clayton <bclayton@google.com>
2019-08-08 09:32:10 +0100

    Yarn: Add Trace class
    When enabled, it writes out a chrome trace file populated with events from the YARN_* event macros.
    
    Bug: b/139010488
    Change-Id: Ifae27b2bcf08f767e9886664b1a998162a617108

--- src/Yarn/Scheduler.cpp
+++ src/Yarn/Scheduler.cpp
@@ -19,9 +19,19 @@
 #include "Debug.hpp"
 #include "Defer.hpp"
 #include "Thread.hpp"
+#include "Trace.hpp"
 
 #if defined(_WIN32)
 #include <intrin.h> // __nop()
+#endif
+
+// Enable to trace scheduler events.
+#define ENABLE_TRACE_EVENTS 0
+
+#if ENABLE_TRACE_EVENTS
+#define TRACE(...) YARN_SCOPED_EVENT(__VA_ARGS__)
+#else
+#define TRACE(...)
 #endif
 
 namespace
@@ -232,6 +242,7 @@ void Scheduler::Fiber::schedule()
 
 void Scheduler::Fiber::yield()
 {
+    YARN_SCOPED_EVENT("YIELD");
     worker->yield(this);
 }
 
@@ -402,6 +413,8 @@ void Scheduler::Worker::run()
     {
     case Mode::MultiThreaded:
     {
+        YARN_NAME_THREAD("Thread<%.2d> Fiber<%.2d>", int(id), Fiber::current()->id);
+
         std::unique_lock<std::mutex> lock(work.mutex);
         work.added.wait(lock, [this] { return work.num > 0 || shutdown; });
         while (!shutdown)
@@ -443,6 +456,7 @@ void Scheduler::Worker::waitForWork(std::unique_lock<std::mutex> &lock)
 
 void Scheduler::Worker::spinForWork()
 {
+    TRACE("SPIN");
     Task stolen;
 
     constexpr auto duration = std::chrono::milliseconds(1);
