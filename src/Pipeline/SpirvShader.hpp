ea5a7990b470a7de9aeea75e3a28ed2d4ede9c33
Ben Clayton <bclayton@google.com>
2019-08-09 23:44:09 +0100

    Vulkan: Implement shaderClipDistance and shaderCullDistance
    The clip distance is used to perform per-fragment clipping - any fragments with a negative linear-interpolated distance are discarded.
    
    The cull distance is used to perform per-primitive culling - any primitives with all vertices with a negative distance are discarded.
    
    Bug: b/139207336
    Tests: dEQP-VK.clipping.*
    Change-Id: Ia6680601b27599152f68410df47aaaa726d0b349

--- src/Pipeline/SpirvShader.hpp
+++ src/Pipeline/SpirvShader.hpp
@@ -727,6 +727,8 @@ namespace sw
 		{
 			bool Matrix : 1;
 			bool Shader : 1;
+			bool ClipDistance : 1;
+			bool CullDistance : 1;
 			bool InputAttachment : 1;
 			bool Sampled1D : 1;
 			bool Image1D : 1;
@@ -735,12 +737,12 @@ namespace sw
 			bool ImageQuery : 1;
 			bool DerivativeControl : 1;
 			bool GroupNonUniform : 1;
-			bool MultiView : 1;
-			bool DeviceGroup : 1;
 			bool GroupNonUniformVote : 1;
 			bool GroupNonUniformBallot : 1;
 			bool GroupNonUniformShuffle : 1;
 			bool GroupNonUniformShuffleRelative : 1;
+			bool DeviceGroup : 1;
+			bool MultiView : 1;
 		};
 
 		Capabilities const &getUsedCapabilities() const
