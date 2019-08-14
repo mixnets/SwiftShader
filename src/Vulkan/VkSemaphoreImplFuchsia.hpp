5ba58befa400cc4180c4693760d2fa5cf6349545
David 'Digit' Turner <digit@google.com>
2019-08-14 11:41:40 +0200

    [vulkan] Implement proper VkSemaphore support.
    DO NOT SUBMIT: NEEDS TO BE TESTED AGAINST Vulkan CTS
    
    This CL adds proper platform-specific implementations for
    non-external VkSemaphore objects, based on the following:
    
      - Fuchsia: Zircon event handles.
      - Win32: Semaphores (available since Windows XP).
      - Anything else: pthread mutex + condition variable.
    
    Using header files for the implementation classes simplifies
    the changes to the build scripts (i.e. both CMakeLists.txt and
    Android.bp don't need to be changed).
    
    Future CLs will add support for external semaphores.
    
    Change-Id: I5e51f9d9e9b67ea9d8d159394195f8cacfea56a3

---
+++ src/Vulkan/VkSemaphoreImplFuchsia.hpp
@@ -0,0 +1,87 @@
+// Copyright 2019 The SwiftShader Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VK_SEMAPHORE_IMPL_WIN32_H_
+#define VK_SEMAPHORE_IMPL_WIN32_H_
+
+#include "VkDebug.hpp"
+
+// An implementation of vk::Semaphore for Fuchsia
+
+#include <zircon/syscalls.h>
+
+namespace vk
+{
+
+class Semaphore::Impl {
+public:
+	Impl(const VkSemaphoreCreateInfo* pCreateInfo)
+	{
+		// TODO: external semaphore support!
+		(void)pCreateInfo;
+
+		zx_status_t status = zx_event_create(0, &handle);
+		if (status != ZX_OK)
+		{
+			ABORT("zx_event_create() returned %d", status);
+		}
+	}
+
+	~Impl()
+	{
+		zx_handle_close(handle);
+	}
+
+	void wait()
+	{
+		zx_signals_t observed = 0;
+		zx_status_t status = zx_object_wait_one(
+				handle, ZX_EVENT_SIGNALED, ZX_TIME_INFINITE, &observed);
+		if (status != ZX_OK)
+		{
+			ABORT("zx_object_wait_one() returned %d", status);
+		}
+		if (observed != ZX_EVENT_SIGNALED)
+		{
+			ABORT("zx_object_wait_one() returned observed %x (%x expected)", observed, ZX_EVENT_SIGNALED);
+		}
+		// Need to unsignal the event now, as required by the Vulkan spec.
+		status = zx_object_signal(handle, ZX_EVENT_SIGNALED, 0);
+		if (status != ZX_OK)
+		{
+			ABORT("zx_object_signal() returned %d", status);
+		}
+	}
+
+	void wait(const VkPipelineStageFlags& flags)
+	{
+		wait();
+	}
+
+	void signal()
+	{
+		zx_status_t status = zx_object_signal(handle, 0, ZX_EVENT_SIGNALED);
+		if (status != ZX_OK)
+		{
+			ABORT("zx_object_signal() returned %d", status);
+		}
+	}
+	
+private:
+	zx_handle_t handle = ZX_HANDLE_INVALID;
+};
+
+}  // namespace vk
+
+#endif  // VK_SEMAPHORE_IMPL_WIN32_H_
