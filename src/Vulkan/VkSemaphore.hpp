417c376ce22d2d3c35f2241f015603d6095370e8
David 'Digit' Turner <digit@google.com>
2019-09-03 17:00:02 +0200

    [vulkan] Implement Linux-based external semaphore
    DO NOT SUBMIT: Experimental CL to add external semaphore
    support for Linux and Android based in a process-shared
    pthread mutex + condition variable stored in a shared
    memory region backed by memfd_create().
    
    This takes care of waiting for external semaphores
    in a background thread when invoked from a Yarn fiber.

--- src/Vulkan/VkSemaphore.hpp
+++ src/Vulkan/VkSemaphore.hpp
@@ -15,6 +15,7 @@
 #ifndef VK_SEMAPHORE_HPP_
 #define VK_SEMAPHORE_HPP_
 
+#include "VkConfig.h"
 #include "VkObject.hpp"
 
 namespace vk
@@ -37,7 +38,13 @@ public:
 
 	void signal();
 
+#if SWIFTSHADER_EXTERNAL_SEMAPHORE_LINUX_MEMFD
+	VkResult importFd(int fd);
+	int exportFd();
+#endif
+
 private:
+	class External;
 	class Impl;
 	Impl* impl = nullptr;
 };
