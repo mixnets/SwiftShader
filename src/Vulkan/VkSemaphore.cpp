1fb1687a49c0666f45caec9d472026bd78646d6b
David 'Digit' Turner <digit@google.com>
2019-09-03 17:00:02 +0200

    [vulkan] Implement Linux-based external semaphore
    Add external semaphore support for Linux and Android
    based on a process-shared pthread mutex + condition
    variable stored in a shared memory region backed by
    memfd_create().
    
    This takes care of waiting for external semaphores
    in a background thread when invoked from a Yarn fiber.
    
    Bug: b/140421726
    Change-Id: Ifa74c807d3e33914e5a37dd96650c312246c8e4f

--- src/Vulkan/VkSemaphore.cpp
+++ src/Vulkan/VkSemaphore.cpp
@@ -14,8 +14,21 @@
 
 #include "VkSemaphore.hpp"
 
+#include "VkConfig.h"
+
+#if SWIFTSHADER_EXTERNAL_SEMAPHORE_LINUX_MEMFD
+#include "VkSemaphoreExternalLinux.hpp"
+#else
+#include "VkSemaphoreExternalNone.hpp"
+#endif
+
+#include "marl/blockingcall.h"
 #include "marl/conditionvariable.h"
+
+#include <functional>
+#include <memory>
 #include <mutex>
+#include <utility>
 
 namespace vk
 {
@@ -24,17 +37,125 @@ namespace vk
 class Semaphore::Impl
 {
 public:
-	Impl() = default;
+	// Create a new instance. The external instance will be allocated only
+	// the pCreateInfo->pNext chain indicates it needs to be exported.
+	Impl(const VkSemaphoreCreateInfo* pCreateInfo) {
+		bool exportSemaphore = false;
+		for (const auto* nextInfo = reinterpret_cast<const VkBaseInStructure*>(pCreateInfo->pNext);
+			 nextInfo != nullptr; nextInfo = nextInfo->pNext)
+		{
+			if (nextInfo->sType == VK_STRUCTURE_TYPE_EXPORT_SEMAPHORE_CREATE_INFO)
+			{
+				const auto* exportInfo = reinterpret_cast<const VkExportSemaphoreCreateInfo *>(nextInfo);
+				if (exportInfo->handleTypes != External::kExternalSemaphoreHandleType)
+				{
+					UNIMPLEMENTED("exportInfo->handleTypes");
+				}
+				exportSemaphore = true;
+				break;
+			}
+		}
+
+		if (exportSemaphore)
+		{
+			allocateExternalNoInit();
+			external->init();
+		}
+	}
+
+	~Impl() {
+		deallocateExternal();
+	}
+
+	// Deallocate the External semaphore if any.
+	void deallocateExternal()
+	{
+		if (external)
+		{
+			external->~External();
+			external = nullptr;
+		}
+	}
+
+	// Allocate the external semaphore.
+	// Note that this does not allocate the internal resource, which must be
+	// performed by calling external->init(), or importing one using
+	// a platform-specific external->importXXX(...) method.
+	void allocateExternalNoInit()
+	{
+		external = new (externalStorage) External();
+	}
 
 	void wait()
 	{
+		if (external)
+		{
+			if (!external->tryWait())
+			{
+				// Dispatch the external wait to a background thread, then let it
+				// signal the internal semaphore to wait for it on the main
+				// fiber.
+				//
+				// NOTE: This currently creates a new std::thread on each call,
+				// and benchmarking shows that this is about 3 to 4 times slower
+				// than actually using a dedicated background thread loop to
+				// do each wait (e.g. on Linux, 24us vs 8us on Xeon E5-2690 @
+				// 2.6 GHz).
+				//
+				// However, it is assumed here that this difference is negligible
+				// compared with the actual semaphore wait() time that will occur.
+
+				// NOTE: The following does not seem to compile and generate
+				// template expansion errors at the moment, so replace it with
+				// an explicit std::thread() creation + signaling the internal
+				// semaphore instead (which is functionally equivalent):
+				//
+				// 		marl::blocking_call([this](){
+				// 			external->wait();
+				// 		});
+				//
+				auto thread = std::thread([this](){
+					external->wait();
+					signalInternal();
+				});
+				waitInternal();
+			}
+		}
+		else
+		{
+			waitInternal();
+		}
+	}
+
+	void signal()
+	{
+		if (external)
+		{
+			// Assumes that signalling an external semaphore is non-blocking,
+			// so it can be performed directly either from a fiber or thread.
+			external->signal();
+		}
+		else
+		{
+			signalInternal();
+		}
+	}
+
+private:
+	// Necessary to make ::importXXX() and ::exportXXX() simpler.
+	friend Semaphore;
+
+	void waitInternal()
+	{
+		// Wait on the marl condition variable only.
 		std::unique_lock<std::mutex> lock(mutex);
 		condition.wait(lock, [this]{ return this->signaled; });
 		signaled = false;  // Vulkan requires resetting after waiting.
 	}
 
-	void signal()
+	void signalInternal()
 	{
+		// Signal the marl condition variable only.
 		std::unique_lock<std::mutex> lock(mutex);
 		if (!signaled)
 		{
@@ -43,15 +164,20 @@ public:
 		}
 	}
 
-private:
+	// Implementation of a non-external semaphore based on Marl.
 	std::mutex mutex;
 	marl::ConditionVariable condition;
 	bool signaled = false;
+
+	// Optional external semaphore data might be referenced and stored here.
+	External* external = nullptr;
+
+	alignas(External) char externalStorage[sizeof(External)];
 };
 
 Semaphore::Semaphore(const VkSemaphoreCreateInfo* pCreateInfo, void* mem)
 {
-	impl = new (mem) Impl();
+	impl = new (mem) Impl(pCreateInfo);
 }
 
 void Semaphore::destroy(const VkAllocationCallbacks* pAllocator)
@@ -74,5 +200,29 @@ void Semaphore::signal()
 {
 	impl->signal();
 }
+
+#if SWIFTSHADER_EXTERNAL_SEMAPHORE_LINUX_MEMFD
+VkResult Semaphore::importFd(int fd)
+{
+	std::unique_lock<std::mutex> lock(impl->mutex);
+	if (!impl->external)
+	{
+		impl->allocateExternalNoInit();
+	}
+	impl->external->importFd(fd);
+	return VK_SUCCESS;
+}
+
+VkResult Semaphore::exportFd(int* pFd) const
+{
+	std::unique_lock<std::mutex> lock(impl->mutex);
+	if (!impl->external)
+	{
+		TRACE("Cannot export non-external semaphore");
+		return VK_ERROR_INVALID_EXTERNAL_HANDLE;
+	}
+	return impl->external->exportFd(pFd);
+}
+#endif  // SWIFTSHADER_EXTERNAL_SEMAPHORE_LINUX_MEMFD
 
 }  // namespace vk
