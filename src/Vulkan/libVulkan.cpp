fe39e697992d7b28d3e3d4f2494e6b348490b6a5
Sean Risser <srisser@google.com>
2019-09-06 13:06:26 -0400

    Log unsupported extensions used in pNext structs
    The Vulkan spec states that if an implementation does not support an
    extension, then it must ignore all fields other than pNext and pType and
    continue on without processing any further information contained in the
    extension's structure. We've been using asserts to log these situations
    so we can tell which extension structs people are trying to use.
    
    This CL changes our behavior so we silently log these unsupported extensions,
    and warn a user once if they're debugging.
    
    Bug: b/139528538
    Change-Id: I8d31f147e22f670c9c10c64e239085700eae73a3

--- src/Vulkan/libVulkan.cpp
+++ src/Vulkan/libVulkan.cpp
@@ -40,6 +40,7 @@
 #include "VkSampler.hpp"
 #include "VkSemaphore.hpp"
 #include "VkShaderModule.hpp"
+#include "VkStringify.hpp"
 #include "VkRenderPass.hpp"
 
 #if defined(VK_USE_PLATFORM_METAL_EXT) || defined(VK_USE_PLATFORM_MACOS_MVK)
@@ -620,7 +621,7 @@ VKAPI_ATTR VkResult VKAPI_CALL vkCreateDevice(VkPhysicalDevice physicalDevice, c
 			break;
 		default:
 			// "the [driver] must skip over, without processing (other than reading the sType and pNext members) any structures in the chain with sType values not defined by [supported extenions]"
-			UNIMPLEMENTED("extensionCreateInfo->sType %d", int(extensionCreateInfo->sType));   // TODO(b/119321052): UNIMPLEMENTED() should be used only for features that must still be implemented. Use a more informational macro here.
+			TRACE_ASSERT("Unimplemented extensionCreateInfo->sType = %s", Stringify(extensionCreateInfo->sType));
 			break;
 		}
 
@@ -783,7 +784,7 @@ VKAPI_ATTR VkResult VKAPI_CALL vkAllocateMemory(VkDevice device, const VkMemoryA
 			// SwiftShader only has a single physical device, so this extension does nothing in this case.
 			break;
 		default:
-			UNIMPLEMENTED("allocationInfo->sType");
+			TRACE_ASSERT("Unimplemented allocationInfo->sType = %s", Stringify(allocationInfo->sType));
 			break;
 		}
 
