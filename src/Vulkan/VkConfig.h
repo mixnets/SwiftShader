1a61655680d4b419327a8fb772778b869c752988
Antonio Maiorano <amaiorano@google.com>
2019-09-27 13:05:48 -0400

    Set default subpixel precision to 8 bits (from 4)
    Bug: b/141676114
    Change-Id: Ic27516d924a2e6fba15eb4f0c03f31dc14fb857a

--- src/Vulkan/VkConfig.h
+++ src/Vulkan/VkConfig.h
@@ -78,7 +78,7 @@ enum
 	MAX_POINT_SIZE = 1,		// Large points are not supported. If/when we turn this on, must be >= 64.
 };
 
-constexpr int SUBPIXEL_PRECISION_BITS = 4;
+constexpr int SUBPIXEL_PRECISION_BITS = 8;
 constexpr float SUBPIXEL_PRECISION_FACTOR = static_cast<float>(1 << SUBPIXEL_PRECISION_BITS);
 constexpr int SUBPIXEL_PRECISION_MASK = 0xFFFFFFFF >> (32 - SUBPIXEL_PRECISION_BITS);
 
