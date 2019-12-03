62c0325314222cce168c46670abcfbbaebd7b153
Ben Clayton <bclayton@google.com>
2019-12-03 13:28:53 +0000

    Pipeline: Implement the SpirvShader debugger.
    Generate a synthetic file containing the spirv disassembly, and allow the debugger to single line step over these instructions, along with inspection of the SSA values.
    
    All of this is no-op unless ENABLE_VK_DEBUGGER is defined at compile time, and the VK_DEBUG_PORT env var is set at run time.
    
    Bug: b/145351270
    Change-Id: Iba71717d78f7213ba586a1632b44f5fe08addf08

--- src/Pipeline/SpirvShader.cpp
+++ src/Pipeline/SpirvShader.cpp
@@ -18,6 +18,8 @@
 #include "Vulkan/VkPipelineLayout.hpp"
 #include "Vulkan/VkRenderPass.hpp"
 
+#include "marl/defer.h"
+
 #include <spirv/unified1/spirv.hpp>
 
 namespace sw
@@ -38,6 +40,10 @@ namespace sw
 				  robustBufferAccess(robustBufferAccess)
 	{
 		ASSERT(insns.size() > 0);
+
+		if (dbgctx){
+			dbgInit(dbgctx);
+		}
 
 		if (renderPass)
 		{
@@ -690,6 +696,13 @@ namespace sw
 		{
 			it.second.AssignBlockFields();
 		}
+
+		dbgCreateFile();
+	}
+
+	SpirvShader::~SpirvShader()
+	{
+		dbgTerm();
 	}
 
 	void SpirvShader::DeclareType(InsnIterator insn)
@@ -1421,6 +1434,7 @@ namespace sw
 		}
 
 		object.definition = insn;
+		dbgDeclareResult(insn, resultId);
 	}
 
 	OutOfBoundsBehavior SpirvShader::EmitState::getOutOfBoundsBehavior(spv::StorageClass storageClass) const
@@ -1511,6 +1525,9 @@ namespace sw
 	{
 		EmitState state(routine, entryPoint, activeLaneMask, storesAndAtomicsMask, descriptorSets, robustBufferAccess, executionModel);
 
+		dbgBeginEmit(&state);
+		defer(dbgEndEmit(&state));
+
 		// Emit everything up to the first label
 		// TODO: Separate out dispatch of block from non-block instructions?
 		for (auto insn : *this)
@@ -1546,6 +1563,9 @@ namespace sw
 
 	SpirvShader::EmitResult SpirvShader::EmitInstruction(InsnIterator insn, EmitState *state) const
 	{
+		dbgBeginEmitInstruction(insn, state);
+		defer(dbgEndEmitInstruction(insn, state));
+
 		auto opcode = insn.opcode();
 
 		switch (opcode)
