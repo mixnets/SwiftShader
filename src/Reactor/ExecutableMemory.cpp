81fc30544fae3729a1575b7aea91322fb4e45ade
Sergey Ulanov <sergeyu@chromium.org>
2019-12-11 11:37:27 -0800

    Use allocateExecutable() in LLVMReactor
    LLVMReactor was using default memory mapper when creating
    SectionMemoryManager. The default memory mapper calls mmap() to allocate
    memory on all platforms except Windows. It doesn't specify MAP_JIT flag
    that is required to create executable memory on Mac and Fuchsia. As
    result the allocated memory is not executable, which leads to crashes
    when trying to run the generated code. Update LLVMRector to use a custom
    MemoryMapper implementation.
    
    Bug: chromium:1032622
    Change-Id: Id188af269d80371b709e625c29b92ff546e9ba28

--- src/Reactor/ExecutableMemory.cpp
+++ src/Reactor/ExecutableMemory.cpp
@@ -89,7 +89,47 @@ void *allocateRaw(size_t bytes, size_t alignment)
 	#endif
 }
 
+#if defined(_WIN32)
+DWORD permissionsToProtectMode(int permissions)
+{
+	switch (permissions) {
+		case PERMISSION_READ:
+		  return PAGE_READONLY;
+		case PERMISSION_EXECUTE:
+		  return PAGE_EXECUTE;
+		case PERMISSION_READ | PERMISSION_WRITE:
+		  return PAGE_READWRITE;
+		case PERMISSION_READ | PERMISSION_EXECUTE:
+		  return PAGE_EXECUTE_READ;
+		case PERMISSION_READ | PERMISSION_WRITE | PERMISSION_EXECUTE:
+		  return PAGE_EXECUTE_READWRITE;
+	}
+	return PAGE_NOACCESS;
+}
+#endif
+
+#if !defined(_WIN32) && !defined(__Fuchsia__)
+int permissionsToMmapProt(int permissions)
+{
+	int result = 0;
+	if (permissions & PERMISSION_READ)
+	{
+		result |= PROT_READ;
+	}
+	if (permissions & PERMISSION_WRITE)
+	{
+		result |= PROT_WRITE;
+	}
+	if (permissions & PERMISSION_EXECUTE)
+	{
+		result |= PROT_EXEC;
+	}
+	return result;
+}
+#endif  // !defined(_WIN32) && !defined(__Fuchsia__)
+
 #if defined(LINUX_ENABLE_NAMED_MMAP)
+
 // Create a file descriptor for anonymous memory with the given
 // name. Returns -1 on failure.
 // TODO: remove once libc wrapper exists.
@@ -135,6 +175,25 @@ void ensureAnonFileSize(int anonFd, size_t length)
 	}
 }
 #endif  // defined(LINUX_ENABLE_NAMED_MMAP)
+
+#if defined(__Fuchsia__)
+zx_vm_option_t permissionsToZxVmOptions(int permissions) {
+	zx_vm_option_t result = 0;
+	if (permissions & PERMISSION_READ)
+	{
+		result |= ZX_VM_PERM_READ;
+	}
+	if (permissions & PERMISSION_WRITE)
+	{
+		result |= ZX_VM_PERM_WRITE;
+	}
+	if (permissions & PERMISSION_EXECUTE)
+	{
+		result |= ZX_VM_PERM_EXECUTE;
+	}
+	return result;
+}
+#endif  // defined(__Fuchsia__)
 
 }  // anonymous namespace
 
@@ -190,27 +249,29 @@ inline uintptr_t roundUp(uintptr_t x, uintptr_t m)
 	return (x + m - 1) & ~(m - 1);
 }
 
-void *allocateExecutable(size_t bytes)
+void *allocateExecutable(size_t bytes, int permissions, bool need_exec)
 {
 	size_t pageSize = memoryPageSize();
 	size_t length = roundUp(bytes, pageSize);
 	void *mapping = nullptr;
 
 	#if defined(LINUX_ENABLE_NAMED_MMAP)
+		int flags = MAP_PRIVATE;
+
 		// Try to name the memory region for the executable code,
 		// to aid profilers.
 		int anonFd = anonymousFd();
 		if(anonFd == -1)
 		{
-			mapping = mmap(nullptr, length, PROT_READ | PROT_WRITE,
-			               MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+			flags |= MAP_ANONYMOUS;
 		}
 		else
 		{
 			ensureAnonFileSize(anonFd, length);
-			mapping = mmap(nullptr, length, PROT_READ | PROT_WRITE,
-			               MAP_PRIVATE, anonFd, 0);
 		}
+
+		mapping = mmap(
+			nullptr, length, permissionsToMmapProt(permissions), flags, anonFd, 0);
 
 		if(mapping == MAP_FAILED)
 		{
@@ -221,13 +282,15 @@ void *allocateExecutable(size_t bytes)
 		if (zx_vmo_create(length, 0, &vmo) != ZX_OK) {
 			return nullptr;
 		}
-		if (zx_vmo_replace_as_executable(vmo, ZX_HANDLE_INVALID, &vmo) != ZX_OK) {
+		if (need_exec &&
+		    zx_vmo_replace_as_executable(vmo, ZX_HANDLE_INVALID, &vmo) != ZX_OK)
+		{
 			return nullptr;
 		}
 		zx_vaddr_t reservation;
 		zx_status_t status = zx_vmar_map(
-			zx_vmar_root_self(), ZX_VM_PERM_READ | ZX_VM_PERM_WRITE,
-			0, vmo, 0, length, &reservation);
+			zx_vmar_root_self(), permissionsToZxVmOptions(permissions), 0, vmo,
+			0, length, &reservation);
 		zx_handle_close(vmo);
 		if (status != ZX_OK) {
 			return nullptr;
@@ -238,18 +301,18 @@ void *allocateExecutable(size_t bytes)
 
 		mapping = reinterpret_cast<void*>(reservation);
 	#elif defined(__APPLE__)
+		int prot = permissionsToMmapProt()
+		int flags = MAP_PRIVATE | MAP_ANONYMOUS;
 		// On macOS 10.14 and higher, executables that are code signed with the
 		// "runtime" option cannot execute writable memory by default. They can opt
 		// into this capability by specifying the "com.apple.security.cs.allow-jit"
 		// code signing entitlement and allocating the region with the MAP_JIT flag.
-		mapping = mmap(nullptr, length, PROT_READ | PROT_WRITE,
-		               MAP_PRIVATE | MAP_ANONYMOUS | MAP_JIT, -1, 0);
+		mapping = mmap(nullptr, length, prot, flags | MAP_JIT, -1, 0);
 
 		if(mapping == MAP_FAILED)
 		{
 			// Retry without MAP_JIT (for older macOS versions).
-			mapping = mmap(nullptr, length, PROT_READ | PROT_WRITE,
-			               MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+			mapping = mmap(nullptr, length, prot, flags, -1, 0);
 		}
 
 		if(mapping == MAP_FAILED)
@@ -258,25 +321,31 @@ void *allocateExecutable(size_t bytes)
 		}
 	#else
 		mapping = allocate(length, pageSize);
+		protectExecutable(mapping, length, permissions);
 	#endif
 
 	return mapping;
 }
 
-void markExecutable(void *memory, size_t bytes)
+void protectExecutable(void *memory, size_t bytes, int permissions)
 {
 	#if defined(_WIN32)
 		unsigned long oldProtection;
-		VirtualProtect(memory, bytes, PAGE_EXECUTE_READ, &oldProtection);
+		BOOL result =
+			VirtualProtect(memory, bytes, permissionsToProtectMode(permissions),
+			               &oldProtection);
+		ASSERT(result);
 	#elif defined(__Fuchsia__)
 		size_t pageSize = memoryPageSize();
 		size_t length = roundUp(bytes, pageSize);
 		zx_status_t status = zx_vmar_protect(
-			zx_vmar_root_self(), ZX_VM_PERM_READ | ZX_VM_PERM_EXECUTE,
+			zx_vmar_root_self(), permissionsToZxVmOptions(permissions),
 			reinterpret_cast<zx_vaddr_t>(memory), length);
 		ASSERT(status == ZX_OK);
 	#else
-		mprotect(memory, bytes, PROT_READ | PROT_EXEC);
+		int result =
+			mprotect(memory, bytes, permissionsToMmapProt(permissions));
+		ASSERT(result == 0);
 	#endif
 }
 
@@ -284,21 +353,26 @@ void deallocateExecutable(void *memory, size_t bytes)
 {
 	#if defined(_WIN32)
 		unsigned long oldProtection;
-		VirtualProtect(memory, bytes, PAGE_READWRITE, &oldProtection);
+		BOOL result =
+			VirtualProtect(memory, bytes, PAGE_READWRITE, &oldProtection);
+		ASSERT(result);
 		deallocate(memory);
 	#elif defined(LINUX_ENABLE_NAMED_MMAP) || defined(__APPLE__)
 		size_t pageSize = memoryPageSize();
 		size_t length = (bytes + pageSize - 1) & ~(pageSize - 1);
-		munmap(memory, length);
+		int result = munmap(memory, length);
+		ASSERT(result == 0);
 	#elif defined(__Fuchsia__)
 		size_t pageSize = memoryPageSize();
 		size_t length = roundUp(bytes, pageSize);
 		zx_status_t status =  zx_vmar_unmap(
-		    zx_vmar_root_self(), reinterpret_cast<zx_vaddr_t>(memory), length);
+			zx_vmar_root_self(), reinterpret_cast<zx_vaddr_t>(memory), length);
 		ASSERT(status == ZX_OK);
 	#else
-		mprotect(memory, bytes, PROT_READ | PROT_WRITE);
+		int result = mprotect(memory, bytes, PROT_READ | PROT_WRITE);
+		ASSERT(result == 0);
 		deallocate(memory);
 	#endif
 }
-}
+
+}  // namespace rr
