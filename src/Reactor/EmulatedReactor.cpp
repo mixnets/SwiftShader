be8c2c1b2a612319a40e6cb4277e24f08179b69c
Antonio Maiorano <amaiorano@google.com>
2019-12-31 11:36:07 -0500

    SubzeroReactor: implemented missing atomic ops
    * Most use Subzero intrinsics, except for Min/MaxAtomic, which are
    emulated.
    * Added unit tests for each implemented function, but am not really
    testing that they behave as atomic functions. Only that they perform the
    expected operation.
    
    Bug: b/145754674
    Change-Id: Ie3ec6e473ee8b448b28bf440da094ac03ac0005b

--- src/Reactor/EmulatedReactor.cpp
+++ src/Reactor/EmulatedReactor.cpp
@@ -14,8 +14,10 @@
 
 #include "EmulatedReactor.hpp"
 
+#include <algorithm>
 #include <cmath>
 #include <functional>
+#include <mutex>
 #include <utility>
 
 namespace rr {
@@ -94,6 +96,30 @@ void scatter(RValue<Pointer<EL>> base, RValue<T> val, RValue<Int4> offsets, RVal
 			Store(Extract(val, i), Pointer<EL>(&baseBytePtr[offset]), alignment, atomic, order);
 		}
 	}
+}
+
+// TODO: Both atomicMin and atomicMax use a static (global) mutex that makes all min operations for a given T mutually exclusive,
+// rather than only the ones on the value pointed to by ptr. We need a proper atomic operation here, or at the very least, a unique
+// mutex per pointed-to value.
+template<typename T>
+static T atomicMin(T *ptr, T value)
+{
+	static std::mutex m;
+
+	std::lock_guard<std::mutex> lock(m);
+	T origValue = *ptr;
+	*ptr = std::min(origValue, value);
+	return origValue;
+}
+template<typename T>
+static T atomicMax(T *ptr, T value)
+{
+	static std::mutex m;
+
+	std::lock_guard<std::mutex> lock(m);
+	T origValue = *ptr;
+	*ptr = std::max(origValue, value);
+	return origValue;
 }
 
 }  // anonymous namespace
@@ -222,6 +248,26 @@ RValue<Float4> Exp2(RValue<Float4> x)
 RValue<Float4> Log2(RValue<Float4> x)
 {
 	return call4(log2f, x);
+}
+
+RValue<Int> MinAtomic(RValue<Pointer<Int>> x, RValue<Int> y, std::memory_order memoryOrder)
+{
+	return Call(atomicMin<int32_t>, x, y);
+}
+
+RValue<UInt> MinAtomic(RValue<Pointer<UInt>> x, RValue<UInt> y, std::memory_order memoryOrder)
+{
+	return Call(atomicMin<uint32_t>, x, y);
+}
+
+RValue<Int> MaxAtomic(RValue<Pointer<Int>> x, RValue<Int> y, std::memory_order memoryOrder)
+{
+	return Call(atomicMax<int32_t>, x, y);
+}
+
+RValue<UInt> MaxAtomic(RValue<Pointer<UInt>> x, RValue<UInt> y, std::memory_order memoryOrder)
+{
+	return Call(atomicMax<uint32_t>, x, y);
 }
 
 }  // namespace emulated
