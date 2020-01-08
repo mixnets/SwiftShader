0ce34612a25414616203c4d481ccd29133a55513
Ben Clayton <bclayton@google.com>
2020-01-08 13:03:11 +0000

    WIP/NFR: Start work on consuming OpenCLDebugInfo100
    Bug: b/145351270
    Change-Id: I138ff20a9c912eb465ef97aac60eac07d4b0ec16

--- src/Vulkan/Debug/Server.cpp
+++ src/Vulkan/Debug/Server.cpp
@@ -32,7 +32,7 @@
 #define ENABLE_DAP_LOGGING 0
 
 #if ENABLE_DAP_LOGGING
-#	define DAP_LOG(msg, ...) printf(msg "\n", __VA_ARGS__)
+#	define DAP_LOG(msg, ...) printf(msg "\n", ##__VA_ARGS__)
 #else
 #	define DAP_LOG(...) \
 		do               \
@@ -104,6 +104,7 @@ Server::Impl::Impl(const std::shared_ptr<Context> &context, int port)
 		    dap::SetFunctionBreakpointsResponse response;
 		    for(auto const &bp : req.breakpoints)
 		    {
+			    DAP_LOG("Setting breakpoint for function '%s'", bp.name.c_str());
 			    lock.addFunctionBreakpoint(bp.name.c_str());
 			    response.breakpoints.push_back({});
 		    }
@@ -473,6 +474,7 @@ Server::Impl::Impl(const std::shared_ptr<Context> &context, int port)
 		ctx->addListener(this);
 	});
 	configurationDone.wait();
+	DAP_LOG("Debugger connection established");
 }
 
 Server::Impl::~Impl()
