1cb59da72a5ab5c4301f5d5e2b08e44c2bb14a41
Ben Clayton <bclayton@google.com>
2020-01-09 17:19:35 +0000

    Kokoro: Use LESS_DEBUG_INFO for mac builds
    This is an attempt to speed up build times and avoid the flaky timeouts
    we're seeing with the LLVM debug builds.
    
    LESS_DEBUG_INFO reduces the amount of debug information produced to provide stack traces, and no more. This should be adequate for our CI needs.
    
    Bug: b/147355576
    Change-Id: I8dc83dd40a570849d02fe95822de534f0faae595

--- tests/kokoro/macos/continuous.sh
+++ tests/kokoro/macos/continuous.sh
@@ -15,7 +15,9 @@ if [[ -z "${REACTOR_BACKEND}" ]]; then
   REACTOR_BACKEND="LLVM"
 fi
 
-cmake .. "-DASAN=ON" "-DCMAKE_BUILD_TYPE=${BUILD_TYPE}" "-DREACTOR_BACKEND=${REACTOR_BACKEND}" "-DREACTOR_VERIFY_LLVM_IR=1"
+# LESS_DEBUG_INFO is used here to work around Kokoro timeouts with debug, llvm
+# builds. See b/147355576 for more information.
+cmake .. "-DASAN=ON" "-DCMAKE_BUILD_TYPE=${BUILD_TYPE}" "-DREACTOR_BACKEND=${REACTOR_BACKEND}" "-DREACTOR_VERIFY_LLVM_IR=1" "-DLESS_DEBUG_INFO=1"
 make -j$(sysctl -n hw.logicalcpu)
 
 # Run unit tests
