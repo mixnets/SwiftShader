df4201962a23c8cc63f8939fad4300265d73760a
Nicolas Capens <capn@google.com>
2020-01-12 01:12:35 -0500

    Disable the GL_NV_read_depth extension
    This extension is believed to be unused.
    
    Bug: b/147536183
    Change-Id: I79c107826c82462fb0f069b9ea3ce4e2773fbc9d

--- src/OpenGL/libGLESv2/Context.cpp
+++ src/OpenGL/libGLESv2/Context.cpp
@@ -4601,7 +4601,7 @@ const GLubyte *Context::getExtensions(GLuint index, GLuint *numExt) const
 	//	"GL_NV_depth_buffer_float2",  // b/147536183
 		"GL_NV_fence",
 	//	"GL_NV_framebuffer_blit",  // b/147536183
-		"GL_NV_read_depth",
+	//	"GL_NV_read_depth",  // b/147536183
 		"GL_NV_read_depth_stencil",
 		"GL_NV_read_stencil",
 	};
