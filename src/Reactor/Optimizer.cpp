67050454ed784ddee22d55c988c87b83326e2386
Antonio Maiorano <amaiorano@google.com>
2020-02-14 14:38:04 -0500

    Subzero: fix another load from constant data
    When I (hack) fixed the fact that Subzero fails to load from a constant
    source because it interprets it as an offset, I did so by routing all
    loads into sz::createLoad, and in there, making sure to bitcast constant
    sources to variables. I thought this covered all loads, but it turns out
    that one of our optimization passes optimizes stores by removing
    intermediate stores. This would sometimes replace a variable source with
    a constant one, so I applied a similar hack here to avoid doing replace
    in this case.
    
    Bug: b/148272103
    Change-Id: I809688e2c79fa1b62918d3b484a8e2a9e601bd90

--- src/Reactor/Optimizer.cpp
+++ src/Reactor/Optimizer.cpp
@@ -415,6 +415,14 @@ void Optimizer::optimizeStoresInSingleBasicBlock()
 						continue;
 					}
 
+					// TODO(b/148272103): InstLoad assumes that a constant ptr is an offset, rather than an
+					// absolute address. We need to make sure we don't replace a variable with a constant
+					// on this load.
+					if(llvm::isa<Ice::Constant>(storeValue))
+					{
+						continue;
+					}
+
 					replace(inst, storeValue);
 				}
 			}
