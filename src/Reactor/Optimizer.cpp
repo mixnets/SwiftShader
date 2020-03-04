mixnets/SwiftShader

commit b3d9a2adab8a1b1f12ea518f8f7378bc20511e34

Subzero: fix another load from constant data
When I (hack) fixed the fact that Subzero fails to load from a constant
source because it interprets it as an offset, I did so by routing all
loads into sz::createLoad, and in there, making sure to bitcast constant
sources to variables. I thought this covered all loads, but it turns out
that one of our optimization passes optimizes stores by removing
intermediate stores. This would sometimes replace a variable source with
a constant one, so I applied a similar hack here to avoid doing replace
in this case.

Bug: b/148272103
Change-Id: I809688e2c79fa1b62918d3b484a8e2a9e601bd90
Reviewed-on: https://swiftshader-review.googlesource.com/c/SwiftShader/+/41269
Kokoro-Presubmit: kokoro <noreply+kokoro@google.com>
Tested-by: Antonio Maiorano <amaiorano@google.com>
Reviewed-by: Nicolas Capens <nicolascapens@google.com>

src/Reactor/Optimizer.cpp
@@ -252,6 +252,14 @@ void Optimizer::eliminateLoadsFollowingSingleStore()
 					continue;
 				}
 
+				// TODO(b/148272103): InstLoad assumes that a constant ptr is an offset, rather than an
+				// absolute address. We need to make sure we don't replace a variable with a constant
+				// on this load.
+				if(llvm::isa<Ice::Constant>(storeValue))
+				{
+					continue;
+				}
+
 				replace(load, storeValue);
 
 				for(size_t i = 0; i < addressUses.loads.size(); i++)
@@ -412,6 +420,14 @@ void Optimizer::optimizeStoresInSingleBasicBlock()
 					if(!loadTypeMatchesStore(inst, store))
 					{
 						unmatchedLoads = true;
+						continue;
+					}
+
+					// TODO(b/148272103): InstLoad assumes that a constant ptr is an offset, rather than an
+					// absolute address. We need to make sure we don't replace a variable with a constant
+					// on this load.
+					if(llvm::isa<Ice::Constant>(storeValue))
+					{
 						continue;
 					}
 

src/Reactor/SubzeroReactor.cpp
@@ -922,10 +922,10 @@ static std::shared_ptr<Routine> acquireRoutine(Ice::Cfg *const (&functions)[Coun
 		rr::optimize(currFunc);
 
 		currFunc->computeInOutEdges();
-		ASSERT(!currFunc->hasError());
+		ASSERT_MSG(!currFunc->hasError(), "%s", currFunc->getError().c_str());
 
 		currFunc->translate();
-		ASSERT(!currFunc->hasError());
+		ASSERT_MSG(!currFunc->hasError(), "%s", currFunc->getError().c_str());
 
 		currFunc->getAssembler<>()->setInternal(currFunc->getInternal());
 

commit 3be03bd8629c4f4cf80bab0aeaaac0954ae3458e

Subzero: fix bug introduced in Optimizer in b3d9a2adab8
When detecting and removing intermediate stores, if the storeValue is a
constant, we skip it; however, we must make sure to set unmatchedLoads
to true, or the optimizer will incorrectly eliminate the skipped store
on a later iteration.

This bug lead to incorrect generated code, which failed certain deqp-vk
tests.

Bug: b/148272103
Change-Id: I024d75decdc4f1d13e7f82cade50f8be09d68f3e

src/Reactor/Optimizer.cpp
@@ -428,6 +428,7 @@ void Optimizer::optimizeStoresInSingleBasicBlock()
 					// on this load.
 					if(llvm::isa<Ice::Constant>(storeValue))
 					{
+						unmatchedLoads = true;
 						continue;
 					}
 