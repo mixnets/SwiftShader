e17d495af78b9e3ee2812f54ab6b7823fe8e19e9
Alexis Hetu <sugoi@google.com>
2020-04-01 16:49:03 -0400

    Fix ImageView size computation
    Erroneous ImageView size computation was causing out of bounds
    memory accesses whenever an ImageView only represents a subsection
    of an Image. The getSizeInBytes() function now only returns the
    size in bytes within the Image object from the beginning to the
    end of the bytes used by the ImageView.
    
    Bug: b/150464740
    Change-Id: I0985af9cdfbb85b10336a7691f76009496bb2ae5

--- src/Vulkan/VkImage.cpp
+++ src/Vulkan/VkImage.cpp
@@ -178,6 +178,40 @@ const VkMemoryRequirements Image::getMemoryRequirements() const
 	return memoryRequirements;
 }
 
+size_t Image::getSizeInBytes(const VkImageSubresourceRange &subresourceRange) const
+{
+	size_t size = 0;
+	bool multiLayer = (subresourceRange.layerCount > 1);
+	uint32_t lastMipLevel = (subresourceRange.baseMipLevel + subresourceRange.levelCount);
+	bool subMipLevel = (lastMipLevel < mipLevels);
+	auto aspect = static_cast<VkImageAspectFlagBits>(subresourceRange.aspectMask);
+
+	if(multiLayer)
+	{
+		if(subMipLevel)  // Compute size for all layers except the last one, then add relevant mip level sizes only for last layer
+		{
+			size = (subresourceRange.layerCount - 1) * getLayerSize(aspect);
+			for(uint32_t mipLevel = subresourceRange.baseMipLevel; mipLevel < lastMipLevel; ++mipLevel)
+			{
+				size += getMultiSampledLevelSize(aspect, mipLevel);
+			}
+		}
+		else  // All mip levels used, compute full layer sizes
+		{
+			size = subresourceRange.layerCount * getLayerSize(aspect);
+		}
+	}
+	else  // Single layer, add all mip levels in the subresource range
+	{
+		for(uint32_t mipLevel = subresourceRange.baseMipLevel; mipLevel < lastMipLevel; ++mipLevel)
+		{
+			size += getMultiSampledLevelSize(aspect, mipLevel);
+		}
+	}
+
+	return size;
+}
+
 bool Image::canBindToMemory(DeviceMemory *pDeviceMemory) const
 {
 	return pDeviceMemory->checkExternalMemoryHandleType(supportedExternalMemoryHandleTypes);
