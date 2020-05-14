f4ec14fda1e89c8b55533d8b57209f350410bd47
Alexis Hetu <sugoi@google.com>
2020-05-14 00:20:33 -0400

    WIP: Image dirtiness
    Instead of updating images every time we write to them,
    this cl adds a dirtiness mechanism, which will mark an
    Image object as dirty for specific unique subresource
    regions and then only actually perform the required
    task (cube border update, decompression, ...) once the
    image memory is accessed for reading.
    
    Bug: TBD
    Change-Id: I791a69529e4c2e78aa6562251020aa0bf978bd01

--- src/Vulkan/VkDescriptorSetLayout.cpp
+++ src/Vulkan/VkDescriptorSetLayout.cpp
@@ -33,6 +33,38 @@ static bool UsesImmutableSamplers(const VkDescriptorSetLayoutBinding &binding)
 	        (binding.pImmutableSamplers != nullptr));
 }
 
+static bool IsReadOnlyInShader(VkDescriptorType descriptorType)
+{
+	switch(descriptorType)
+	{
+	case VK_DESCRIPTOR_TYPE_SAMPLER:
+	case VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER:
+	case VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE:
+	case VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER:
+	case VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER:
+	case VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC:
+	case VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT:
+	case VK_DESCRIPTOR_TYPE_INLINE_UNIFORM_BLOCK_EXT:
+	case VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR: // Same as VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_NV
+		return true;
+	case VK_DESCRIPTOR_TYPE_STORAGE_IMAGE:
+	case VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER:
+	case VK_DESCRIPTOR_TYPE_STORAGE_BUFFER:
+	case VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC:
+		return false;
+	default:
+		UNREACHABLE("descriptorType: %d", int(descriptorType));
+		break;
+	}
+
+	return false;
+}
+
+static DescriptorSet* GetDescriptorSetFromData(void* descriptorSetData)
+{
+	return reinterpret_cast<DescriptorSet*>(reinterpret_cast<uintptr_t>(descriptorSetData) - sizeof(vk::DescriptorSetHeader));
+}
+
 DescriptorSetLayout::DescriptorSetLayout(const VkDescriptorSetLayoutCreateInfo *pCreateInfo, void *mem)
     : flags(pCreateInfo->flags)
     , bindings(reinterpret_cast<Binding *>(mem))
@@ -88,6 +120,12 @@ DescriptorSetLayout::DescriptorSetLayout(const VkDescriptorSetLayoutCreateInfo *
 
 void DescriptorSetLayout::destroy(const VkAllocationCallbacks *pAllocator)
 {
+	for(auto metadata : descriptorMetaData)
+	{
+		DescriptorSet* set = metadata.first;
+		set->header.layout = nullptr;
+	}
+
 	vk::deallocate(bindings, pAllocator);  // This allocation also contains pImmutableSamplers
 }
 
@@ -137,6 +175,49 @@ bool DescriptorSetLayout::IsDescriptorDynamic(VkDescriptorType type)
 {
 	return type == VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC ||
 	       type == VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC;
+}
+
+void DescriptorSetLayout::Notify(uint8_t* descriptorSetData, DescriptorView::AccessType accessType)
+{
+	if(descriptorSetData)
+	{
+		DescriptorSet* descriptorSet = GetDescriptorSetFromData(descriptorSetData);
+		DescriptorSetLayout* layout = descriptorSet->header.layout;
+		if(layout)
+		{
+			auto it = layout->descriptorMetaData.find(descriptorSet);
+			if(it != layout->descriptorMetaData.end())
+			{
+				DescriptorSetMetaData metadata = it->second;
+				for(auto bindingData : metadata)
+				{
+					for(auto arrayElementData : bindingData)
+					{
+						if(arrayElementData.view && (!arrayElementData.readOnly || (accessType & DescriptorView::READ_ACCESS)))
+						{
+							arrayElementData.view->notify(accessType);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+void DescriptorSetLayout::Unregister(DescriptorSet* descriptorSet)
+{
+	if(descriptorSet)
+	{
+		DescriptorSetLayout* layout = descriptorSet->header.layout;
+		if(layout)
+		{
+			auto it = layout->descriptorMetaData.find(descriptorSet);
+			if(it != layout->descriptorMetaData.end())
+			{
+				layout->descriptorMetaData.erase(it);
+			}
+		}
+	}
 }
 
 size_t DescriptorSetLayout::getDescriptorSetAllocationSize() const
@@ -241,6 +322,20 @@ uint8_t *DescriptorSetLayout::getDescriptorPointer(DescriptorSet *descriptorSet,
 	return &descriptorSet->data[byteOffset];
 }
 
+void DescriptorSetLayout::increment(uint32_t& bindingNumber, uint32_t& arrayElement) const
+{
+	if(arrayElement >= bindings[bindingNumber].descriptorCount)
+	{
+		++bindingNumber;
+		arrayElement = 0;
+		ASSERT(bindingNumber < bindingsArraySize);
+	}
+	else
+	{
+		++arrayElement;
+	}
+}
+
 void SampledImageDescriptor::updateSampler(const vk::Sampler *newSampler)
 {
 	memcpy(reinterpret_cast<void *>(&sampler), newSampler, sizeof(sampler));
@@ -248,13 +343,18 @@ void SampledImageDescriptor::updateSampler(const vk::Sampler *newSampler)
 
 void DescriptorSetLayout::WriteDescriptorSet(Device *device, DescriptorSet *dstSet, VkDescriptorUpdateTemplateEntry const &entry, char const *src)
 {
+	uint32_t bindingNumber = entry.dstBinding;
+	uint32_t arrayElement = entry.dstArrayElement;
 	DescriptorSetLayout *dstLayout = dstSet->header.layout;
-	auto &binding = dstLayout->bindings[entry.dstBinding];
+	auto &binding = dstLayout->bindings[bindingNumber];
 	ASSERT(dstLayout);
 	ASSERT(binding.descriptorType == entry.descriptorType);
 
 	size_t typeSize = 0;
-	uint8_t *memToWrite = dstLayout->getDescriptorPointer(dstSet, entry.dstBinding, entry.dstArrayElement, entry.descriptorCount, &typeSize);
+	uint8_t *memToWrite = dstLayout->getDescriptorPointer(dstSet, bindingNumber, arrayElement, entry.descriptorCount, &typeSize);
+
+	DescriptorSetMetaData& metaData = dstLayout->getOrCreateDescriptorSetMetaData(dstSet);
+	bool readOnlyInShader = IsReadOnlyInShader(entry.descriptorType);
 
 	ASSERT(reinterpret_cast<intptr_t>(memToWrite) % 16 == 0);  // Each descriptor must be 16-byte aligned.
 
@@ -272,6 +372,9 @@ void DescriptorSetLayout::WriteDescriptorSet(Device *device, DescriptorSet *dstS
 				imageSampler[i].updateSampler(vk::Cast(update->sampler));
 			}
 			imageSampler[i].device = device;
+
+			metaData[bindingNumber][arrayElement] = { nullptr, readOnlyInShader };
+			dstLayout->increment(bindingNumber, arrayElement);
 		}
 	}
 	else if(entry.descriptorType == VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER)
@@ -308,6 +411,9 @@ void DescriptorSetLayout::WriteDescriptorSet(Device *device, DescriptorSet *dstS
 			mipmap.sliceP.x = mipmap.sliceP.y = mipmap.sliceP.z = mipmap.sliceP.w = 0;
 			mipmap.onePitchP[0] = mipmap.onePitchP[2] = 1;
 			mipmap.onePitchP[1] = mipmap.onePitchP[3] = static_cast<short>(numElements);
+
+			metaData[bindingNumber][arrayElement] = { bufferView, readOnlyInShader };
+			dstLayout->increment(bindingNumber, arrayElement);
 		}
 	}
 	else if(entry.descriptorType == VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER ||
@@ -416,6 +522,9 @@ void DescriptorSetLayout::WriteDescriptorSet(Device *device, DescriptorSet *dstS
 					WriteTextureLevelInfo(texture, mipmapLevel, width, height, depth, pitchP, sliceP, samplePitchP, sampleMax);
 				}
 			}
+
+			metaData[bindingNumber][arrayElement] = { imageView, readOnlyInShader };
+			dstLayout->increment(bindingNumber, arrayElement);
 		}
 	}
 	else if(entry.descriptorType == VK_DESCRIPTOR_TYPE_STORAGE_IMAGE ||
@@ -446,6 +555,9 @@ void DescriptorSetLayout::WriteDescriptorSet(Device *device, DescriptorSet *dstS
 				                                            : imageView->slicePitchBytes(VK_IMAGE_ASPECT_STENCIL_BIT, 0);
 				descriptor[i].stencilSlicePitchBytes = descriptor[i].stencilSamplePitchBytes * imageView->getSampleCount();
 			}
+
+			metaData[bindingNumber][arrayElement] = { imageView, readOnlyInShader };
+			dstLayout->increment(bindingNumber, arrayElement);
 		}
 	}
 	else if(entry.descriptorType == VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER)
@@ -463,6 +575,9 @@ void DescriptorSetLayout::WriteDescriptorSet(Device *device, DescriptorSet *dstS
 			descriptor[i].arrayLayers = 1;
 			descriptor[i].sampleCount = 1;
 			descriptor[i].sizeInBytes = bufferView->getRangeInBytes();
+
+			metaData[bindingNumber][arrayElement] = { bufferView, readOnlyInShader };
+			dstLayout->increment(bindingNumber, arrayElement);
 		}
 	}
 	else if(entry.descriptorType == VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER ||
@@ -478,6 +593,9 @@ void DescriptorSetLayout::WriteDescriptorSet(Device *device, DescriptorSet *dstS
 			descriptor[i].ptr = buffer->getOffsetPointer(update->offset);
 			descriptor[i].sizeInBytes = static_cast<int>((update->range == VK_WHOLE_SIZE) ? buffer->getSize() - update->offset : update->range);
 			descriptor[i].robustnessSize = static_cast<int>(buffer->getSize() - update->offset);
+
+			metaData[bindingNumber][arrayElement] = { buffer, readOnlyInShader };
+			dstLayout->increment(bindingNumber, arrayElement);
 		}
 	}
 }
@@ -630,6 +748,44 @@ void DescriptorSetLayout::CopyDescriptorSet(const VkCopyDescriptorSet &descripto
 	ASSERT(srcTypeSize == dstTypeSize);
 	size_t writeSize = dstTypeSize * descriptorCopies.descriptorCount;
 	memcpy(memToWrite, memToRead, writeSize);
+
+	uint32_t srcBinding = descriptorCopies.srcBinding;
+	uint32_t srcArrayElement =  descriptorCopies.srcArrayElement;
+	uint32_t dstBinding = descriptorCopies.dstBinding;
+	uint32_t dstArrayElement = descriptorCopies.dstArrayElement;
+	DescriptorSetMetaData& srcMetaData = srcLayout->getOrCreateDescriptorSetMetaData(srcSet);
+	DescriptorSetMetaData& dstMetaData = dstLayout->getOrCreateDescriptorSetMetaData(dstSet);
+	for(uint32_t i = 0; i < descriptorCopies.descriptorCount; ++i)
+	{
+		dstMetaData[dstBinding][dstArrayElement] = srcMetaData[srcBinding][srcArrayElement];
+		srcLayout->increment(srcBinding, srcArrayElement);
+		dstLayout->increment(dstBinding, dstArrayElement);
+	}
+}
+
+DescriptorSetLayout::DescriptorSetMetaData& DescriptorSetLayout::getOrCreateDescriptorSetMetaData(DescriptorSet *set)
+{
+	DescriptorSetLayout *layout = set->header.layout;
+	ASSERT(layout == this); // Descriptor sets shouldn't change layouts
+
+	auto it = descriptorMetaData.find(set);
+	if(it != descriptorMetaData.end())
+	{
+		return it->second;
+	}
+
+	auto& metadata = descriptorMetaData[set];
+	metadata.resize(bindingsArraySize);
+	for(uint32_t i = 0; i < bindingsArraySize; i++)
+	{
+		metadata[i].resize(bindings[i].descriptorCount);
+		for(uint32_t j = 0; j < bindings[i].descriptorCount; j++)
+		{
+			metadata[i][j] = { nullptr, 0 };
+		}
+	}
+
+	return metadata;
 }
 
 }  // namespace vk
