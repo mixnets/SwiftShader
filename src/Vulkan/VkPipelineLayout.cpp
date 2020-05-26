243a48527b51fdfa264c778bd5b2b59508aa7060
Trevor David Black <vantablack@google.com>
2020-05-26 19:28:05 +0000

    Added reference counting for PipelineLayout objects
    Bug: b/155664177
    Change-Id: I9322be202434908dfd652980f50e1024cf2efa2d

--- src/Vulkan/VkPipelineLayout.cpp
+++ src/Vulkan/VkPipelineLayout.cpp
@@ -52,11 +52,23 @@ PipelineLayout::PipelineLayout(const VkPipelineLayoutCreateInfo *pCreateInfo, vo
 	size_t pushConstantRangesSize = pCreateInfo->pushConstantRangeCount * sizeof(VkPushConstantRange);
 	pushConstantRanges = reinterpret_cast<VkPushConstantRange *>(bindingStorage);
 	memcpy(pushConstantRanges, pCreateInfo->pPushConstantRanges, pushConstantRangesSize);
+
+	incRefCount();
 }
 
 void PipelineLayout::destroy(const VkAllocationCallbacks *pAllocator)
 {
 	vk::deallocate(descriptorSets[0].bindings, pAllocator);  // pushConstantRanges are in the same allocation
+}
+
+bool PipelineLayout::release(const VkAllocationCallbacks *pAllocator)
+{
+	if(decRefCount() == 0)
+	{
+		vk::deallocate(descriptorSets[0].bindings, pAllocator);  // pushConstantRanges are in the same allocation
+		return true;
+	}
+	return false;
 }
 
 size_t PipelineLayout::ComputeRequiredAllocationSize(const VkPipelineLayoutCreateInfo *pCreateInfo)
@@ -102,6 +114,16 @@ uint32_t PipelineLayout::getDescriptorSize(uint32_t setNumber, uint32_t bindingN
 bool PipelineLayout::isDescriptorDynamic(uint32_t setNumber, uint32_t bindingNumber) const
 {
 	return DescriptorSetLayout::IsDescriptorDynamic(getDescriptorType(setNumber, bindingNumber));
+}
+
+uint32_t PipelineLayout::incRefCount()
+{
+	return refCount.fetch_add(1, std::memory_order_seq_cst) + 1;
+}
+
+uint32_t PipelineLayout::decRefCount()
+{
+	return refCount.fetch_sub(1, std::memory_order_seq_cst) - 1;
 }
 
 }  // namespace vk
