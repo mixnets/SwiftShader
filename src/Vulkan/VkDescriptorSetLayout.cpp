3d01f054467e28135c0a670238a6a20cdb6f3827
Nicolas Capens <capn@google.com>
2020-07-31 01:33:33 -0400

    WIP-combine-cube-face-layer
    Change-Id: I379fa251c21248a70a67d9abf84fbbe39e62157b

--- src/Vulkan/VkDescriptorSetLayout.cpp
+++ src/Vulkan/VkDescriptorSetLayout.cpp
@@ -411,7 +411,8 @@ void DescriptorSetLayout::WriteDescriptorSet(Device *device, DescriptorSet *dstS
 					int width = extent.width;
 					int height = extent.height;
 					int bytes = format.bytes();
-					int layers = imageView->getSubresourceRange().layerCount;  // TODO(b/129523279): Untangle depth vs layers throughout the sampler
+					int layers = imageView->getSubresourceRange().layerCount;  //// TODO(b/129523279): Untangle depth vs layers throughout the sampler
+					if(imageView->getType() == VK_IMAGE_VIEW_TYPE_CUBE_ARRAY) layers /= 6;
 					int depth = layers > 1 ? layers : extent.depth;
 					int pitchP = imageView->rowPitchBytes(aspect, level, ImageView::SAMPLING) / bytes;
 					int sliceP = (layers > 1 ? imageView->layerPitchBytes(aspect, ImageView::SAMPLING) : imageView->slicePitchBytes(aspect, level, ImageView::SAMPLING)) / bytes;
