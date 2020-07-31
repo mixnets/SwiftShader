cb4e0cfe5928822dfea2949dc0e3c56d9c572f5b
Sean Risser <srisser@google.com>
2020-07-31 11:38:40 -0400

    Switch Vulkan version to 1.2
    This is the final CL required for our 1.2 work, so all 1.2 patches
    should be based on top of this.
    
    Also update the unittests to expect version 1.2
    
    Bug: b/147825369
    Tests: dEQP-VK.*
    Change-Id: Ib3bb07662140fa976ec2532124ab2d7337f34c55

--- src/Vulkan/VkConfig.hpp
+++ src/Vulkan/VkConfig.hpp
@@ -28,7 +28,7 @@ namespace vk {
 
 enum
 {
-	API_VERSION = VK_API_VERSION_1_1,
+	API_VERSION = VK_API_VERSION_1_2,
 	DRIVER_VERSION = VK_MAKE_VERSION(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION),
 	VENDOR_ID = 0x1AE0,  // Google, Inc.: https://pcisig.com/google-inc-1
 	DEVICE_ID = 0xC0DE,  // SwiftShader (placeholder)
