1f041b7cd8e290b2bca35cac652e948d9b113ad5
Nicolas Capens <capn@google.com>
2020-08-06 14:13:43 -0400

    Implement shaderStorageImageMultisample support
    This fixes a bug in the descriptor data where the sample pitch was set
    to the layer pitch for array images (it should always be the slice
    pitch), and the descriptor's slice pitch (which stores the layer pitch
    in case of array images), was taking the sample count into account
    twice.
    
    Bug: b/163142358
    Tests: dEQP-VK.*
    Change-Id: I9d816c249e14a17722d29f56015c8dc8cc7d51d4

--- src/Pipeline/SpirvShaderImage.cpp
+++ src/Pipeline/SpirvShaderImage.cpp
@@ -115,7 +115,7 @@ SpirvShader::EmitResult SpirvShader::EmitImageSampleExplicitLod(Variant variant,
 		return EmitImageSample({ variant, Grad }, insn, state);
 	}
 	else
-		UNSUPPORTED("Image Operands %x", imageOperands);
+		UNSUPPORTED("Image operands 0x%08X", imageOperands);
 
 	return EmitResult::Continue;
 }
@@ -234,7 +234,7 @@ void SpirvShader::EmitImageSampleUnconditional(Array<SIMD::Float> &out, ImageIns
 
 		if(imageOperands != 0)
 		{
-			UNSUPPORTED("Image operand %x", imageOperands);
+			UNSUPPORTED("Image operands 0x%08X", imageOperands);
 		}
 	}
 
@@ -605,7 +605,7 @@ SpirvShader::EmitResult SpirvShader::EmitImageRead(InsnIterator insn, EmitState 
 	if(insn.wordCount() > 5)
 	{
 		int operand = 6;
-		auto imageOperands = insn.word(5);
+		uint32_t imageOperands = insn.word(5);
 		if(imageOperands & spv::ImageOperandsSampleMask)
 		{
 			sampleId = insn.word(operand++);
@@ -613,7 +613,10 @@ SpirvShader::EmitResult SpirvShader::EmitImageRead(InsnIterator insn, EmitState 
 		}
 
 		// Should be no remaining image operands.
-		ASSERT(!imageOperands);
+		if(imageOperands != 0)
+		{
+			UNSUPPORTED("Image operands 0x%08X", imageOperands);
+		}
 	}
 
 	ASSERT(imageType.definition.opcode() == spv::OpTypeImage);
@@ -990,8 +993,24 @@ SpirvShader::EmitResult SpirvShader::EmitImageWrite(InsnIterator insn, EmitState
 
 	ASSERT(imageType.definition.opcode() == spv::OpTypeImage);
 
-	// TODO(b/131171141): Not handling any image operands yet.
-	ASSERT(insn.wordCount() == 4);
+	Object::ID sampleId = 0;
+
+	if(insn.wordCount() > 4)
+	{
+		int operand = 5;
+		uint32_t imageOperands = insn.word(4);
+		if(imageOperands & spv::ImageOperandsSampleMask)
+		{
+			sampleId = insn.word(operand++);
+			imageOperands &= ~spv::ImageOperandsSampleMask;
+		}
+
+		// Should be no remaining image operands.
+		if(imageOperands != 0)
+		{
+			UNSUPPORTED("Image operands 0x%08X", (int)imageOperands);
+		}
+	}
 
 	auto coordinate = Operand(this, state, insn.word(2));
 	auto texel = Operand(this, state, insn.word(3));
@@ -1176,7 +1195,7 @@ SpirvShader::EmitResult SpirvShader::EmitImageWrite(InsnIterator insn, EmitState
 	// - https://www.khronos.org/registry/vulkan/specs/1.2/html/chap16.html#textures-output-coordinate-validation
 	auto robustness = OutOfBoundsBehavior::Nullify;
 
-	auto texelPtr = GetTexelAddress(state, imageBase, imageSizeInBytes, coordinate, imageType, binding, texelSize, 0, false, robustness);
+	auto texelPtr = GetTexelAddress(state, imageBase, imageSizeInBytes, coordinate, imageType, binding, texelSize, sampleId, false, robustness);
 
 	// Scatter packed texel data.
 	// TODO(b/160531165): Provide scatter abstractions for various element sizes.
