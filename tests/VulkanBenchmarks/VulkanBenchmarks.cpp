4db108d613f11fe3d73a86e61edff362dec8605d
Nicolas Capens <capn@google.com>
2020-08-26 15:43:22 -0400

    WIP-fast-resolve
    Change-Id: I15729552f01a509a5cfce20cd7de06d0b764cf0a

--- tests/VulkanBenchmarks/VulkanBenchmarks.cpp
+++ tests/VulkanBenchmarks/VulkanBenchmarks.cpp
@@ -213,10 +213,10 @@ static void ClearImage(benchmark::State &state, vk::Format clearFormat, vk::Imag
 	{
 		benchmark.clear();
 	}
-}
+} /*
 BENCHMARK_CAPTURE(ClearImage, VK_FORMAT_R8G8B8A8_UNORM, vk::Format::eR8G8B8A8Unorm, vk::ImageAspectFlagBits::eColor)->Unit(benchmark::kMillisecond);
 BENCHMARK_CAPTURE(ClearImage, VK_FORMAT_R32_SFLOAT, vk::Format::eR32Sfloat, vk::ImageAspectFlagBits::eColor)->Unit(benchmark::kMillisecond);
-BENCHMARK_CAPTURE(ClearImage, VK_FORMAT_D32_SFLOAT, vk::Format::eD32Sfloat, vk::ImageAspectFlagBits::eDepth)->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(ClearImage, VK_FORMAT_D32_SFLOAT, vk::Format::eD32Sfloat, vk::ImageAspectFlagBits::eDepth)->Unit(benchmark::kMillisecond);*/
 
 class Window
 {
