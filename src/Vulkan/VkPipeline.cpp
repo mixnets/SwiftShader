ac8a226b4c2492a7e714e506022a1bc0e64c7bd1
Corentin Wallez <cwallez@chromium.org>
2020-10-01 11:45:31 +0200

    Is depthBias tested in the Vulkan CTS?

--- src/Vulkan/VkPipeline.cpp
+++ src/Vulkan/VkPipeline.cpp
@@ -250,9 +250,9 @@ GraphicsPipeline::GraphicsPipeline(const VkGraphicsPipelineCreateInfo *pCreateIn
 	context.cullMode = rasterizationState->cullMode;
 	context.frontFace = rasterizationState->frontFace;
 	context.polygonMode = rasterizationState->polygonMode;
-	context.depthBias = (rasterizationState->depthBiasEnable != VK_FALSE) ? rasterizationState->depthBiasConstantFactor : 0.0f;
-	context.slopeDepthBias = (rasterizationState->depthBiasEnable != VK_FALSE) ? rasterizationState->depthBiasSlopeFactor : 0.0f;
-	context.depthBiasClamp = (rasterizationState->depthBiasEnable != VK_FALSE) ? rasterizationState->depthBiasClamp : 0.0f;
+	context.depthBias = 0.0f;
+	context.slopeDepthBias = 0.0f;
+	context.depthBiasClamp = 0.0f;
 	context.depthRangeUnrestricted = device->hasExtension(VK_EXT_DEPTH_RANGE_UNRESTRICTED_EXTENSION_NAME);
 
 	// From the Vulkan spec for vkCmdSetDepthBias:
