59114aa89ffa31edcc924529430a366ba263ec7f
Alexis Hetu <sugoi@google.com>
2021-02-09 18:02:11 -0500

    Only perform LOD computation when it is necessary
    If an image has a single mip level, and filtering isn't LOD dependent,
    as is the case when using FILTER_MIN_POINT_MAG_LINEAR or
    FILTER_MIN_LINEAR_MAG_POINT, LOD computation is not necessary in
    order to perform image sampling and can be bypassed entirely.
    
    Bug: b/179889245
    Change-Id: Ic69fe0c45211ccbb66c88c502c2dba1c50630aa7

--- src/Pipeline/SpirvShaderSampling.cpp
+++ src/Pipeline/SpirvShaderSampling.cpp
@@ -78,6 +78,19 @@ SpirvShader::ImageSampler *SpirvShader::getImageSampler(uint32_t inst, vk::Sampl
 			samplerState.maxAnisotropy = sampler->maxAnisotropy;
 			samplerState.minLod = sampler->minLod;
 			samplerState.maxLod = sampler->maxLod;
+
+			// If there's more than one mip level or filtering depends on the LOD level,
+			// the sampler will need to compute the LOD to produce the proper result.
+			// Otherwise, it can be ignored.
+			// We can skip the LOD computation for all modes, except LOD query,
+			// where we have to return the proper value.
+			if(!imageViewData.multipleMipLevels &&
+			   (samplerState.textureFilter != FILTER_MIN_POINT_MAG_LINEAR) &&
+			   (samplerState.textureFilter != FILTER_MIN_LINEAR_MAG_POINT) &&
+			   (instruction.samplerMethod != Query))
+			{
+				instruction.samplerMethod = Base;
+			}
 		}
 		else
 		{
