8ecea689ab7c012d60b04a99a4faf9b643dba304
Alexis Hetu <sugoi@google.com>
2021-02-09 18:02:11 -0500

    Only perform LOD computation when it is necessary
    If an image has a single mip level, and filtering isn't LOD dependent,
    as is the case when using FILTER_MIN_POINT_MAG_LINEAR or
    FILTER_MIN_LINEAR_MAG_POINT, LOD computation is not necessary in
    order to perform image sampling and can be bypassed entirely.
    
    Bug: b/179889245
    Change-Id: Ic69fe0c45211ccbb66c88c502c2dba1c50630aa7

--- src/Pipeline/SamplerCore.cpp
+++ src/Pipeline/SamplerCore.cpp
@@ -54,7 +54,15 @@ Vector4f SamplerCore::sampleTexture(Pointer<Byte> &texture, Float4 uvwa[4], Floa
 		w = As<Float4>(face);
 	}
 
-	if(function == Implicit || function == Bias || function == Grad || function == Query)
+	bool lodClamp = (function != Base && function != Fetch && function != Gather);
+
+	// We can't skip the LOD computation for LOD query, where we have to return the proper value
+	if(lodClamp && (function != Query) && (state.minLod == state.maxLod))
+	{
+		// Skip costly LOD computation if there's only 1 possible outcome
+		lod = state.minLod;
+	}
+	else if(function == Implicit || function == Bias || function == Grad || function == Query)
 	{
 		if(state.is1D())
 		{
@@ -101,7 +109,7 @@ Vector4f SamplerCore::sampleTexture(Pointer<Byte> &texture, Float4 uvwa[4], Floa
 	else
 		UNREACHABLE("Sampler function %d", int(function));
 
-	if(function != Base && function != Fetch && function != Gather)
+	if(lodClamp)
 	{
 		if(function == Query)
 		{
