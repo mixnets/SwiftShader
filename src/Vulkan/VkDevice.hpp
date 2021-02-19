fc2ad7ab58844124445a22346d052e0362130ce0
Alexis Hetu <sugoi@google.com>
2021-02-19 17:41:35 -0500

    Deduce all sampler parameters from SamplingRoutineCache::Key
    This cl changes createSamplingRoutine to capture only the "device"
    variable, instead of capturing all function variables. The "device"
    variable is required to find the sampler state from the sampler id.
    
    This cl also changes the parameters of the getImageSampler function
    so that it uses only the minimal parameters required to produce the
    sampling routine.
    
    Bug: b/152227757
    Change-Id: Id7f5e18e09f078589a1a1edc12622ed40126cd32

--- src/Vulkan/VkDevice.hpp
+++ src/Vulkan/VkDevice.hpp
@@ -140,6 +140,7 @@ public:
 
 		uint32_t index(const SamplerState &samplerState);
 		void remove(const SamplerState &samplerState);
+		const SamplerState *find(uint32_t id);
 
 	private:
 		struct Identifier
@@ -156,6 +157,7 @@ public:
 
 	uint32_t indexSampler(const SamplerState &samplerState);
 	void removeSampler(const SamplerState &samplerState);
+	const SamplerState *findSampler(uint32_t samplerId) const;
 
 	std::shared_ptr<vk::dbg::Context> getDebuggerContext() const
 	{
