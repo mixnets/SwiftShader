3fa23a3caa0dabc37c0bf4a5eb53667c8523d1c2
Nicolas Capens <capn@google.com>
2021-08-20 09:12:45 -0400

    WIP-noopt-subzero-exe-3
    Change-Id: I85737b21553839b418c257c9b93252dec5bb23ad

--- src/Reactor/SubzeroReactor.cpp
+++ src/Reactor/SubzeroReactor.cpp
@@ -1021,7 +1021,7 @@ static std::shared_ptr<Routine> acquireRoutine(Ice::Cfg *const (&functions)[Coun
 		{
 			rr::optimize(currFunc);
 		}*/
-		// 2
+		// 3
 
 		currFunc->computeInOutEdges();
 		ASSERT_MSG(!currFunc->hasError(), "%s", currFunc->getError().c_str());
