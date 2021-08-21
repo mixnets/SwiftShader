301e1fef3de2280782aa6ceeedf544e47b0548c3
Nicolas Capens <capn@google.com>
2021-08-21 02:28:50 -0400

    3
    Change-Id: Icaf41fb2743f6112e1322844dcf83d7a1c37cb4e

--- src/Reactor/Optimizer.cpp
+++ src/Reactor/Optimizer.cpp
@@ -396,7 +396,7 @@ void Optimizer::optimizeSingleBasicBlockLoadsStores()
 			bool allLoadsReplaced = true;
 		};
 
-		std::unordered_map<Ice::SizeT /* AllocaInst::Number */, LastStore> lastStoreTo;  // 2
+		std::unordered_map<Ice::SizeT /* AllocaInst::Number */, LastStore> lastStoreTo;  // 3
 
 		for(Ice::Inst &inst : block->getInsts())
 		{
