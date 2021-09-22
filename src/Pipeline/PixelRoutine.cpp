7a2b5cad56c45bc4e1a7687ba512a91a8c216721
Nicolas Capens <capn@google.com>
2021-09-22 14:58:02 -0400

    WIP-EarlyFragmentTests-color
    DO NOT SUBMIT
    
    This change makes us skip outputting fragment color when
    EarlyFragmentTests has been declared in the shader. That's probably not
    the intended behavior, and should cause some dEQP-VK tests to fail.
    
    Bug: b/200826740
    Change-Id: I9b43139650ad1800fe18b85a2574c3a71fddc26e

--- src/Pipeline/PixelRoutine.cpp
+++ src/Pipeline/PixelRoutine.cpp
@@ -320,7 +320,10 @@ void PixelRoutine::quad(Pointer<Byte> cBuffer[MAX_COLOR_BUFFERS], Pointer<Byte> 
 						writeDepth(zBuffer, x, zMask, samples);
 					}
 
-					blendColor(cBuffer, x, sMask, zMask, cMask, samples);
+					if(!earlyFragmentTests)
+					{
+						blendColor(cBuffer, x, sMask, zMask, cMask, samples);
+					}
 
 					occlusionSampleCount(zMask, sMask, samples);
 				}
