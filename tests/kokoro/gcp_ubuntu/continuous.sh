b107d5807b1c34610509a7a0ca16810727eea239
Nicolas Capens <nicolascapens@google.com>
2021-10-01 13:34:35 +0000

    Kokoro: Fix gcp_ubuntu builds failing to update CMake
    Uses the instructions at https://apt.kitware.com
    
    Bug: apt-key is deprecated
    Change-Id: I58f49cf256fd660cc249668cdec5f8bf948e1309

--- tests/kokoro/gcp_ubuntu/continuous.sh
+++ tests/kokoro/gcp_ubuntu/continuous.sh
@@ -7,9 +7,14 @@ set -x # Display commands being run.
 
 # Update CMake
 sudo aptitude purge -yq cmake
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
-sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ xenial main'
-sudo aptitude update -yq
+sudo apt-get update
+sudo apt-get install apt-transport-https wget
+sudo apt-get -y install ca-certificates
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null --no-check-certificate | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ xenial main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
+sudo apt-get update
+sudo rm /usr/share/keyrings/kitware-archive-keyring.gpg
+sudo apt-get install kitware-archive-keyring
 sudo aptitude install -yq cmake
 cmake --version
 
