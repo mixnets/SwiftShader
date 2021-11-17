eb6fad287026cb034a6f849ee925c73f02b78efd
Nicolas Capens <capn@google.com>
2021-11-17 00:25:05 -0500

    WIP-reactor-assert
    Bug: b/156389924
    Bug: chromium:1209285
    Change-Id: I96f19aa907e6d8cee315a374441bb7a9554b4441

--- tests/ReactorUnitTests/ReactorUnitTests.cpp
+++ tests/ReactorUnitTests/ReactorUnitTests.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "Assert.hpp"
 #include "Coroutine.hpp"
 #include "Print.hpp"
 #include "Reactor.hpp"
@@ -673,6 +674,55 @@ TEST(ReactorUnitTests, StoreBeforeIndirectStore)
 
 	int result = routine(true);
 	EXPECT_EQ(result, 4);
+}
+
+TEST(ReactorUnitTests, AssertTrue)
+{
+	FunctionT<int()> function;
+	{
+		Int a = 3;
+		Int b = 5;
+
+		Assert(a < b);
+
+		Return(a + b);
+	}
+
+	auto routine = function(testName().c_str());
+
+	int result = routine();
+	EXPECT_EQ(result, 8);
+}
+
+TEST(ReactorUnitTests, AssertFalse)
+{
+	FunctionT<int()> function;
+	{
+		Int a = 3;
+		Int b = 5;
+
+		Assert(a == b);
+
+		Return(a + b);
+	}
+
+	auto routine = function(testName().c_str());
+
+#ifndef NDEBUG
+	EXPECT_DEATH(
+	    {
+		    int result = routine();
+		    EXPECT_NE(result, result);  // We should never reach this
+	    },
+#	if !defined(__APPLE__)
+	    "AssertFalse");  // stderr should contain the assert's expression, file:line, and function
+#	else
+	    "");  // TODO(b/156389924): On macOS an stderr redirect can cause googletest to fail the capture
+#	endif
+#else
+	int result = routine();
+	EXPECT_EQ(result, 8);
+#endif
 }
 
 TEST(ReactorUnitTests, SubVectorLoadStore)
