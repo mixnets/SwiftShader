3d1e729c452c62e434d48097e7a1515968bd3ef9
Nicolas Capens <capn@google.com>
2022-01-06 11:02:31 -0500

    WIP-linear-layout-image-feature-storage
    DO NOT SUBMIT - checks if there is test coverage for this
    
    Bug: b/171299814
    Tests: dEQP-VK.*
    Change-Id: I54d37f476f9bdaea3588ed7ee76d1bafd9884ddf

--- src/Vulkan/VkPhysicalDevice.cpp
+++ src/Vulkan/VkPhysicalDevice.cpp
@@ -2006,6 +2006,7 @@ void PhysicalDevice::GetFormatProperties(Format format, VkFormatProperties3 *pFo
 		if(!format.isCompressed())
 		{
 			VkFormatFeatureFlagBits2KHR transferableFeatureBits = VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT |
+			                                                      VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT |
 			                                                      VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT |
 			                                                      VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_DEPTH_COMPARISON_BIT_KHR;
 
