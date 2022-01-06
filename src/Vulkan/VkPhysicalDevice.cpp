1b2c237878ed928447931ecc61eea92d0e832d70
Nicolas Capens <capn@google.com>
2022-01-06 12:08:50 -0500

    WIP-linear-layout-image-feature-blit
    DO NOT SUBMIT - checks if there is test coverage for this
    
    Bug: b/171299814
    Tests: dEQP-VK.*
    Change-Id: I4b9a4031d2c236673675681e42694cbc8387143c

--- src/Vulkan/VkPhysicalDevice.cpp
+++ src/Vulkan/VkPhysicalDevice.cpp
@@ -2011,6 +2011,8 @@ void PhysicalDevice::GetFormatProperties(Format format, VkFormatProperties3 *pFo
 			                                                      VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT |
 			                                                      VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BLEND_BIT |
 			                                                      VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT |
+			                                                      VK_FORMAT_FEATURE_BLIT_SRC_BIT |
+			                                                      VK_FORMAT_FEATURE_BLIT_DST_BIT |
 			                                                      VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT |
 			                                                      VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_DEPTH_COMPARISON_BIT_KHR;
 
