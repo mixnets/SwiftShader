6d41f17f3068dab63ed9959cd8526ec35eff13c5
Nicolas Capens <capn@google.com>
2022-01-06 11:03:41 -0500

    WIP-linear-layout-image-feature-storage-atomic
    DO NOT SUBMIT - checks if there is test coverage for this
    
    Bug: b/171299814
    Tests: dEQP-VK.*
    Change-Id: If3af876bf84250909a4eb5cbbba99b6d5149695a

--- src/Vulkan/VkPhysicalDevice.cpp
+++ src/Vulkan/VkPhysicalDevice.cpp
@@ -2007,6 +2007,7 @@ void PhysicalDevice::GetFormatProperties(Format format, VkFormatProperties3 *pFo
 		{
 			VkFormatFeatureFlagBits2KHR transferableFeatureBits = VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT |
 			                                                      VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT |
+			                                                      VK_FORMAT_FEATURE_STORAGE_IMAGE_ATOMIC_BIT |
 			                                                      VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT |
 			                                                      VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_DEPTH_COMPARISON_BIT_KHR;
 
