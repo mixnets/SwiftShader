53dd290738886e49f2589bd973b3d33eb3d2283b
Nicolas Capens <capn@google.com>
2022-01-06 11:07:00 -0500

    WIP-linear-layout-image-feature-attachment
    DO NOT SUBMIT - checks if there is test coverage for this
    
    Bug: b/171299814
    Tests: dEQP-VK.*
    Change-Id: I11d5513317dd09c95cdd901832972d7098deb957

--- src/Vulkan/VkPhysicalDevice.cpp
+++ src/Vulkan/VkPhysicalDevice.cpp
@@ -1688,6 +1688,9 @@ void PhysicalDevice::GetFormatProperties(Format format, VkFormatProperties3KHR *
 			VkFormatFeatureFlagBits2KHR transferrableFeatureBits = VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT |
 			                                                       VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT |
 			                                                       VK_FORMAT_FEATURE_STORAGE_IMAGE_ATOMIC_BIT |
+			                                                       VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT |
+			                                                       VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BLEND_BIT |
+			                                                       VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT |
 			                                                       VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT |
 			                                                       VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_DEPTH_COMPARISON_BIT_KHR;
 
