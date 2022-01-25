e52cda4134cc6be11504b1281530e028234a7df4
Daniele Vettorel <dvet@google.com>
2022-01-25 19:11:09 +0000

    Refactor scheduler creation, add affinity opts.
    Note: the VkDevice class now accepts a simple pointer to marl::Scheduler
    since holding a shared_ptr is misleading. The device does not really
    share ownership of the scheduler, which is a static object that exists
    until the SwiftShader library is unloaded.
    
    Bug: b/216019572
    Change-Id: Ie464d1d63a02c25f8aff327437e1a578dd42f889

--- src/Vulkan/libVulkan.cpp
+++ src/Vulkan/libVulkan.cpp
@@ -48,7 +48,7 @@
 #include "Reactor/Nucleus.hpp"
 #include "System/CPUID.hpp"
 #include "System/Debug.hpp"
-#include "System/SwiftConfig.hpp"
+#include "System/Scheduler.hpp"
 #include "WSI/HeadlessSurfaceKHR.hpp"
 #include "WSI/VkSwapchainKHR.hpp"
 
@@ -75,11 +75,6 @@
 #ifdef VK_USE_PLATFORM_WIN32_KHR
 #	include "WSI/Win32SurfaceKHR.hpp"
 #endif
-
-#include "marl/mutex.h"
-#include "marl/scheduler.h"
-#include "marl/thread.h"
-#include "marl/tsa.h"
 
 #ifdef __ANDROID__
 #	include "commit.h"
@@ -125,36 +120,6 @@ void setReactorDefaultConfig()
 	               .add(rr::Optimization::Pass::InstructionCombining);
 
 	rr::Nucleus::adjustDefaultConfig(cfg);
-}
-
-std::shared_ptr<marl::Scheduler> getOrCreateScheduler()
-{
-	struct Scheduler
-	{
-		marl::mutex mutex;
-		std::weak_ptr<marl::Scheduler> weakptr GUARDED_BY(mutex);
-	};
-
-	static Scheduler scheduler;  // TODO(b/208256248): Avoid exit-time destructor.
-
-	const sw::Configuration &config = sw::getConfiguration();
-	int threadCount = config.threadCount;
-
-	marl::lock lock(scheduler.mutex);
-	auto sptr = scheduler.weakptr.lock();
-	if(!sptr)
-	{
-		marl::Scheduler::Config cfg;
-		cfg.setWorkerThreadCount(threadCount == 0 ? std::min<size_t>(marl::Thread::numLogicalCPUs(), 16)
-		                                          : threadCount);
-		cfg.setWorkerThreadInitializer([](int) {
-			sw::CPUID::setFlushToZero(true);
-			sw::CPUID::setDenormalsAreZero(true);
-		});
-		sptr = std::make_shared<marl::Scheduler>(cfg);
-		scheduler.weakptr = sptr;
-	}
-	return sptr;
 }
 
 // initializeLibrary() is called by vkCreateInstance() to perform one-off global
@@ -987,8 +952,8 @@ VKAPI_ATTR VkResult VKAPI_CALL vkCreateDevice(VkPhysicalDevice physicalDevice, c
 		(void)queueFamilyPropertyCount;  // Silence unused variable warning
 	}
 
-	auto scheduler = getOrCreateScheduler();
-	return vk::DispatchableDevice::Create(pAllocator, pCreateInfo, pDevice, vk::Cast(physicalDevice), enabledFeatures, scheduler);
+	static sw::Scheduler scheduler(sw::getConfiguration());  // TODO(b/208256248): Avoid exit-time destructor.
+	return vk::DispatchableDevice::Create(pAllocator, pCreateInfo, pDevice, vk::Cast(physicalDevice), enabledFeatures, scheduler.get());
 }
 
 VKAPI_ATTR void VKAPI_CALL vkDestroyDevice(VkDevice device, const VkAllocationCallbacks *pAllocator)
