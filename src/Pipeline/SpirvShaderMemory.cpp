745e79db6c701c3a3ea7f00a5608005d4d2e6416
Alexis Hetu <sugoi@google.com>
2022-06-14 17:51:58 -0400

    Load and Store functions for pointers
    This CL adds functions which allow loading and storing pointers
    from/to storage. Is is implemented with a brute force loop to
    load or store each pointer individually. Loading pointers this
    was always results into creating 4 individual pointers (as
    opposed to "isBasePlusOffset" pointers).
    
    Bug: b/184952772
    Change-Id: I99f442623220b4a3de568586c5b7ccb4437581ff

--- src/Pipeline/SpirvShaderMemory.cpp
+++ src/Pipeline/SpirvShaderMemory.cpp
@@ -60,7 +60,14 @@ SpirvShader::EmitResult SpirvShader::EmitLoad(InsnIterator insn, EmitState *stat
 
 	VisitMemoryObject(pointerId, [&](const MemoryElement &el) {
 		auto p = GetElementPointer(ptr, el.offset, interleavedByLane);
-		dst.move(el.index, p.Load<SIMD::Float>(robustness, state->activeLaneMask(), atomic, memoryOrder));
+		if(dst.isPointer())
+		{
+			dst.move(el.index, p.Load<SIMD::Pointer>(robustness, state->activeLaneMask(), atomic, memoryOrder, sizeof(void *)));
+		}
+		else
+		{
+			dst.move(el.index, p.Load<SIMD::Float>(robustness, state->activeLaneMask(), atomic, memoryOrder));
+		}
 	});
 
 	SPIRV_SHADER_DBG("Load(atomic: {0}, order: {1}, ptr: {2}, val: {3}, mask: {4})", atomic, int(memoryOrder), ptr, dst, state->activeLaneMask());
@@ -111,7 +118,14 @@ void SpirvShader::Store(Object::ID pointerId, const Operand &value, bool atomic,
 
 	VisitMemoryObject(pointerId, [&](const MemoryElement &el) {
 		auto p = GetElementPointer(ptr, el.offset, interleavedByLane);
-		p.Store(value.Float(el.index), robustness, mask, atomic, memoryOrder);
+		if(value.isPointer())
+		{
+			p.Store(value.Pointer(el.index), robustness, mask, atomic, memoryOrder);
+		}
+		else
+		{
+			p.Store(value.Float(el.index), robustness, mask, atomic, memoryOrder);
+		}
 	});
 }
 
