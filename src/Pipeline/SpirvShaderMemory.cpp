7789ae11d00c29dbac1fdaafd31bd6429677f9c0
Nicolas Capens <capn@google.com>
2022-10-11 17:05:56 -0400

    WIP-detect-stores-in-helper
    Bug: check test coverage
    Change-Id: I2e874b7841a5c6231f1eac6a18ef6f25a2ad98bd

--- src/Pipeline/SpirvShaderMemory.cpp
+++ src/Pipeline/SpirvShaderMemory.cpp
@@ -548,8 +548,13 @@ bool SpirvShader::StoresInHelperInvocation(spv::StorageClass storageClass)
 	case spv::StorageClassStorageBuffer:
 	case spv::StorageClassPhysicalStorageBuffer:
 	case spv::StorageClassImage:
+	case spv::StorageClassPrivate:
+	case spv::StorageClassOutput:
 		return false;
+	case spv::StorageClassFunction:
+		return true;
 	default:
+		ASSERT(false);
 		return true;
 	}
 }
