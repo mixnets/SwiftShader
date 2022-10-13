38b2b75f611d4c3368ab9aadc9ab8abd8320bdc5
Nicolas Capens <capn@google.com>
2022-10-13 15:59:34 -0400

    WIP-detect-stores-in-helper-global
    Bug: check test coverage
    Change-Id: Id538698ef86a4ecb5d95b93bc89ae119de926b04

--- src/Pipeline/SpirvShaderMemory.cpp
+++ src/Pipeline/SpirvShaderMemory.cpp
@@ -548,9 +548,9 @@ bool SpirvShader::StoresInHelperInvocation(spv::StorageClass storageClass)
 	case spv::StorageClassStorageBuffer:
 	case spv::StorageClassPhysicalStorageBuffer:
 	case spv::StorageClassImage:
-	case spv::StorageClassPrivate:
 		return false;
 	case spv::StorageClassFunction:
+	case spv::StorageClassPrivate:
 		return true;
 	default:
 		ASSERT(false);
